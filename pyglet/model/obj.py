c61d6f74ff29ff2432eabfc13a22e984671f44ab
r1chardj0n3s <devnull@localhost>
2006-10-03 10:01:41 +0000

    some initial OBJ file loading and testing

---
+++ pyglet/model/obj.py
@@ -0,0 +1,129 @@
+import os
+import ctypes
+from pyglet.GL.VERSION_1_1 import *
+
+THREEFV = ctypes.c_float * 3
+
+def MTL(filename):
+    contents = {}
+    mtl = None
+    for line in open(filename, "r"):
+        if line.startswith('#'): continue
+        values = line.split()
+        if not values: continue
+        if values[0] == 'newmtl':
+            mtl = contents[values[1]] = {}
+        elif mtl is None:
+            raise ValueError, "mtl file doesn't start with newmtl stmt"
+        elif values[0] == 'map_Kd':
+            # load the texture referred to by this declaration
+            mtl[values[0]] = values[1]
+            surf = pygame.image.load(mtl['map_Kd'])
+            image = pygame.image.tostring(surf, 'RGBA', 1)
+            ix, iy = surf.get_rect().size
+            texid = mtl['texture_Kd'] = glGenTextures(1)
+            glBindTexture(GL_TEXTURE_2D, texid)
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+                GL_LINEAR)
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,
+                GL_LINEAR)
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, ix, iy, 0, GL_RGBA,
+                GL_UNSIGNED_BYTE, image)
+        else:
+            mtl[values[0]] = map(float, values[1:])
+    return contents
+
+class OBJ:
+    def __init__(self, filename, swapyz=False):
+        """Loads a Wavefront OBJ file. """
+        self.filename = filename
+        self.vertices = []
+        self.normals = []
+        self.texcoords = []
+        self.faces = []
+        self.mtl = None
+
+        material = None
+        path = os.path.split(filename)[0]
+        use_texture = False
+        for line in open(filename, "r"):
+            if line.startswith('#'): continue
+            values = line.split()
+            if not values: continue
+            if values[0] == 'v':
+                v = map(float, values[1:4])
+                if swapyz:
+                    v = v[0], v[2], v[1]
+                self.vertices.append(v)
+            elif values[0] == 'vn':
+                v = map(float, values[1:4])
+                if swapyz:
+                    v = v[0], v[2], v[1]
+                self.normals.append(v)
+            elif values[0] == 'vt':
+                self.texcoords.append(map(float, values[1:3]))
+            elif values[0] in ('usemtl', 'usemat'):
+                material = values[1]
+            elif values[0] == 'mtllib':
+                self.mtl = MTL(os.path.join(path, values[1]))
+                for name in self.mtl:
+                    if 'texture_Kd' in self.mtl[name]:
+                        use_texture = True
+            elif values[0] == 'f':
+                face = []
+                texcoords = []
+                norms = []
+                for v in values[1:]:
+                    w = v.split('/')
+                    vert = int(w[0])
+                    if vert < 0:
+                        # refers to -ve indexed verts defined up to this
+                        # point
+                        vert += len(self.vertices)
+                    face.append(vert)
+                    if len(w) >= 2 and len(w[1]) > 0:
+                        texcoords.append(int(w[1]))
+                    else:
+                        texcoords.append(0)
+                    if len(w) >= 3 and len(w[2]) > 0:
+                        norms.append(int(w[2]))
+                    else:
+                        norms.append(0)
+                self.faces.append((face, norms, texcoords, material))
+            else:
+                #print 'UNHANDLED', values
+                continue
+
+        self.gl_list = glGenLists(1)
+        glNewList(self.gl_list, GL_COMPILE)
+        if use_texture: glEnable(GL_TEXTURE_2D)
+        glFrontFace(GL_CCW)
+
+        for face in self.faces:
+            vertices, normals, texture_coords, material = face
+
+            if material:
+                mtl = self.mtl[material]
+                if 'texture_Kd' in mtl:
+                    # use diffuse texmap
+                    glBindTexture(GL_TEXTURE_2D, mtl['texture_Kd'])
+                else:
+                    # just use diffuse colour
+                    glColor3f(*mtl['Kd'])
+            else:
+                glColor4f(1., 1., 1., 1.)
+
+            glBegin(GL_POLYGON)
+            for i in range(0, len(vertices)):
+                if normals[i]:
+                    glNormal3f(*self.normals[normals[i] - 1])
+                if texture_coords[i]:
+                    glTexCoord2f(*self.texcoords[texture_coords[i] - 1])
+                glVertex3f(*self.vertices[vertices[i] - 1])
+            glEnd()
+        if use_texture: glDisable(GL_TEXTURE_2D)
+        glEndList()
+
+    def __repr__(self):
+        return '<OBJ %r>'%self.filename
+
---
+++ tests/obj_test.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+'''
+'''
+
+__docformat__ = 'restructuredtext'
+__version__ = '$Id$'
+
+import os
+import ctypes
+import pyglet.window
+from pyglet.window.event import *
+
+from pyglet.GL.VERSION_1_1 import *
+from pyglet.GLU.VERSION_1_1 import *
+from pyglet import clock
+from pyglet.model import obj
+
+factory = pyglet.window.WindowFactory()
+factory.config._attributes['doublebuffer'] = 1
+w1 = factory.create(width=200, height=200)
+
+class ExitHandler(object):
+    running = True
+    def on_close(self):
+        self.running = False
+    def on_keypress(self, symbol, modifiers):
+        if symbol == pyglet.window.key.K_ESCAPE:
+            self.running = False
+        return EVENT_UNHANDLED
+exit_handler = ExitHandler()
+w1.push_handlers(exit_handler)
+
+c = clock.Clock()
+
+fourfv = ctypes.c_float * 4
+c_float_p = ctypes.POINTER(ctypes.c_float)
+glLightfv(GL_LIGHT0, GL_POSITION, ctypes.cast(fourfv(100, 200, 100, 0), c_float_p))
+glLightfv(GL_LIGHT0, GL_AMBIENT, ctypes.cast(fourfv(0.2, 0.2, 0.2, 1.0), c_float_p))
+glLightfv(GL_LIGHT0, GL_DIFFUSE, ctypes.cast(fourfv(0.5, 0.5, 0.5, 1.0), c_float_p))
+glEnable(GL_LIGHT0)
+glEnable(GL_LIGHTING)
+glEnable(GL_COLOR_MATERIAL)
+glEnable(GL_DEPTH_TEST)
+glShadeModel(GL_SMOOTH)
+
+glPolygonMode(GL_FRONT, GL_FILL)
+glDepthFunc(GL_LESS)
+glEnable(GL_CULL_FACE)
+glCullFace(GL_BACK)
+
+glMatrixMode(GL_PROJECTION)
+glLoadIdentity()
+gluPerspective(60., 1., 1., 100.)
+glRotatef(30, 1, 0, 0)
+glTranslatef(0, -5, -10)
+
+glMatrixMode(GL_MODELVIEW)
+glClearColor(0, 0, 0, 0)
+
+bunny = obj.OBJ(os.path.join(os.path.split(__file__)[0], 'rabbit.obj'))
+
+r = 0
+while exit_handler.running:
+    c.set_fps(60)
+    w1.dispatch_events()
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glLoadIdentity()
+
+    r += 1
+    if r > 360: r = 0
+    glRotatef(r, 0, 1, 0)
+    glCallList(bunny.gl_list)
+
+    w1.flip()
+
