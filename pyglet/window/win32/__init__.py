6bd467075bdeb82fcbbc98ac51138289f2b91b57
Alex.Holkner <Alex.Holkner@gmail.com>
2006-11-11 01:44:30 +0000

    All win32 events implemented.

--- pyglet/window/win32/__init__.py
+++ pyglet/window/win32/__init__.py
@@ -154,14 +154,37 @@ class Win32Context(BaseGLContext):
     def destroy(self):
         wglDeleteContext(self._context)
 
+_win32_event_handler_types = []
+
+def Win32EventHandler(message):
+    def handler_wrapper(f):
+        handler = (message, f.__name__)
+        _win32_event_handler_types.append(handler)
+        return f
+    return handler_wrapper
+
+class Win32Mouse(object):
+    x = 0
+    y = 0
+
 class Win32Window(BaseWindow):
     _window_class = None
     _hwnd = None
     _dc = None
     _wgl_context = None
+    _tracking = False
+    _hidden = False
 
     def __init__(self):
         super(Win32Window, self).__init__()
+
+        # Bind event handlers
+        self._event_handlers = {}
+        for message, func_name in _win32_event_handler_types:
+            func = getattr(self, func_name)
+            self._event_handlers[message] = func
+
+        self._mouse = Win32Mouse()
 
     def create(self, factory):
         super(Win32Window, self).create(factory)
@@ -169,7 +192,6 @@ class Win32Window(BaseWindow):
         width, height = factory.get_size()
         context = factory.get_context()
 
-        #handle = _kernel32.GetModuleHandleA(c_int(0))
         handle = 0
         self._window_class = WNDCLASS()
         self._window_class.lpszClassName = 'GenericAppClass'
@@ -187,7 +209,7 @@ class Win32Window(BaseWindow):
 
         self._hwnd = _user32.CreateWindowExA(0,
             self._window_class.lpszClassName,
-            'pyglet window',
+            '',
             WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN | WS_CLIPSIBLINGS,
             CW_USEDEFAULT,
             CW_USEDEFAULT,
@@ -207,17 +229,12 @@ class Win32Window(BaseWindow):
         _user32.ShowWindow(self._hwnd, SW_SHOWDEFAULT)
         _user32.UpdateWindow(self._hwnd)
 
-    def _wnd_proc(self, hwnd, msg, wParam, lParam):
-        event_dispatcher = _event_dispatchers.get(msg, None)
-        if event_dispatcher:
-            result = event_dispatcher(self, msg, wParam, lParam)
-        else:
-            result = _user32.DefWindowProcA(c_int(hwnd), c_int(msg),
-                c_int(wParam), c_int(lParam)) 
-        return result
-
     def close(self):
-        pass
+        super(Win32Window, self).close()
+        _user32.DestroyWindow(self._hwnd)
+        self._hwnd = None
+        self._dc = None
+        self._wgl_context = None
 
     def switch_to(self):
         wglMakeCurrent(self._dc, self._wgl_context)
@@ -231,6 +248,200 @@ class Win32Window(BaseWindow):
             _user32.TranslateMessage(byref(msg))
             _user32.DispatchMessageA(byref(msg))
 
+    def _wnd_proc(self, hwnd, msg, wParam, lParam):
+        event_handler = self._event_handlers.get(msg, None)
+        if event_handler:
+            result = event_handler(msg, wParam, lParam)
+        else:
+            result = _user32.DefWindowProcA(c_int(hwnd), c_int(msg),
+                c_int(wParam), c_int(lParam)) 
+        return result
+
+    # Event handlers
+
+    def _get_modifiers(self, key_lParam=0):
+        modifiers = 0
+        if _user32.GetKeyState(VK_SHIFT) & 0xff00:
+            modifiers |= MOD_SHIFT
+        if _user32.GetKeyState(VK_CONTROL) & 0xff00:
+            modifiers |= MOD_CTRL
+        if _user32.GetKeyState(VK_LWIN) & 0xff00:
+            modifiers |= MOD_WINDOWS
+        if _user32.GetKeyState(VK_CAPITAL) & 0x00ff:    # toggle
+            modifiers |= MOD_CAPSLOCK
+        if _user32.GetKeyState(VK_NUMLOCK) & 0x00ff:    # toggle
+            modifiers |= MOD_NUMLOCK
+        if key_lParam & (1 << 29):
+            modifiers |= MOD_ALT
+        return modifiers
+
+    @staticmethod
+    def _get_location(lParam):
+        x = c_int16(lParam & 0xffff).value
+        y = c_int16(lParam >> 16).value
+        return x, y
+
+    @Win32EventHandler(WM_KEYDOWN)
+    @Win32EventHandler(WM_KEYUP)
+    @Win32EventHandler(WM_SYSKEYDOWN)
+    @Win32EventHandler(WM_SYSKEYUP)
+    def _event_key(self, msg, wParam, lParam):
+        if lParam & (1 << 30):
+            if msg not in (WM_KEYUP, WM_SYSKEYUP):
+                return 0    # key repeat
+            event = EVENT_KEY_RELEASE
+        else:
+            event = EVENT_KEY_PRESS
+
+        symbol = keymap.get(wParam, None)
+        if symbol:
+            if symbol == K_LCTRL and lParam & (1 << 24):
+                symbol = K_RCTRL
+            if symbol == K_LALT and lParam & (1 << 24):
+                symbol = K_RALT
+            elif symbol == K_LSHIFT:
+                pass # TODO: some magic with getstate to find out if it's the
+                     # right or left shift key. 
+            
+            self.dispatch_event(event, symbol, self._get_modifiers(lParam))
+        return 0
+
+    @Win32EventHandler(WM_CHAR)
+    def _event_char(self, msg, wParam, lParam):
+        text = unichr(wParam)
+        if unicodedata.category(text) != 'Cc' or text == '\r':
+            self.dispatch_event(EVENT_TEXT, text)
+        return 0
+
+    @Win32EventHandler(WM_MOUSEMOVE)
+    def _event_mousemove(self, msg, wParam, lParam):
+        x, y = self._get_location(lParam)
+        if not self._tracking:
+            # There is no WM_MOUSEENTER message (!), so fake it from the
+            # first WM_MOUSEMOVE event after leaving.  Use self._tracking
+            # to determine when to recreate the tracking structure after
+            # re-entering (to track the next WM_MOUSELEAVE).
+            self.dispatch_event(EVENT_MOUSE_ENTER, x, y)
+            self._tracking = True
+            track = TRACKMOUSEEVENT()
+            track.cbSize = sizeof(track)
+            track.dwFlags = TME_LEAVE
+            track.hwndTrack = self._hwnd
+            _user32.TrackMouseEvent(byref(track))
+
+        dx = x - self._mouse.x
+        dy = y - self._mouse.y
+        self._mouse.x = x
+        self._mouse.y = y
+        self.dispatch_event(EVENT_MOUSE_MOTION, x, y, dx, dy)
+        return 0
+
+    @Win32EventHandler(WM_MOUSELEAVE)
+    def _event_mouseleave(self, msg, wParam, lParam):
+        point = POINT()
+        _user32.GetCursorPos(byref(point))
+        _user32.ScreenToClient(self._hwnd, byref(point))
+        self._tracking = False
+        self.dispatch_event(EVENT_MOUSE_LEAVE, point.x, point.y)
+        return 0
+
+    def _event_mousebutton(self, event, button, lParam):
+        x, y = self._get_location(lParam)
+        self.dispatch_event(event, button, x, y, self._get_modifiers())
+        return 0
+
+    @Win32EventHandler(WM_LBUTTONDOWN)
+    def _event_lbuttondown(self, msg, wParam, lParam):
+        return self._event_mousebutton(
+            EVENT_MOUSE_PRESS, MOUSE_LEFT_BUTTON, lParam)
+
+    @Win32EventHandler(WM_LBUTTONUP)
+    def _event_lbuttonup(self, msg, wParam, lParam):
+        return self._event_mousebutton(
+            EVENT_MOUSE_RELEASE, MOUSE_LEFT_BUTTON, lParam)
+
+    @Win32EventHandler(WM_MBUTTONDOWN)
+    def _event_mbuttondown(self, msg, wParam, lParam):
+        return self._event_mousebutton(
+            EVENT_MOUSE_PRESS, MOUSE_MIDDLE_BUTTON, lParam)
+
+    @Win32EventHandler(WM_MBUTTONUP)
+    def _event_mbuttonup(self, msg, wParam, lParam):
+        return self._event_mousebutton(
+            EVENT_MOUSE_RELEASE, MOUSE_MIDDLE_BUTTON, lParam)
+
+    @Win32EventHandler(WM_RBUTTONDOWN)
+    def _event_rbuttondown(self, msg, wParam, lParam):
+        return self._event_mousebutton(
+            EVENT_MOUSE_PRESS, MOUSE_RIGHT_BUTTON, lParam)
+
+    @Win32EventHandler(WM_RBUTTONUP)
+    def _event_rbuttonup(self, msg, wParam, lParam):
+        return self._event_mousebutton(
+            EVENT_MOUSE_RELEASE, MOUSE_RIGHT_BUTTON, lParam)
+
+    @Win32EventHandler(WM_MOUSEWHEEL)
+    def _event_mousewheel(self, msg, wParam, lParam):
+        delta = c_short(wParam >> 16).value
+        self.dispatch_event(EVENT_MOUSE_SCROLL, 0, float(delta) / WHEEL_DELTA)
+        return 0
+
+    @Win32EventHandler(WM_CLOSE)
+    def _event_close(self, msg, wParam, lParam):
+        self.dispatch_event(EVENT_CLOSE)
+        return 0
+
+    @Win32EventHandler(WM_PAINT)
+    def _event_paint(self, msg, wParam, lParam):
+        self.dispatch_event(EVENT_EXPOSE)
+        _user32.ValidateRect(self._hwnd, c_void_p())
+        return 0
+
+    @Win32EventHandler(WM_SIZE)
+    def _event_size(self, msg, wParam, lParam):
+        if wParam == SIZE_MINIMIZED:
+            # Minimized, not resized.
+            self._hidden = True
+            self.dispatch_event(EVENT_HIDE)
+            return 0
+        if self._hidden:
+            # Restored
+            self._hidden = False
+            self.dispatch_event(EVENT_SHOW)
+        w, h = self._get_location(lParam)
+        self.dispatch_event(EVENT_RESIZE, w, h)
+        return 0
+
+    @Win32EventHandler(WM_MOVE)
+    def _event_move(self, msg, wParam, lParam):
+        x, y = self._get_location(lParam)
+        self.dispatch_event(EVENT_MOVE, x, y)
+        return 0
+
+    '''
+    # Alternative to using WM_SETFOCUS and WM_KILLFOCUS.  Which
+    # is better?
+
+    @Win32EventHandler(WM_ACTIVATE)
+    def _event_activate(self, msg, wParam, lParam):
+        if wParam & 0xffff == WA_INACTIVE:
+            self.dispatch_event(EVENT_DEACTIVATE)
+        else:
+            self.dispatch_event(EVENT_ACTIVATE)
+            _user32.SetFocus(self._hwnd)
+        return 0
+    '''
+
+    @Win32EventHandler(WM_SETFOCUS)
+    def _event_setfocus(self, msg, wParam, lParam):
+        self.dispatch_event(EVENT_ACTIVATE)
+        return 0
+
+    @Win32EventHandler(WM_KILLFOCUS)
+    def _event_killfocus(self, msg, wParam, lParam):
+        self.dispatch_event(EVENT_DEACTIVATE)
+        return 0
+
 def _check():
     dw = _kernel32.GetLastError()
     if dw != 0:
@@ -243,52 +454,3 @@ def _check():
                               len(msg),
                               c_void_p())
         raise Win32Exception(msg.value)
-
-def _dispatch_key(window, msg, wParam, lParam):
-    if lParam & (1 << 30):
-        if msg not in (WM_KEYUP, WM_SYSKEYUP):
-            return 0    # key repeat
-        event = EVENT_KEY_RELEASE
-    else:
-        event = EVENT_KEY_PRESS
-
-    symbol = keymap.get(wParam, None)
-    if symbol:
-        if symbol == K_LCTRL and lParam & (1 << 24):
-            symbol = K_RCTRL
-        if symbol == K_LALT and lParam & (1 << 24):
-            symbol = K_RALT
-        elif symbol == K_LSHIFT:
-            pass # TODO: some magic with getstate to find out if it's the
-                 # right or left shift key. 
-
-        modifiers = 0
-        if _user32.GetKeyState(VK_SHIFT) & 0xff00:
-            modifiers |= MOD_SHIFT
-        if _user32.GetKeyState(VK_CONTROL) & 0xff00:
-            modifiers |= MOD_CTRL
-        if _user32.GetKeyState(VK_LWIN) & 0xff00:
-            modifiers |= MOD_WINDOWS
-        if _user32.GetKeyState(VK_CAPITAL) & 0x00ff:    # toggle
-            modifiers |= MOD_CAPSLOCK
-        if _user32.GetKeyState(VK_NUMLOCK) & 0x00ff:    # toggle
-            modifiers |= MOD_NUMLOCK
-        if lParam & (1 << 29):
-            modifiers |= MOD_ALT
-
-        window.dispatch_event(event, symbol, modifiers)
-    return 0
-
-def _dispatch_char(window, msg, wParam, lParam):
-    text = unichr(wParam)
-    if unicodedata.category(text) != 'Cc' or text == '\r':
-        window.dispatch_event(EVENT_TEXT, text)
-    return 0
-
-_event_dispatchers = {
-    WM_KEYDOWN: _dispatch_key,
-    WM_KEYUP: _dispatch_key,
-    WM_SYSKEYDOWN: _dispatch_key,
-    WM_SYSKEYUP: _dispatch_key,
-    WM_CHAR: _dispatch_char,
-}
