be07f755dcba3106f21facea33b7957656f69198
Alex.Holkner <Alex.Holkner@gmail.com>
2007-01-03 07:33:15 +0000

    ctypes version check

--- pyglet/__init__.py
+++ pyglet/__init__.py
@@ -6,3 +6,11 @@
 __docformat__ = 'restructuredtext'
 __version__ = '$Id$'
 
+def _require_ctypes_version(version):
+    # Check ctypes version
+    import ctypes
+    req = [int(i) for i in version.split('.')]
+    have = [int(i) for i in ctypes.__version__.split('.')]
+    if not tuple(have) >= tuple(req):
+        raise ImportError('pyglet requires ctypes %s or later.' % version)
+_require_ctypes_version('1.0.0')
