2e743c365ae1d72ee0fc32fcab97042902102109
Alex.Holkner <Alex.Holkner@gmail.com>
2007-02-08 13:46:24 +0000

    Remove old WGL module

--- pyglet/GL/WGL/EXT_extensions_string.py
+++
@@ -1,8 +0,0 @@
-'''XXX hand-coded from
-
-http://oss.sgi.com/projects/ogl-sample/registry/EXT/wgl_extensions_string.txt
-'''
-
-import ctypes as _ctypes
-from pyglet.GL import get_function as _get_function
-wglGetExtensionsStringEXT = _get_function('wglGetExtensionsStringEXT', [], _ctypes.c_char_p)
--- pyglet/GL/WGL/EXT_swap_control.py
+++
@@ -1,9 +0,0 @@
-'''XXX hand-coded from
-
-http://oss.sgi.com/projects/ogl-sample/registry/EXT/wgl_swap_control.txt
-'''
-
-import ctypes as _ctypes
-from pyglet.GL import get_function as _get_function
-wglSwapIntervalEXT = _get_function('wglSwapIntervalEXT', [_ctypes.c_int], _ctypes.c_int)
-wglGetSwapIntervalEXT = _get_function('wglGetSwapIntervalEXT', [], _ctypes.c_int)
