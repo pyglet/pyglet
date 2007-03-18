a7300cca041dbcfc00800c9ae35782712c768eee
r1chardj0n3s <devnull@localhost>
2007-03-18 01:44:08 +0000

    Start of a simple SVG path renderer example. Lots of work still to do.

---
+++ examples/hello_world.svg
@@ -0,0 +1,113 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<!-- Created with Inkscape (http://www.inkscape.org/) -->
+<svg
+   xmlns:dc="http://purl.org/dc/elements/1.1/"
+   xmlns:cc="http://web.resource.org/cc/"
+   xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+   xmlns:svg="http://www.w3.org/2000/svg"
+   xmlns="http://www.w3.org/2000/svg"
+   xmlns:sodipodi="http://inkscape.sourceforge.net/DTD/sodipodi-0.dtd"
+   xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+   width="744.09448819"
+   height="1052.3622047"
+   id="svg2"
+   sodipodi:version="0.32"
+   inkscape:version="0.43"
+   sodipodi:docbase="/home/richard/src/pyglet.googlecode.com/trunk/examples"
+   sodipodi:docname="hello_world.svg">
+  <defs
+     id="defs4" />
+  <sodipodi:namedview
+     id="base"
+     pagecolor="#ffffff"
+     bordercolor="#666666"
+     borderopacity="1.0"
+     inkscape:pageopacity="0.0"
+     inkscape:pageshadow="2"
+     inkscape:zoom="0.98994949"
+     inkscape:cx="375"
+     inkscape:cy="479.5939"
+     inkscape:document-units="px"
+     inkscape:current-layer="layer1"
+     inkscape:window-width="751"
+     inkscape:window-height="540"
+     inkscape:window-x="674"
+     inkscape:window-y="432" />
+  <metadata
+     id="metadata7">
+    <rdf:RDF>
+      <cc:Work
+         rdf:about="">
+        <dc:format>image/svg+xml</dc:format>
+        <dc:type
+           rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
+      </cc:Work>
+    </rdf:RDF>
+  </metadata>
+  <g
+     inkscape:label="Layer 1"
+     inkscape:groupmode="layer"
+     id="layer1">
+    <path
+       style="fill:none;fill-opacity:0.75;fill-rule:evenodd;stroke:#000000;stroke-width:0.25pt;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
+       d="M 91.428571,449.50504 L 88.571429,569.50504"
+       id="path1307" />
+    <path
+       style="fill:none;fill-opacity:0.75;fill-rule:evenodd;stroke:#000000;stroke-width:0.25pt;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
+       d="M 88.571429,509.50504 L 128.57143,509.50504"
+       id="path1309" />
+    <path
+       style="fill:none;fill-opacity:0.75;fill-rule:evenodd;stroke:#000000;stroke-width:0.25pt;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
+       d="M 131.42857,458.07647 L 134.28571,569.50504"
+       id="path1311" />
+    <path
+       style="fill:none;fill-opacity:0.75;fill-rule:evenodd;stroke:#000000;stroke-width:0.25pt;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
+       d="M 178.797,541.225 C 186.62568,541.47754 203.0394,539.1795 203.04066,528.09302 C 203.04192,518.0167 195.21198,510.66789 181.82746,510.92042 C 169.45272,511.1539 155.77084,514.46637 154.55334,535.16408 C 153.33583,555.86179 161.6244,566.98389 182.83761,567.48896 C 204.05081,567.99404 204.55589,562.18566 208.09143,560.4179"
+       id="path1315"
+       sodipodi:nodetypes="czzzzz" />
+    <path
+       style="fill:none;fill-opacity:0.75;fill-rule:evenodd;stroke:#000000;stroke-width:0.25pt;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
+       d="M 227.28432,465.46355 L 228.29447,566.4788"
+       id="path1317" />
+    <path
+       style="fill:none;fill-opacity:0.75;fill-rule:evenodd;stroke:#000000;stroke-width:0.25pt;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
+       d="M 250.51783,459.40264 L 254.55844,571.52957"
+       id="path1319" />
+    <path
+       style="fill:none;fill-opacity:0.75;fill-rule:evenodd;stroke:#000000;stroke-width:0.25pt;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
+       d="M 293.95439,513.95088 C 260.11683,512.88971 257.59498,564.15067 294.96454,565.46866 C 332.31215,566.78587 327.77536,515.01153 293.95439,513.95088 z "
+       id="path1321"
+       sodipodi:nodetypes="czz" />
+    <path
+       style="fill:none;fill-opacity:0.75;fill-rule:evenodd;stroke:#000000;stroke-width:0.25pt;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
+       d="M 97.984794,595.77324 C 97.984794,595.77324 84.342094,676.73832 112.12693,677.59559 C 139.91176,678.45286 137.38074,661.43316 137.38074,661.43316 C 137.38074,661.43316 132.97647,675.5753 165.66502,675.57529 C 198.35357,675.57528 183.84776,595.77324 183.84776,595.77324"
+       id="path1323"
+       sodipodi:nodetypes="czczc" />
+    <path
+       style="fill:none;fill-opacity:0.75;fill-rule:evenodd;stroke:#000000;stroke-width:0.25pt;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
+       d="M 271.73103,674.56513 L 270.72088,632.13872"
+       id="path1327" />
+    <path
+       style="fill:none;fill-opacity:0.75;fill-rule:evenodd;stroke:#000000;stroke-width:0.25pt;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
+       d="M 271.73103,647.29101 C 271.73103,647.29101 274.76149,631.12857 291.93408,632.13872 C 309.10668,633.14887 310.11683,645.27071 310.11683,645.27071"
+       id="path1329" />
+    <path
+       style="fill:none;fill-opacity:0.75;fill-rule:evenodd;stroke:#000000;stroke-width:0.27549551pt;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
+       d="M 324.25896,594.79658 L 325.26912,674.53164"
+       id="path1331" />
+    <path
+       style="fill:none;fill-opacity:0.75;fill-rule:evenodd;stroke:#000000;stroke-width:0.25pt;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
+       d="M 369.71583,651.33163 C 369.71583,631.63366 336.38079,619.00675 336.38079,650.32147 C 336.38079,680.62605 369.71583,672.06438 369.71583,651.33163 z "
+       id="path1333"
+       sodipodi:nodetypes="czz" />
+    <path
+       style="fill:none;fill-opacity:0.75;fill-rule:evenodd;stroke:#000000;stroke-width:0.25pt;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
+       d="M 371.73613,600.824 L 368.70567,673.55498"
+       id="path1335" />
+    <path
+       style="fill:none;fill-opacity:0.75;fill-rule:evenodd;stroke:#000000;stroke-width:0.25pt;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
+       d="M 230.80836,625.56851 C 196.9708,624.50734 194.44895,675.7683 231.81851,677.08629 C 269.16612,678.4035 264.62933,626.62916 230.80836,625.56851 z "
+       id="path1337"
+       sodipodi:nodetypes="czz" />
+  </g>
+</svg>
---
+++ examples/svg_test.py
@@ -0,0 +1,314 @@
+#!/usr/bin/env python
+# -*- coding: latin-1 -*-
+
+'''
+'''
+
+__docformat__ = 'restructuredtext'
+__version__ = '$Id$'
+
+import math
+import random
+import re
+import os.path
+from pyglet.window import Window
+from pyglet import clock
+from pyglet.window.event import *
+from pyglet.euclid import *
+from pyglet.gl import *
+import xml.dom
+import xml.dom.minidom
+
+# sample from http://www.w3.org/TR/SVG/paths.html#PathDataCubicBezierCommands
+sample = 'M100,200 C100,100 250,100 250,200 S400,300 400,200'
+
+class Curve:
+    PATH_RE = re.compile(r'([MLHVCSQTAZ])([^MLHVCSQTAZ]+)', re.IGNORECASE)
+    INT = r'([+-]?\d+)'
+    FLOAT = r'(?:[\s,]*)([+-]?\d+(?:\.\d+)?)'
+
+    HANDLERS = {}
+    def handle(char, rx, types, HANDLERS=HANDLERS):
+        def register(function):
+            HANDLERS[char] = (rx and re.compile(rx), function, types)
+            return function
+        return register
+
+    def __init__(self, spec):
+        self.start = None
+        self.current = None
+
+        self.gl_list = glGenLists(1)
+        glNewList(self.gl_list, GL_COMPILE)
+
+        for cmd, value in self.PATH_RE.findall(spec):
+#            print (cmd, value)
+            if not cmd: continue
+            rx, handler, types = self.HANDLERS[cmd.upper()]
+            if rx is None:
+                handler(self, cmd)
+            else:
+                v = []
+                for fields in rx.findall(value):
+                    v.append([types[i](e) for i, e in enumerate(fields)])
+                handler(self, cmd, v)
+        glEndList()
+
+    def draw(self):
+        glCallList(self.gl_list)
+
+    @handle('M', FLOAT*2, (float, float))
+    def moveto(self, cmd, points):
+        '''Start a new sub-path at the given (x,y) coordinate. M (uppercase)
+        indicates that absolute coordinates will follow; m (lowercase)
+        indicates that relative coordinates will follow. If a relative moveto
+        (m) appears as the first element of the path, then it is treated as a
+        pair of absolute coordinates. If a moveto is followed by multiple pairs
+        of coordinates, the subsequent pairs are treated as implicit lineto
+        commands.
+
+        Parameters are (x y)+
+        '''
+        points = [map(float, point) for point in points]
+        # XXX handle relative
+        # XXX confirm that we always reset start here
+        self.start = self.current = points[0]
+        if len(points) > 2:
+            self.lineto({'m': 'l', 'M': 'L'}[cmd], points[1:])
+
+
+    @handle('L', FLOAT*2, (float, float))
+    def lineto(self, cmd, points):
+        '''Draw a line from the current point to the given (x,y) coordinate
+        which becomes the new current point. L (uppercase) indicates that
+        absolute coordinates will follow; l (lowercase) indicates that relative
+        coordinates will follow. A number of coordinates pairs may be specified
+        to draw a polyline. At the end of the command, the new current point is
+        set to the final set of coordinates provided.
+
+        Parameters are (x y)+
+        '''
+        glBegin(GL_LINE_STRIP)
+        for point in points:
+            cx, cy = self.current
+            x, y = map(float, point)
+            glVertex2f(cx, cy)
+            glVertex2f(x, y)
+            self.current = (x, y)
+        glEnd()
+
+
+    @handle('H', FLOAT, (float,))
+    def horizontal_lineto(self, cmd, xvals):
+        '''Draws a horizontal line from the current point (cpx, cpy) to (x,
+        cpy). H (uppercase) indicates that absolute coordinates will follow; h
+        (lowercase) indicates that relative coordinates will follow. Multiple x
+        values can be provided (although usually this doesn't make sense). At
+        the end of the command, the new current point becomes (x, cpy) for the
+        final value of x.
+
+        Parameters are x+
+        '''
+        glBegin(GL_LINE_STRIP)
+        cx, cy = self.current
+        x = float(xvals[-1])
+        glVertex2f(cx, cy)
+        glVertex2f(x, cy)
+        self.current = (x, cy)
+        glEnd()
+
+
+    @handle('V', FLOAT, (float,))
+    def vertical_lineto(self, cmd, yvals):
+        '''Draws a vertical line from the current point (cpx, cpy) to (cpx, y).
+        V (uppercase) indicates that absolute coordinates will follow; v
+        (lowercase) indicates that relative coordinates will follow. Multiple y
+        values can be provided (although usually this doesn't make sense). At
+        the end of the command, the new current point becomes (cpx, y) for the
+        final value of y.
+
+        Parameters are y+
+        '''
+        glBegin(GL_LINE_STRIP)
+        cx, cy = self.current
+        y = float(yvals[-1])
+        glVertex2f(cx, cy)
+        glVertex2f(cx, y)
+        self.current = (cx, y)
+        glEnd()
+
+
+    @handle('Z', None, None)
+    def closepath(self, cmd):
+        '''Close the current subpath by drawing a straight line from the
+        current point to current subpath's initial point.
+        '''
+        glBegin(GL_LINE_STRIP)
+        glVertex2f(*self.current)
+        glVertex2f(*self.start)
+        glEnd()
+
+
+    @handle('C', FLOAT*6, (float, )*6)
+    def curveto(self, cmd, control_points):
+        '''Draws a cubic Bézier curve from the current point to (x,y) using
+        (x1,y1) as the control point at the beginning of the curve and (x2,y2)
+        as the control point at the end of the curve. C (uppercase) indicates
+        that absolute coordinates will follow; c (lowercase) indicates that
+        relative coordinates will follow. Multiple sets of coordinates may be
+        specified to draw a polybézier. At the end of the command, the new
+        current point becomes the final (x,y) coordinate pair used in the
+        polybézier.
+
+        Control points are (x1 y1 x2 y2 x y)+
+        '''
+        glBegin(GL_LINE_STRIP)
+        for entry in control_points:
+            x1, y1, x2, y2, x, y = map(float, entry)
+            t = 0
+            cx, cy = self.current
+            self.last_control = (x2, y2)
+            self.current = (x, y)
+            x1 *= 3; x2 *= 3
+            y1 *= 3; y2 *= 3
+            while t <= 1.01:
+                a = t; a2 = a**2; a3 = a**3
+                b = 1 - t; b2 = b**2; b3 = b**3
+                px = cx*b3 + x1*b2*a + x2*b*a2 + x*a3
+                py = cy*b3 + y1*b2*a + y2*b*a2 + y*a3
+                glVertex2f(px, py)
+                t += 0.01
+        glEnd()
+
+
+    @handle('S', FLOAT*4, (float, )*4)
+    def smooth_curveto(self, cmd, control_points):
+        '''Draws a cubic Bézier curve from the current point to (x,y). The
+        first control point is assumed to be the reflection of the second
+        control point on the previous command relative to the current point.
+        (If there is no previous command or if the previous command was not an
+        C, c, S or s, assume the first control point is coincident with the
+        current point.) (x2,y2) is the second control point (i.e., the control
+        point at the end of the curve). S (uppercase) indicates that absolute
+        coordinates will follow; s (lowercase) indicates that relative
+        coordinates will follow. Multiple sets of coordinates may be specified
+        to draw a polybézier. At the end of the command, the new current point
+        becomes the final (x,y) coordinate pair used in the polybézier.
+
+        Control points are (x2 y2 x y)+
+        '''
+        assert self.last_control is not None, 'S must follow S or C'
+
+        glBegin(GL_LINE_STRIP)
+        for entry in control_points:
+            x2, y2, x, y = map(float, entry)
+
+            # Reflect last control point
+            cx, cy = self.current; lcx, lcy = self.last_control
+            dx, dy = cx - lcx, cy - lcy
+            x1, y1 = cx + dx, cy + dy
+
+            t = 0
+            cx, cy = self.current
+            self.last_control = (x2, y2)
+            self.current = (x, y)
+
+            x1 *= 3; x2 *= 3
+            y1 *= 3; y2 *= 3
+            while t <= 1.01:
+                a = t; a2 = a**2; a3 = a**3
+                b = 1 - t; b2 = b**2; b3 = b**3
+                px = cx*b3 + x1*b2*a + x2*b*a2 + x*a3
+                py = cy*b3 + y1*b2*a + y2*b*a2 + y*a3
+                glVertex2f(px, py)
+                t += 0.01
+        glEnd()
+
+
+    @handle('Q', FLOAT*4, (float, )*4)
+    def quadratic_curveto(self, cmd, control_points):
+        '''Draws a quadratic Bézier curve from the current point to (x,y)
+        using (x1,y1) as the control point. Q (uppercase) indicates that
+        absolute coordinates will follow; q (lowercase) indicates that
+        relative coordinates will follow. Multiple sets of coordinates may
+        be specified to draw a polybézier. At the end of the command, the
+        new current point becomes the final (x,y) coordinate pair used in
+        the polybézier.
+
+        Control points are (x1 y1 x y)+
+        '''
+        raise NotImplementedError('not implemented')
+
+
+    @handle('T', FLOAT*2, (float, )*2)
+    def smooth_quadratic_curveto(self, cmd, control_points):
+        '''Draws a quadratic Bézier curve from the current point to (x,y).
+        The control point is assumed to be the reflection of the control
+        point on the previous command relative to the current point. (If
+        there is no previous command or if the previous command was not a
+        Q, q, T or t, assume the control point is coincident with the
+        current point.) T (uppercase) indicates that absolute coordinates
+        will follow; t (lowercase) indicates that relative coordinates will
+        follow. At the end of the command, the new current point becomes
+        the final (x,y) coordinate pair used in the polybézier.
+
+        Control points are (x y)+
+        '''
+        raise NotImplementedError('not implemented')
+
+
+    @handle('A', FLOAT*3 + INT*2 + FLOAT*2, (float,)*3 + (int,)*2 + (float,)*2)
+    def elliptical_arc(self, cmd, parameters):
+        '''Draws an elliptical arc from the current point to (x, y). The
+        size and orientation of the ellipse are defined by two radii (rx,
+        ry) and an x-axis-rotation, which indicates how the ellipse as a
+        whole is rotated relative to the current coordinate system. The
+        center (cx, cy) of the ellipse is calculated automatically to
+        satisfy the constraints imposed by the other parameters.
+        large-arc-flag and sweep-flag contribute to the automatic
+        calculations and help determine how the arc is drawn.
+
+        Parameters are (rx ry x-axis-rotation large-arc-flag sweep-flag x y)+
+        '''
+        raise NotImplementedError('not implemented')
+
+
+class SVG:
+    def __init__(self, filename):
+        dom = xml.dom.minidom.parse(filename)
+        tag = dom.documentElement
+        if tag.tagName != 'svg':
+            raise ValueError('document is <%s> instead of <svg>'%tag.tagName)
+
+        self.objects = []
+        for tag in tag.getElementsByTagName('g'):
+            for tag in tag.getElementsByTagName('path'):
+                self.objects.append(Curve(tag.getAttribute('d')))
+
+    def draw(self):
+        glPushMatrix()
+        glTranslatef(0, 1024, 0)
+        glScalef(1, -1, 1)
+        for object in self.objects:
+            object.draw()
+        glPopMatrix()
+
+w = Window(width=1024, height=768)
+
+# XXX move this into display list
+glEnable(GL_LINE_SMOOTH)
+glEnable(GL_BLEND)
+glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+glHint(GL_LINE_SMOOTH_HINT, GL_DONT_CARE)
+
+dirname = os.path.dirname(__file__)
+svg = SVG(os.path.join(dirname, 'hello_world.svg'))
+
+clock.set_fps_limit(10)
+while not w.has_exit:
+    clock.tick()
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    w.dispatch_events()
+    svg.draw()
+    w.flip()
+
