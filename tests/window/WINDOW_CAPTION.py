6401a5d11d210804202ada3558d5526089763b3a
Alex.Holkner <Alex.Holkner@gmail.com>
2007-04-12 11:07:00 +0000

    Make windows bigger in this test so the captions can be read.
    Index: tests/window/WINDOW_CAPTION.py
    ===================================================================
    --- tests/window/WINDOW_CAPTION.py	(revision 777)
    +++ tests/window/WINDOW_CAPTION.py	(working copy)
    @@ -19,8 +19,8 @@
    
     class WINDOW_CAPTION(unittest.TestCase):
         def test_caption(self):
    -        w1 = window.Window(200, 200)
    -        w2 = window.Window(200, 200)
    +        w1 = window.Window(400, 200, resizable=True)
    +        w2 = window.Window(400, 200, resizable=True)
             count = 1
             w1.set_caption('Window caption %d' % count)
             w2.set_caption(u'\u00bfHabla espa\u00f1ol?')

--- tests/window/WINDOW_CAPTION.py
+++ tests/window/WINDOW_CAPTION.py
@@ -19,8 +19,8 @@ from pyglet import window
 
 class WINDOW_CAPTION(unittest.TestCase):
     def test_caption(self):
-        w1 = window.Window(200, 200)
-        w2 = window.Window(200, 200)
+        w1 = window.Window(400, 200, resizable=True)
+        w2 = window.Window(400, 200, resizable=True)
         count = 1
         w1.set_caption('Window caption %d' % count)
         w2.set_caption(u'\u00bfHabla espa\u00f1ol?')
