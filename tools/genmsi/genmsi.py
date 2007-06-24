a2f6d402635d4655dde8309398311a2ecb1f32c5
Alex.Holkner <Alex.Holkner@gmail.com>
2007-06-24 13:21:13 +0000

    Windows installer now handles multiple Python instances in the same way as OS X.. each python version is a separate installable feature, they can be added/removed at any time by the installer.. very cool.  UI needs some fixing, byte compilation needs to be added back in.

--- tools/genmsi/genmsi.py
+++ tools/genmsi/genmsi.py
@@ -9,6 +9,20 @@ import subprocess
 from uuid import uuid1
 from xml.dom.minidom import parse
 
+class PythonVersion(object):
+    def __init__(self, version):
+        self.version = version
+        self.id = 'PY' + version.replace('.', '')
+        self.key = r'SOFTWARE\Python\PythonCore\%s\InstallPath' % version
+        self.dir_prop = 'PYTHONHOME%s' % self.id
+        self.components = []
+PYTHON_VERSIONS = (
+    PythonVersion('2.4'),
+    PythonVersion('2.5'),
+)
+MISSING_PYTHON_MESSAGE = 'pyglet requires Python 2.4 or later.  The ' \
+                         'installation will be aborted.'
+
 exclude_packages = ['ext']
 
 ids = set()
@@ -37,7 +51,7 @@ def node(doc, node_name, **kwargs):
         node.setAttribute(key, value)
     return node
 
-def add_package(name, src_dir, doc, dest_node):
+def add_package(name, src_dir, doc, dest_node, pyver):
     if name in exclude_packages:
         return
 
@@ -45,8 +59,7 @@ def add_package(name, src_dir, doc, dest_node):
 
     directory = node(doc, 'Directory',
         Id=id('%sDir' % name),
-        Name='%s.dir' % name[:8],
-        LongName=name)
+        Name=name)
     dest_node.appendChild(doc.createTextNode('\n\n'))
     dest_node.appendChild(directory)
     dest_node.appendChild(doc.createTextNode('\n\n'))
@@ -56,14 +69,13 @@ def add_package(name, src_dir, doc, dest_node):
         file_path = os.path.join(src_path, filename)
         if os.path.isdir(file_path):
             if os.path.exists(os.path.join(file_path, '__init__.py')):
-                add_package(filename, src_path, doc, directory)
+                add_package(filename, src_path, doc, directory, pyver)
         elif filename.endswith('.py'):
-            add_module(filename, src_path, doc, directory)
+            add_module(filename, src_path, doc, directory, pyver)
 
-components = []
-def component_id(name):
+def component_id(name, pyver):
     component = id(name)
-    components.append(component)
+    pyver.components.append(component)
     return component
 
 guid_seq = 0
@@ -72,32 +84,29 @@ def guid():
     guid_seq += 1
     return uuid1(clock_seq=guid_seq).hex.upper()
 
-def add_module(name, src_dir, doc, dest_node):
+def add_module(name, src_dir, doc, dest_node, pyver):
     src_path = os.path.join(src_dir, name)
     basefile = os.path.splitext(name)[0]
 
     component = node(doc, 'Component',
-        Id= component_id('%sComponent' % basefile),
+        Id= component_id('%sComponent' % basefile, pyver),
         Guid=guid())
 
     component.appendChild(
         node(doc, 'File',
              Id=id('%sPy' % basefile),
-             Name=shortname(basefile, 'py'),
-             LongName=name,
+             Name=name,
              DiskId='1',
              Source=src_path))
     component.appendChild(
         node(doc, 'RemoveFile',
              Id=id('%sPyc' % basefile),
-             Name=shortname(basefile, 'pyc'),
-             LongName='%s.pyc' % basefile,
+             Name='%s.pyc' % basefile,
              On='uninstall'))
     component.appendChild(
         node(doc, 'RemoveFile',
              Id=id('%sPyo' % basefile),
-             Name=shortname(basefile, 'pyo'),
-             LongName='%s.pyo' % basefile,
+             Name='%s.pyo' % basefile,
              On='uninstall'))
     dest_node.appendChild(component)
 
@@ -125,24 +134,71 @@ if __name__ == '__main__':
             version = match.groups()[0]
     print 'Writing pyglet.wxs'
 
-    # Open template wxs
+    # Open template wxs and find Product element
     wxs = parse(os.path.join(script_dir, 'pyglet.in.wxs'))
+    Product = wxs.getElementsByTagName('Product')[0]
+
+    # Add Python discovery
+    for pyver in PYTHON_VERSIONS:
+        Property = node(wxs, 'Property', 
+                        Id=pyver.dir_prop)
+        Property.appendChild(
+            node(wxs, 'RegistrySearch',
+                 Id='%sRegSearch' % pyver.dir_prop,
+                 Root='HKLM',
+                 Key=pyver.key,
+                 Type='directory'))
+        Product.appendChild(Property) 
     
-    # Find site-packages Directory element
+    # Add install conditional on at least one Python version present.
+    Condition = node(wxs, 'Condition',
+                     Message=MISSING_PYTHON_MESSAGE)
+    Condition.appendChild(wxs.createTextNode(
+        ' or '.join([pyver.dir_prop for pyver in PYTHON_VERSIONS])))
+    Product.appendChild(Condition)
+
+    
+    # Get TARGETDIR Directory element
     for elem in wxs.getElementsByTagName('Directory'):
-        if elem.getAttribute('LongName') == 'site-packages':
-            site_packages = elem
+        if elem.getAttribute('Id') == 'TARGETDIR':
+            target_dir = elem
             break
        
-    # Find all modules and add components to wxs document
-    add_package('pyglet', root_dir, wxs, site_packages)
+    # Create entire set of components for each python version (WiX 3 will
+    # ensure only one copy of the source file is in the archive)
+    for pyver in PYTHON_VERSIONS:
+        python_home = node(wxs, 'Directory', 
+                           Id=pyver.dir_prop)
+        target_dir.appendChild(python_home)
 
-    # Add all components to feature
-    feature = wxs.getElementsByTagName('Feature')[0]
-    for component in components:
-        feature.appendChild(node(wxs, 'ComponentRef',
-                                 Id=component))
-        feature.appendChild(wxs.createTextNode('\n'))
+        lib_dir = node(wxs, 'Directory',
+                       Id='%sLibDir' % pyver.dir_prop,
+                       Name='Lib')
+        python_home.appendChild(lib_dir)
+
+        site_packages = node(wxs, 'Directory',
+                             Id='%sSitePackages' % pyver.dir_prop,
+                             Name='site-packages')
+        lib_dir.appendChild(site_packages)
+                             
+        add_package('pyglet', root_dir, wxs, site_packages, pyver)
+
+    # Add all components to features
+    RuntimeFeature = wxs.getElementsByTagName('Feature')[0]
+    for pyver in PYTHON_VERSIONS:
+        feature = node(wxs, 'Feature',
+                       Id='RuntimeFeature%s' % pyver.id,
+                       Title='pyglet runtime for Python %s' % pyver.version,
+                       Level='1')
+        condition = node(wxs, 'Condition',
+                         Level='0')
+        condition.appendChild(wxs.createTextNode('NOT ' + pyver.dir_prop))
+        feature.appendChild(condition)
+        for component in pyver.components:
+            feature.appendChild(node(wxs, 'ComponentRef',
+                                     Id=component))
+            feature.appendChild(wxs.createTextNode('\n'))
+        RuntimeFeature.appendChild(feature)
     
     # Write wxs file
     wxs.writexml(open(os.path.join(script_dir, 'pyglet.wxs'), 'w'))
@@ -152,5 +208,6 @@ if __name__ == '__main__':
                                 os.path.join(script_dir, 'pyglet.wxs')))
 
     # Link
-    call('light -out %s %s' %(os.path.join(dist_dir, 'pyglet-%s.msi' % version),
-                               os.path.join(script_dir, 'pyglet.wixobj')))
+    call('light -sval -out %s %s' % \
+        (os.path.join(dist_dir, 'pyglet-%s.msi' % version),
+         os.path.join(script_dir, 'pyglet.wixobj')))
