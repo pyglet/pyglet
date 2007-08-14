060f068f2659d5bc5cfabdb8b59e6bfead699e0c
Alex.Holkner <Alex.Holkner@gmail.com>
2007-08-14 07:07:38 +0000

    Richard reckons this is more informative.

--- tools/genmsi/genmsi.py
+++ tools/genmsi/genmsi.py
@@ -25,7 +25,7 @@ PYTHON_VERSIONS = (
     PythonVersion('2.4', 'HKLM', 'Python 2.4'),
     PythonVersion('2.5', 'HKLM', 'Python 2.5'),
     PythonVersion('2.4', 'HKCU', 'Python 2.4 (current user only)'),
-    PythonVersion('2.5', 'HKCU', 'Python 2.4 (current user only)'),
+    PythonVersion('2.5', 'HKCU', 'Python 2.5 (current user only)'),
 )
 MISSING_PYTHON_MESSAGE = 'pyglet requires Python 2.4 or later.  The ' \
                          'installation will be aborted.'
