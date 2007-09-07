3bce8ae4b8efb2dabcf86fbf9165d82db5746229
r1chardj0n3s <devnull@localhost>
2007-09-07 02:49:35 +0000

    Fix argument handling.

--- examples/image_display.py
+++ examples/image_display.py
@@ -17,7 +17,7 @@ from pyglet import window
 from pyglet import image
 
 if __name__ == '__main__':
-    if len(sys.argv) != 1:
+    if len(sys.argv) != 2:
         print __doc__
         sys.exit(1)
 
