b5c573e271008c5527cb13e872277d4cffbd972c
Alex.Holkner <Alex.Holkner@gmail.com>
2007-09-13 07:52:56 +0000

    Move website/new to website/

--- website/topnav.php
+++
@@ -1,9 +0,0 @@
-<table align="center">
-<tbody>
-<tr>
-  <td><A href="index.php"><IMG src="about.png" title="About" alt="About" width="138" height="42" align="left" border="0"></A></td>
-  <td><A href="download.php"><IMG src="download.png" title="Download" alt="Download" width="138" height="42" align="left" border="0"></A></td>
-  <td><A href="get_involved.php"><IMG src="get_involved.png" title="Get Involved" alt="Get Involved" width="138" height="42" align="left" border="0"></A></td>
-</tr>
-</tbody>
-</table>
