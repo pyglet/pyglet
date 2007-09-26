468a21e3a0bf364ac32d9e90b20b4dcee4d7293c
r1chardj0n3s <devnull@localhost>
2007-09-26 01:52:51 +0000

    I broke Alex's wrapping again.

--- tests/font/WRAP_INVARIANT.py
+++ tests/font/WRAP_INVARIANT.py
@@ -3,7 +3,8 @@
 '''Test that text will not wrap when its width is set to its calculated
 width.
 
-You should be able to clearly see "TEST TEST" on a single line.
+You should be able to clearly see "TEST TEST" on a single line (not two)
+and "SPAM SPAM SPAM" over two lines, not three.
 '''
 
 __docformat__ = 'restructuredtext'
@@ -11,17 +12,22 @@ __version__ = '$Id: $'
 
 import unittest
 import base_text
+from pyglet import font
 
 class TEST_WRAP_INVARIANT(base_text.TextTestBase):
     font_name = ''
     text = 'TEST TEST'
 
     def render(self):
-        super(TEST_WRAP_INVARIANT, self).render()
-        self.label.width = self.label.width
+        fnt = font.load('', 24) 
+        self.label1 = font.Text(fnt, 'TEST TEST', 10, 150)
+        self.label1.width = self.label1.width
+        self.label2 = font.Text(fnt, 'SPAM SPAM\nSPAM', 10, 50)
+        self.label2.width = self.label2.width
 
     def draw(self):
-        self.label.draw()
+        self.label1.draw()
+        self.label2.draw()
 
 if __name__ == '__main__':
     unittest.main()
