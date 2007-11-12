8213d308b167160fababd3e6633705955ad3ccf3
Alex.Holkner <Alex.Holkner@gmail.com>
2007-11-12 11:33:15 +0000

    Fix setup.py for cheese shop compatibility, update dist instructions to include cheese shop registration.

--- setup.py
+++ setup.py
@@ -28,12 +28,12 @@ setup_info = dict(
     long_description=long_description,
     classifiers=[
         'Development Status :: 4 - Beta',
-        'Environment :: Mac OS X',
+        'Environment :: MacOS X',
         'Environment :: Win32 (MS Windows)',
         'Environment :: X11 Applications',
         'Intended Audience :: Developers',
-        'License :: OS Approved :: BSD License',
-        'Operating System :: MacOS :: Mac OS X',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: MacOS :: MacOS X',
         'Operating System :: Microsoft :: Windows', # XP
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python',
