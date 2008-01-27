c748b0086323eab085ee2fdc35eb09f92c560dca
Alex.Holkner <Alex.Holkner@gmail.com>
2008-01-27 09:13:50 +0000

    Fix silent driver get_time

--- pyglet/media/drivers/silent.py
+++ pyglet/media/drivers/silent.py
@@ -44,6 +44,8 @@ from pyglet.media import AudioPlayer, Listener, AudioData
 from pyglet.media import MediaException
 
 class SilentAudioPlayer(AudioPlayer):
+    UPDATE_PERIOD = 0.1
+    
     def __init__(self, audio_format):
         super(SilentAudioPlayer, self).__init__(audio_format)
 
@@ -52,15 +54,18 @@ class SilentAudioPlayer(AudioPlayer):
 
         self._audio_data_list = []
         self._head_time = 0.0
+        self._head_timestamp = 0.0
         self._head_system_time = time.time()
 
     def get_write_size(self):
-        return max(0, 10000 - sum(
+        bytes = int(self.audio_format.bytes_per_second * self.UPDATE_PERIOD)
+        return max(0, bytes - sum(
             [a.length for a in self._audio_data_list if a is not None]))
 
     def write(self, audio_data):
         if not self._audio_data_list:
             self._head_time = 0.0
+            self._head_timestamp = audio_data.timestamp
             self._head_system_time = time.time()
         self._audio_data_list.append(
             AudioData(None, 
@@ -102,14 +107,14 @@ class SilentAudioPlayer(AudioPlayer):
                 while self._audio_data_list[0] is None:
                     self._eos_count += 1
                     self._audio_data_list.pop(0)
+            self._head_timestamp = self._audio_data_list[0].timestamp
             self._head_system_time = system_time - head_time
-            return head_time + self._audio_data_list[0].timestamp
         except IndexError:
-            return 0.0
+            pass
 
     def get_time(self):
         if not self._audio_data_list:
-            return 0.0
+            return time.time() - self._head_system_time + self._head_timestamp
 
         if self._playing:
             system_time = time.time()
