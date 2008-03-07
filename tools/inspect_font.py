5efd0561e4cfb2b5e067227caabe8b553a545141
Alex.Holkner <Alex.Holkner@gmail.com>
2008-03-07 13:40:15 +0000

    Add inspect_font.py to distributed tools list; add crappy error handling.

--- tools/inspect_font.py
+++ tools/inspect_font.py
@@ -12,11 +12,16 @@ import sys
 from pyglet.font import ttf
 
 def inspect_font(filename):
-    info = ttf.TruetypeInfo(filename)
-    print '%s:' % filename,
-    print info.get_name('family'), 
-    print 'bold=%r' % info.is_bold(),
-    print 'italic=%r' % info.is_italic(),
+    try:
+        info = ttf.TruetypeInfo(filename)
+        print '%s:' % filename,
+        print info.get_name('family'), 
+        print 'bold=%r' % info.is_bold(),
+        print 'italic=%r' % info.is_italic(),
+    except:
+        print '''%s could not be identified.  It is probably not a TrueType or
+OpenType font.  However, pyglet may still be able to load it
+on some platforms.''' % filename
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
