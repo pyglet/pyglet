17222ec1b7af536595231a6b222b8572251dd666
Alex.Holkner <Alex.Holkner@gmail.com>
2008-03-23 07:10:51 +0000

    Bump version to 1.1alpha2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ import shutil
 import sys
 
 # Bump pyglet/__init__.py version as well.
-VERSION = '1.1alpha1'
+VERSION = '1.1alpha2'
 
 long_description = '''pyglet provides an object-oriented programming
 interface for developing games and other visually-rich applications
