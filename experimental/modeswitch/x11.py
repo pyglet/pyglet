b5839f8a1b255aeb842d6ed8033c4664d0464278
Alex.Holkner <Alex.Holkner@gmail.com>
2008-05-05 14:32:48 +0000

    XF86VidMode proof of concept.  Single monitor only.  Dual-head mode switching and linux are impossible without crapping up the WM.

--- experimental/modeswitch/x11.py
+++ experimental/modeswitch/x11.py
@@ -68,6 +68,9 @@ import setup_path
 import ctypes
 import time
 
+import pyglet
+from pyglet.gl import *
+from pyglet.window.xlib import xlib
 from pyglet import window
 
 import xf86vmode
@@ -79,40 +82,118 @@ screen = display.get_default_screen()
 x_screen = screen._x_screen_id
 
 count = ctypes.c_int()
-modes_ptr = ctypes.POINTER(ctypes.POINTER(xf86vmode.XF86VidModeModeInfo))()
+modes = ctypes.POINTER(ctypes.POINTER(xf86vmode.XF86VidModeModeInfo))()
 xf86vmode.XF86VidModeGetAllModeLines(x_display, x_screen, 
-    ctypes.byref(count), ctypes.byref(modes_ptr))
-
-modes = ctypes.cast(
-    modes_ptr.contents, 
-    ctypes.POINTER(xf86vmode.XF86VidModeModeInfo * count.value)).contents
+    ctypes.byref(count), ctypes.byref(modes))
 
 restore_mode = modes[0]
 
-for mode in modes:
-    print mode.hdisplay, mode.vdisplay,
-    print mode.flags
-    # XXX returns -2 for current mode, and 1 for others? (0 == fine)
-    print xf86vmode.XF86VidModeValidateModeLine(x_display, x_screen, mode)
+def mode_info_to_line(info):
+    line = xf86vmode.XF86VidModeModeLine()
+    line.hdisplay = info.hdisplay
+    line.hsyncstart = info.hsyncstart
+    line.hsyncend = info.hsyncend
+    line.htotal = info.htotal
+    line.vdisplay = info.vdisplay
+    line.vsyncstart = info.vsyncstart
+    line.vtotal = info.vtotal
+    line.flags = info.flags
+    line.privsize = info.privsize
+    line.private = info.private
+    return line
 
-
-new_mode = xf86vmode.XF86VidModeModeLine()
-dotclock = ctypes.c_int()
-xf86vmode.XF86VidModeGetModeLine(x_display, x_screen, dotclock, new_mode)
-
-print new_mode.hdisplay, new_mode.vdisplay
-
-#new_mode.hdisplay = 800
-#new_mode.vdisplay = 600
-
-# XXX always fails -- doesn't seem to get used, according to codesearch
-#xf86vmode.XF86VidModeModModeLine(x_display, x_screen, new_mode)
-
-# XXX mode switch doesn't happen until process exits... need to nudge X?
-#xf86vmode.XF86VidModeSwitchToMode(x_display, x_screen, modes[1])
-
-#time.sleep(5)
-
-#xf86vmode.XF86VidModeSwitchToMode(x_display, x_screen, modes[0])
+def print_modes():
+    for i in range(count.value):
+        mode = modes.contents[i]
+        print (mode.hdisplay, mode.vdisplay, mode.hsyncstart, mode.hsyncend,
+            mode.htotal, mode.vsyncstart, mode.vsyncend, mode.vtotal)
+        # XXX returns -2 for current mode, and 1 for others? (0 == fine)
+        #print xf86vmode.XF86VidModeValidateModeLine(x_display, x_screen, mode)
 
 # TODO free modes_ptr
+
+def test_mod_modeline():
+    '''Doesn't work at all.'''
+    new_mode = xf86vmode.XF86VidModeModeLine()
+    dotclock = ctypes.c_int()
+    xf86vmode.XF86VidModeGetModeLine(x_display, x_screen, dotclock, new_mode)
+
+    print new_mode.hdisplay, new_mode.vdisplay, dotclock
+
+    new_mode.hdisplay = 800
+    new_mode.vdisplay = 800
+
+    # XXX always fails -- doesn't seem to get used, according to codesearch
+    print xf86vmode.XF86VidModeModModeLine(x_display, x_screen, new_mode)
+    xlib.XFlush(x_display)
+    #xlib.XSync(x_display, False)
+
+    xf86vmode.XF86VidModeGetModeLine(x_display, x_screen, dotclock, new_mode)
+    print new_mode.hdisplay, new_mode.vdisplay
+
+def test_change_modes():
+    '''Select an existing mode.'''
+    x = ctypes.c_int()
+    y = ctypes.c_int()
+    xf86vmode.XF86VidModeGetViewPort(x_display, x_screen, x, y)
+    print 'old viewport %d, %d' % (x.value, y.value)
+
+    xf86vmode.XF86VidModeSwitchToMode(x_display, x_screen, modes[1])
+    xf86vmode.XF86VidModeSetViewPort(x_display, x_screen, 0, 0)
+    xlib.XFlush(x_display)
+
+    screen.width = 1024
+    screen.height = 768
+    window = pyglet.window.Window(fullscreen=True, screen=screen)
+    window.set_location(0, 0)
+
+    xlib.XGrabPointer(x_display, window._window,
+                       True,
+                        0,
+                        xlib.GrabModeAsync,
+                        xlib.GrabModeAsync,
+                        window._window,
+                        0,
+                        xlib.CurrentTime)
+
+    @window.event
+    def on_key_press(symbol, modifiers):
+        xf86vmode.XF86VidModeSwitchToMode(x_display, x_screen, modes[0])
+        xf86vmode.XF86VidModeSetViewPort(x_display, x_screen, x, y)
+        xlib.XFlush(x_display)
+        pyglet.app.exit()
+
+    @window.event
+    def on_draw():
+        glClearColor(.5, .5, .5, 1)
+        window.clear()
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
+        glColor3f(0.6, 0, 0)
+        glRectf(0, 0, 320, 200)
+        glColor3f(0.6, 0.6, 0)
+        glRectf(0, 0, 640, 480)
+        glColor3f(0.6, 0.6, 0.6)
+        glRectf(0, 0, 800, 600)
+        glColor3f(0, 0.6, 0)
+        glRectf(0, 0, 1024, 768)
+        glColor3f(0, 0, 0.6)
+        glRectf(0, 0, 1280, 1024)
+
+    pyglet.app.run()
+
+def test_add_mode():
+    # Copy an existing mode and modify its hdisplay
+    src_mode = modes.contents[0]
+    new_mode = xf86vmode.XF86VidModeModeInfo()
+    ctypes.memmove(ctypes.byref(new_mode), ctypes.byref(src_mode), ctypes.sizeof(new_mode))
+    #new_mode.hdisplay = 800
+    #new_mode.vdisplay = 800
+    print 'adding',
+    print xf86vmode.XF86VidModeValidateModeLine(x_display, x_screen, new_mode)
+    xf86vmode.XF86VidModeAddModeLine(x_display, x_screen, new_mode, src_mode)
+    xlib.XFlush(x_display)
+
+    print_modes()     
+
+print_modes()     
+test_change_modes()
