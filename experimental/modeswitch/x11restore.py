dd0e5e6332cd11ba5169af4ac60c543af8628a75
Alex.Holkner <Alex.Holkner@gmail.com>
2008-05-09 11:30:04 +0000

    Non-polling parent death detection, and send mode updates to child through a pipe.

--- experimental/modeswitch/x11restore.py
+++ experimental/modeswitch/x11restore.py
@@ -6,11 +6,16 @@
 __docformat__ = 'restructuredtext'
 __version__ = '$Id$'
 
+import ctypes
 import os
 import fcntl
+import mutex
 import time
+import select
 import struct
+import signal
 import sys
+import threading
 
 import pyglet
 import pyglet.window
@@ -21,22 +26,85 @@ w = pyglet.window.Window(display=display)
 def on_draw():
     w.clear()
 
-# assume off_t is 64-bits
-lock = struct.pack('HHQQI', fcntl.F_WRLCK, 0, 0, 1, 0)
-tmpfile = os.tmpfile()
-fcntl.fcntl(tmpfile, fcntl.F_SETLK, lock)
+
+# Parent communicates to child by sending "mode packets" through a pipe:
+mode_read_pipe, mode_write_pipe = os.pipe()
+
+# Mode packets tell the child process how to restore a given display and
+# screen.  Only one packet should be sent per display/screen (more would
+# indicate redundancy or incorrect restoration).  Packet format is:
+#   display (max 256 chars), 
+#   screen
+#   width
+#   height
+#   dotclock
+class ModePacket(object):
+    format = '256siHHI'
+    size = struct.calcsize(format)
+    def __init__(self, display_name, screen, width, height, dotclock):
+        self.display_name = display_name
+        self.screen = screen
+        self.width = width
+        self.height = height
+        self.dotclock = dotclock
+
+    def encode(self):
+        return struct.pack(self.format, self.display_name, self.screen,
+                           self.width, self.height, self.dotclock)
+
+    @classmethod
+    def decode(cls, data):
+        display_name, screen, width, height, dotclock = \
+            struct.unpack(cls.format, data)
+        return cls(display_name.strip('\0'), screen, width, height, dotclock)
+
+    def __repr__(self):
+        return '%s(%r, %r, %r, %r, %r)' % (
+            self.__class__.__name__, self.display_name, self.screen,
+            self.width, self.height, self.dotclock)
 
 if os.fork() == 0:
-    # Child polls until parent has released lock (by dying).
-    time.sleep(1)
-    while True:
-        try:
-            fcntl.fcntl(tmpfile, fcntl.F_SETLK, lock)
-            break
-        except IOError:
-            time.sleep(1)
-    print 'child activated'
+    # Child process (watches for parent to die then restores video mode(s).
+    os.close(mode_write_pipe)
+
+    # Set up SIGHUP to be the signal for when the parent dies.
+    PR_SET_PDEATHSIG = 1
+    libc = ctypes.cdll.LoadLibrary('libc.so.6')
+    libc.prctl.argtypes = (ctypes.c_int, ctypes.c_ulong, ctypes.c_ulong,
+                           ctypes.c_ulong, ctypes.c_ulong)
+    libc.prctl(PR_SET_PDEATHSIG, signal.SIGHUP, 0, 0, 0)
+
+    # SIGHUP indicates the parent has died.  The child mutex is unlocked, it
+    # stops reading from the mode packet pipe and restores video modes on all
+    # displays/screens it knows about.
+    def _sighup(signum, frame):
+        parent_wait_mutex.unlock()
+    parent_wait_mutex = mutex.mutex()
+    parent_wait_mutex.lock(lambda arg: arg, None)
+    signal.signal(signal.SIGHUP, _sighup)
+
+    # Wait for parent to die and read packets from parent pipe
+    packets = []
+    buffer = ''
+    while parent_wait_mutex.test():
+        data = os.read(mode_read_pipe, ModePacket.size)
+        buffer += data
+        # Decode packets
+        while len(buffer) >= ModePacket.size:
+            packet = ModePacket.decode(buffer[:ModePacket.size])
+            packets.append(packet)
+            buffer = buffer[ModePacket.size:]
+
+    for packet in packets:
+        print packet
+    
 else:
-    # Parent runs indefinitely (until window is closed)
+    # Parent process.  Clean up pipe then continue running program as normal.
+    # Send mode packets through pipe as additional displays/screens are mode
+    # switched.
+    os.close(mode_read_pipe)
+
+    os.write(mode_write_pipe, ModePacket(':0', 1, 1024, 768, 75).encode())
     pyglet.app.run()
-    print 'parent finished.'
+    os.write(mode_write_pipe, ModePacket(':0', 0, 1280, 1024, 60).encode())
+    print 'parent finished'
