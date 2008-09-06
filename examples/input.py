077d7c31cd05b80619cf58ba02461b84a693180e
Alex.Holkner <Alex.Holkner@gmail.com>
2008-09-06 11:53:58 +0000

    pyglet.input example

---
+++ examples/input.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+'''
+'''
+
+__docformat__ = 'restructuredtext'
+__version__ = '$Id: $'
+
+import pyglet
+
+window = pyglet.window.Window()
+devices = pyglet.input.get_devices()
+
+def watch_control(device, control):
+    @control.event
+    def on_change(value):
+        print '%r: %r.on_change(%r)' % (device, control, value)
+
+print 'Devices:'
+for device in devices:
+    print '  ', device.name,
+    try:
+        device.open()
+        print 'OK'
+
+        for control in device.get_controls():
+            print '    ', control.name
+            watch_control(device, control)
+
+    except pyglet.input.DeviceException:
+        print 'Fail'
+    
+pyglet.app.run()
