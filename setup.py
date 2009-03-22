86d9dfcc6379d702ca6259ed1674e1be5660fb66
Alex.Holkner <Alex.Holkner@gmail.com>
2009-03-22 08:51:01 +0000

    Bump version to 1.1.3 and update changelog

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ import shutil
 import sys
 
 # Bump pyglet/__init__.py version as well.
-VERSION = '1.1.2'
+VERSION = '1.1.3'
 
 long_description = '''pyglet provides an object-oriented programming
 interface for developing games and other visually-rich applications
