f34f098149ec48b01af7ae4904134100f8c71f25
benjamin.coder.smith <devnull@localhost>
2009-08-19 01:29:45 +0000

    fix for issue 439

--- tests/window/EVENT_CLOSE.py
+++ tests/window/EVENT_CLOSE.py
@@ -7,7 +7,7 @@ Expected behaviour:
     event is printed to the terminal.  The window should not close
     when you do this.
 
-    Press ESC to end the test.
+    Click Left mouse button to end the test.
 '''
 
 __docformat__ = 'restructuredtext'
@@ -17,18 +17,24 @@ import unittest
 
 from pyglet import window
 from pyglet import event
+w = None
 
 class EVENT_CLOSE(unittest.TestCase):
     def on_close(self):
         print 'Window close event.'
         return event.EVENT_HANDLED
 
+    def on_mouse_press(self, x, y, button, modifiers):
+        global w
+        super(window.Window,w).on_close()
+
     def test_close(self):
+        global w
         w = window.Window(200, 200)
         w.push_handlers(self)
         while not w.has_exit:
             w.dispatch_events()
         w.close()
-
+        
 if __name__ == '__main__':
     unittest.main()
