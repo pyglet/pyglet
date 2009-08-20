e3366dea0e6b198408084152c0bdbf5ca4215fff
benjamin.coder.smith <devnull@localhost>
2009-08-20 00:38:45 +0000

    updated from maintenance

--- tests/window/EVENT_CLOSE.py
+++ tests/window/EVENT_CLOSE.py
@@ -17,21 +17,25 @@ import unittest
 
 from pyglet import window
 from pyglet import event
+w = None
 
 class EVENT_CLOSE(unittest.TestCase):
     def on_close(self):
         print 'Window close event.'
         return event.EVENT_HANDLED
 
-    def on_mouse_press(self, x, y, button, modifiers):
-        super(window.Window, self.w).on_close()
+    def on_key_press(self, symbol, mods):
+        if symbol == window.key.ESCAPE:
+            global w
+            super(window.Window, w).on_close()
 
     def test_close(self):
-        self.w = window.Window(200, 200)
-        self.w.push_handlers(self)
-        while not self.w.has_exit:
-            self.w.dispatch_events()
-        self.w.close()
+        global w
+        w = window.Window(200, 200)
+        w.push_handlers(self)
+        while not w.has_exit:
+            w.dispatch_events()
+        w.close()
         
 if __name__ == '__main__':
     unittest.main()
