d629418e858d5b3a6f028809c0ee79d72d4e0244
benjamin coder smith <benjamin.coder.smith@gmail.com>
2009-10-12 17:39:44 +0000

    Commit permission changes for real this time.  Also backed out a print statement that made it into tests/clock/FPS.py

--- tests/clock/FPS.py
+++ tests/clock/FPS.py
@@ -23,7 +23,6 @@ class FPS(unittest.TestCase):
             time.sleep(0.2)
             clock.tick()
         result = clock.get_fps()
-        print result
         self.assertTrue(abs(result - 5.0) < 0.05)
 
 if __name__ == '__main__':
