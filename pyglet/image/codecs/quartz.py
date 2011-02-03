818b1d753c0da8c1c6c1784a649ac1b54c337cbb
Phillip Nguyen <evil.phillip@gmail.com>
2011-02-03 00:12:34 -0600

    Added quartz-based decode_animation method.

--- pyglet/image/codecs/quartz.py
+++ pyglet/image/codecs/quartz.py
@@ -40,14 +40,12 @@ __version__ = '$Id$'
 
 from ctypes import *
 
-from pyglet.image import ImageData
+from pyglet.image import ImageData, Animation, AnimationFrame
 from pyglet.image.codecs import *
 
 import Quartz, CoreFoundation, LaunchServices 
 
 # TODO:
-# decode_animation
-# encoders?
 # more complete list of openable formats?
 # which formats actually make sense for pyglet to try to open?
 
@@ -57,7 +55,7 @@ class QuartzImageDecoder(ImageDecoder):
         # A lot of these are RAW file types, which are probably unlikely 
         # candidates for files to be opened anyway.
         # First get a list of uniform type identifiers (they look like 'com.microsoft.bmp').
-        types = Quartz.ImageIO.CGImageSourceCopyTypeIdentifiers()
+        types = Quartz.CGImageSourceCopyTypeIdentifiers()
         # Then convert these into a list of associated extensions.
         extensions = []
         for uti in types:
@@ -78,6 +76,9 @@ class QuartzImageDecoder(ImageDecoder):
         # Quartz can actually decode many more formats, but these are the most common.
         return [ '.bmp', '.cur', '.gif', '.ico', '.jp2', '.jpg', '.jpeg', 
                  '.pcx', '.png', '.tga', '.tif', '.tiff', '.xbm', '.xpm' ]
+
+    def get_animation_file_extensions(self):
+        return ['.gif']
      
     def _get_format(self, imageRef):
         """Try to figure out the internal format of the image."""
@@ -102,15 +103,8 @@ class QuartzImageDecoder(ImageDecoder):
         # If we get here, then we don't know what the format is.
         raise ImageDecodeException(filename or file)
 
-    def decode(self, file, filename):
-        file_bytes = file.read()
-        data = CoreFoundation.CFDataCreate(None, file_bytes, len(file_bytes))
-        # Second argument is an options dictionary.  It might be a good idea to provide
-        # a value for kCGImageSourceTypeIdentifierHint here using filename extension.
-        sourceRef = Quartz.ImageIO.CGImageSourceCreateWithData(data, None)
-        imageRef = Quartz.ImageIO.CGImageSourceCreateImageAtIndex(sourceRef, 0, None)
-
-        del data, sourceRef
+    def _get_pyglet_ImageData_from_source_at_index(self, sourceRef, index):
+        imageRef = Quartz.CGImageSourceCreateImageAtIndex(sourceRef, index, None)
 
         # Get info about the image.
         width = Quartz.CGImageGetWidth(imageRef)
@@ -125,11 +119,49 @@ class QuartzImageDecoder(ImageDecoder):
         buffer = (c_byte * (height * bytesPerRow))()
         byteRange = CoreFoundation.CFRangeMake(0, CoreFoundation.CFDataGetLength(imageData))
         CoreFoundation.CFDataGetBytes(imageData, byteRange, buffer)
-
+        
         del imageRef, imageData
-
+        
         pitch = bytesPerRow
         return ImageData(width, height, format, buffer, -pitch)
+
+    def decode(self, file, filename):
+        file_bytes = file.read()
+        data = CoreFoundation.CFDataCreate(None, file_bytes, len(file_bytes))
+        # Second argument is an options dictionary.  It might be a good idea to provide
+        # a value for kCGImageSourceTypeIdentifierHint here using filename extension.
+        sourceRef = Quartz.CGImageSourceCreateWithData(data, None)
+        image = self._get_pyglet_ImageData_from_source_at_index(sourceRef, 0)
+        del data, sourceRef
+        return image
+
+    def decode_animation(self, file, filename):
+        # If file is not an animated GIF, it will be loaded as a single-frame animation.
+        file_bytes = file.read()
+        data = CoreFoundation.CFDataCreate(None, file_bytes, len(file_bytes))
+        sourceRef = Quartz.CGImageSourceCreateWithData(data, None)
+
+        # Get number of frames in the animation.
+        count = Quartz.CGImageSourceGetCount(sourceRef)
+        
+        frames = []
+
+        for index in range(count):
+            # Try to determine frame duration from GIF properties dictionary.
+            duration = 0.1  # default duration if none found
+            props = Quartz.CGImageSourceCopyPropertiesAtIndex(sourceRef, index, None)
+            if Quartz.kCGImagePropertyGIFDictionary in props:
+                gif_props = props[Quartz.kCGImagePropertyGIFDictionary]
+                if Quartz.kCGImagePropertyGIFDelayTime in gif_props:
+                    duration = gif_props[Quartz.kCGImagePropertyGIFDelayTime]
+            
+            image = self._get_pyglet_ImageData_from_source_at_index(sourceRef, index)
+            frames.append( AnimationFrame(image, duration) )
+
+        del data, sourceRef
+
+        return Animation(frames)
+        
 
 def get_decoders():
     return [ QuartzImageDecoder() ]
