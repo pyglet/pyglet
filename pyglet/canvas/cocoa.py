210539be244c5d370fdb4653cd5d099b64a12ea8
Phillip Nguyen <evil.phillip@gmail.com>
2011-03-14 23:52:47 -0500

    Converted screen mode code to mostly ctypes; no longer depends on PyObjC 2.3

--- pyglet/canvas/cocoa.py
+++ pyglet/canvas/cocoa.py
@@ -1,3 +1,4 @@
+# Note: The display mode API used here is Mac OS 10.6 only.
 
 '''
 '''
@@ -5,21 +6,52 @@
 __docformat__ = 'restructuredtext'
 __version__ = '$Id: $'
 
+from ctypes import *
+from ctypes import util
+
 from pyglet import app
 from base import Display, Screen, ScreenMode, Canvas
 
-from pyglet.libs.darwin import *
+# Use PyObjC to get the list of displays and their dimensions,
+# just because it is easier.  However this module could be made
+# to be pure ctypes if necessary.
+from Cocoa import NSScreen
 
-# NOTE: The display mode code uses methods that are available only with PyObjC 2.3
-# The alternative methods available in PyObjC 2.2 are all deprecated in Mac OS X 10.6
+######################################################################
+# ctypes setup:
 
-from objc import __version__ as pyobjc_version
-pyobjc_version = float(pyobjc_version[:3])
+cf = cdll.LoadLibrary(util.find_library('CoreFoundation'))
+quartz = cdll.LoadLibrary(util.find_library('quartz'))
+
+# Core Foundation constants
+kCFStringEncodingUTF8 = 0x08000100
+
+cf.CFArrayGetValueAtIndex.restype = c_void_p
+quartz.CGDisplayCopyAllDisplayModes.restype = c_void_p
+quartz.CGDisplayCopyAllDisplayModes.argtypes = [c_uint32, c_void_p]
+quartz.CGDisplayModeGetRefreshRate.restype = c_double
+quartz.CGDisplayModeCopyPixelEncoding.restype = c_void_p
+
+def cfstring_to_string(cfstring):
+    length = cf.CFStringGetLength(cfstring)
+    size = cf.CFStringGetMaximumSizeForEncoding(length, kCFStringEncodingUTF8)
+    buffer = c_buffer(size + 1)
+    result = cf.CFStringGetCString(cfstring, buffer, len(buffer), kCFStringEncodingUTF8)
+    if result:
+        return buffer.value
+
+def cfarray_to_list(cfarray):
+    count = cf.CFArrayGetCount(cfarray)
+    return [ c_void_p(cf.CFArrayGetValueAtIndex(cfarray, i))
+             for i in range(count) ]
+
+######################################################################
 
 class CocoaDisplay(Display):
 
     def get_screens(self):
         return [CocoaScreen(self, nsscreen) for nsscreen in NSScreen.screens()]
+
 
 class CocoaScreen(Screen):
 
@@ -40,44 +72,50 @@ class CocoaScreen(Screen):
         return template.match(canvas)
 
     def get_modes(self):
-        if pyobjc_version < 2.3: return []
-        mode_list = CGDisplayCopyAllDisplayModes(self._cg_display_id, None)
-        return [ CocoaScreenMode(self, cgmode) for cgmode in mode_list ]
+        cgmodes = c_void_p(quartz.CGDisplayCopyAllDisplayModes(self._cg_display_id, None))
+        modes = [ CocoaScreenMode(self, cgmode) for cgmode in cfarray_to_list(cgmodes) ]
+        cf.CFRelease(cgmodes)
+        return modes
 
     def get_mode(self):
-        if pyobjc_version < 2.3: return
-        cgmode = CGDisplayCopyDisplayMode(self._cg_display_id)
-        return CocoaScreenMode(self, cgmode)
+        quartz.CGDisplayCopyDisplayMode.restype = c_void_p
+        cgmode = c_void_p(quartz.CGDisplayCopyDisplayMode(self._cg_display_id))
+        mode = CocoaScreenMode(self, cgmode)
+        quartz.CGDisplayModeRelease(cgmode)
+        return mode
 
     def set_mode(self, mode): 
-        if pyobjc_version < 2.3: return
         assert mode.screen is self
-        CGDisplayCapture(self._cg_display_id)
-        CGDisplaySetDisplayMode(self._cg_display_id, mode.cgmode, None)
+        quartz.CGDisplayCapture(self._cg_display_id)
+        quartz.CGDisplaySetDisplayMode(self._cg_display_id, mode.cgmode, None)
         self.width = mode.width
         self.height = mode.height
 
     def restore_mode(self):
-        if pyobjc_version >= 2.3: 
-            CGDisplaySetDisplayMode(self._cg_display_id, self._default_mode.cgmode, None)
-        CGDisplayRelease(self._cg_display_id)
+        quartz.CGDisplaySetDisplayMode(self._cg_display_id, self._default_mode.cgmode, None)
+        quartz.CGDisplayRelease(self._cg_display_id)
 
     def capture_display(self):
-        CGDisplayCapture(self._cg_display_id)
+        quartz.CGDisplayCapture(self._cg_display_id)
 
     def release_display(self):
-        CGDisplayRelease(self._cg_display_id)
+        quartz.CGDisplayRelease(self._cg_display_id)
 
 
 class CocoaScreenMode(ScreenMode):
 
     def __init__(self, screen, cgmode):
         super(CocoaScreenMode, self).__init__(screen)
+        quartz.CGDisplayModeRetain(cgmode)
         self.cgmode = cgmode
-        self.width = int(CGDisplayModeGetWidth(cgmode))
-        self.height = int(CGDisplayModeGetHeight(cgmode))
+        self.width = int(quartz.CGDisplayModeGetWidth(cgmode))
+        self.height = int(quartz.CGDisplayModeGetHeight(cgmode))
         self.depth = self.getBitsPerPixel(cgmode)
-        self.rate = CGDisplayModeGetRefreshRate(cgmode)
+        self.rate = quartz.CGDisplayModeGetRefreshRate(cgmode)
+
+    def __del__(self):
+        quartz.CGDisplayModeRelease(self.cgmode)
+        self.cgmode = None
         
     def getBitsPerPixel(self, cgmode):
         # from /System/Library/Frameworks/IOKit.framework/Headers/graphics/IOGraphicsTypes.h
@@ -85,7 +123,9 @@ class CocoaScreenMode(ScreenMode):
         IO16BitDirectPixels = "-RRRRRGGGGGBBBBB"
         IO32BitDirectPixels = "--------RRRRRRRRGGGGGGGGBBBBBBBB"
 
-        pixelEncoding = CGDisplayModeCopyPixelEncoding(cgmode)
+        cfstring = c_void_p(quartz.CGDisplayModeCopyPixelEncoding(cgmode))
+        pixelEncoding = cfstring_to_string(cfstring)
+        cf.CFRelease(cfstring)
 
         if pixelEncoding == IO8BitIndexedPixels: return 8
         if pixelEncoding == IO16BitDirectPixels: return 16
