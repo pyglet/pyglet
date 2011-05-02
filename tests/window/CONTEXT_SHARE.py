d569351733d1ab399d50f5a9c04cabbb0233cfae
Ben Smith <benjamin.coder.smith@gmail.com>
2011-05-01 20:06:00 -0700

    close windows when exceptions occur

--- tests/window/CONTEXT_SHARE.py
+++ tests/window/CONTEXT_SHARE.py
@@ -25,62 +25,78 @@ class CONTEXT_SHARE(unittest.TestCase):
 
     def test_context_share_list(self):
         w1 = window.Window(200, 200)
-        w1.switch_to()
-        list = glGenLists(1)
-        glNewList(list, GL_COMPILE)
-        glLoadIdentity()
-        glEndList()
-        self.assertTrue(glIsList(list))
+        try:
+            w1.switch_to()
+            glist = glGenLists(1)
+            glNewList(glist, GL_COMPILE)
+            glLoadIdentity()
+            glEndList()
+            self.assertTrue(glIsList(glist))
+        except:
+            w1.close()
+            raise
 
         w2 = window.Window(200, 200)
-        w2.switch_to()
-        self.assertTrue(glIsList(list))
-
-        w1.close()
-        w2.close()
+        try:
+            w2.switch_to()
+            self.assertTrue(glIsList(glist))
+        finally:
+            w1.close()
+            w2.close()
 
     def test_context_noshare_list(self):
         w1 = window.Window(200, 200)
-        w1.switch_to()
-        list = glGenLists(1)
-        glNewList(list, GL_COMPILE)
-        glLoadIdentity()
-        glEndList()
-        self.assertTrue(glIsList(list))
+        try:
+            w1.switch_to()
+            glist = glGenLists(1)
+            glNewList(glist, GL_COMPILE)
+            glLoadIdentity()
+            glEndList()
+            self.assertTrue(glIsList(glist))
+        except:
+            w1.close()
+            raise
 
         w2 = window.Window(200, 200, context=self.create_context(None))
-        w2.set_visible(True)
-        w2.switch_to()
-        self.assertTrue(not glIsList(list))
-
-        w1.close()
-        w2.close()
+        try:
+            w2.set_visible(True)
+            w2.switch_to()
+            self.assertTrue(not glIsList(glist))
+        finally:
+            w1.close()
+            w2.close()
 
     def test_context_share_texture(self):
         w1 = window.Window(200, 200)
-        w1.switch_to()
-        textures = c_uint()
-        glGenTextures(1, byref(textures))
-        texture = textures.value
+        try:
+            w1.switch_to()
+            textures = c_uint()
+            glGenTextures(1, byref(textures))
+            texture = textures.value
 
-        glBindTexture(GL_TEXTURE_2D, texture)
-        data = (c_ubyte * 4)()
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 1, 1, 0, GL_RGBA,
-                     GL_UNSIGNED_BYTE, data)
-        self.assertTrue(glIsTexture(texture))
+            glBindTexture(GL_TEXTURE_2D, texture)
+            data = (c_ubyte * 4)()
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 1, 1, 0, GL_RGBA,
+                         GL_UNSIGNED_BYTE, data)
+            self.assertTrue(glIsTexture(texture))
+        except:
+            w1.close()
+            raise
 
         w2 = window.Window(200, 200)
-        w2.switch_to()
-        self.assertTrue(glIsTexture(texture))
+        try:
+            w2.switch_to()
+            self.assertTrue(glIsTexture(texture))
 
-        glDeleteTextures(1, byref(textures))
-        self.assertTrue(not glIsTexture(texture))
+            glDeleteTextures(1, byref(textures))
+            self.assertTrue(not glIsTexture(texture))
 
-        w1.switch_to()
-        self.assertTrue(not glIsTexture(texture))
+            w1.switch_to()
+            self.assertTrue(not glIsTexture(texture))
 
-        w1.close()
-        w2.close()
+        finally:
+            w1.close()
+            w2.close()
 
 if __name__ == '__main__':
     unittest.main()
