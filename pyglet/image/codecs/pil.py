329f0d037e4f21be17903d6e2334e550c7897019
Andreas Schiefer <andreas.schiefer@gmail.com>
2012-03-11 11:50:26 +0100

    Fix issue 551
    The correct way to import Image seems to differ depending on how
    PIL was installed. This fix supports both ways of importing.

--- pyglet/image/codecs/pil.py
+++ pyglet/image/codecs/pil.py
@@ -44,7 +44,10 @@ from pyglet.gl import *
 from pyglet.image import *
 from pyglet.image.codecs import *
 
-import Image
+try:
+    import Image
+except ImportError:
+    from PIL import Image
 
 class PILImageDecoder(ImageDecoder):
     def get_file_extensions(self):
