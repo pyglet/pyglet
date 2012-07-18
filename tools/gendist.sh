63aa57a78e076dadee7646266fc66f52515e353d
Richard Jones <richard@mechanicalcat.net>
2012-07-18 10:58:19 +1000

    some release tweaks

--- tools/gendist.sh
+++ tools/gendist.sh
@@ -10,11 +10,15 @@ VERSION=`grep 'VERSION =' setup.py | cut -d "'" -f2`
 python setup.py sdist --formats=gztar,zip
 
 # Eggs
-python2.4 setup.py bdist_egg --exclude-source-files
-python2.5 setup.py bdist_egg --exclude-source-files
+#python2.4 setup.py bdist_egg --exclude-source-files
+#python2.5 setup.py bdist_egg --exclude-source-files
 python2.6 setup.py bdist_egg --exclude-source-files
+python2.7 setup.py bdist_egg --exclude-source-files
+python3.3 setup.py bdist_egg --exclude-source-files
 
 # Build docs archive
 python setup.py sdist --manifest-only
-rm dist/pyglet-$VERSION-docs.zip
-grep -v ^pyglet MANIFEST | zip dist/pyglet-$VERSION-docs.zip -@
+rm dist/pyglet-docs-$VERSION.zip
+(cd doc/_build; zip -r docs.zip html)
+mv doc/_build/docs.zip dist/pyglet-docs-$VERSION.zip
+zip dist/pyglet-docs-$VERSION.zip `grep '^examples/' MANIFEST`
