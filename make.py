0f5632298e09a019788c8eea8d9e323195188e04
Juan J. Martínez <jjm@usebox.net>
2013-10-13 18:56:37 +0100

    Support for python 3

--- make.py
+++ make.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from __future__ import print_function
 import os
 import os.path as op
 import sys
@@ -13,7 +14,7 @@ DOC_DIR = op.join(THIS_DIR, 'doc')
 def clean():
     dirs = [op.join(DOC_DIR, '_build')]
     for d in dirs:
-        print '   Removing:', d
+        print('   Removing:', d)
         shutil.rmtree(d, ignore_errors=True)
 
 def docs():
@@ -24,15 +25,15 @@ def docs():
         webbrowser.open('file://'+op.abspath(DOC_DIR)+'/_build/html/index.html')
 
 if __name__=='__main__':
-    avail_cmds = dict(filter(lambda (k,v): not k.startswith('_') 
-                             and inspect.isfunction(v)
-                             and v.__module__ == '__main__',
+    avail_cmds = dict(filter(lambda kv: not kv[0].startswith('_') 
+                             and inspect.isfunction(kv[1])
+                             and kv[1].__module__ == '__main__',
                              locals().items()))
     try:
         cmd = avail_cmds[sys.argv[1]]
-    except Exception, exc:
-        print type(exc).__name__, ':', exc
-        print 'Usage:', op.basename(sys.argv[0]), '<command>'
-        print '  where commands are:', ', '.join(avail_cmds)
+    except Exception as exc:
+        print(type(exc).__name__, ':', exc)
+        print('Usage:', op.basename(sys.argv[0]), '<command>')
+        print('  where commands are:', ', '.join(avail_cmds))
     else:
         cmd()
