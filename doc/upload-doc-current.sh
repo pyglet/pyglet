1a25c4c235c6ccd341aa611f44464b31cc23ed54
Juan J. Martínez <jjm@usebox.net>
2014-04-17 18:16:18 +0100

    Create html directory (sphinx 1.2.2)

--- doc/upload-doc-current.sh
+++ doc/upload-doc-current.sh
@@ -5,6 +5,7 @@ PYGLET_ORG=pyglet.org
 PYGLET_DIR=/home/pyglet/www
 
 make clean
+mkdir _build/html
 make html
 
 rsync -az --delete _build/html/ -e ssh $PYGLET_USR@$PYGLET_ORG:$PYGLET_DIR/doc-current
