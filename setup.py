92233b55df2509ec10421a681e9ab04b2dfb32fc
Rob van der Most <silvester747@gmail.com>
2015-01-27 19:54:45 +0100

    Add supported Python versions to classifiers.

--- setup.py
+++ setup.py
@@ -43,7 +43,12 @@ setup_info = dict(
         'Operating System :: MacOS :: MacOS X',
         'Operating System :: Microsoft :: Windows', # XP
         'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Games/Entertainment',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
