4d545c247b53156d8a7e58dab1ea375f192514be
Rob van der Most <silvester747@gmail.com>
2015-02-14 12:31:57 +0100

    Fix osx version check unit test. Pulled version did not expect carbon support.

--- tests/unit/test_osx.py
+++ tests/unit/test_osx.py
@@ -6,20 +6,30 @@ import pyglet
 
 class OSXImportTestCase(unittest.TestCase):
 
+    @mock.patch('sys.platform', 'darwin')
+    @mock.patch('platform.mac_ver', lambda: ['10.5.0'])
+    @mock.patch('struct.calcsize', lambda i: 4)
+    def test_old_32bit_osx_uses_carbon(self):
+        imp.reload(pyglet)
+        self.assertEqual(pyglet.options['darwin_cocoa'], False)
+
     @mock.patch("sys.platform", "darwin")
     @mock.patch("platform.mac_ver", lambda: ['10.5.0'])
-    def test_cannot_import_old_osx(self):
+    @mock.patch('struct.calcsize', lambda i: 8)
+    def test_old_64bit_osx_not_supported(self):
         with self.assertRaises(Exception) as e:
             imp.reload(pyglet)
 
     @mock.patch("sys.platform", "darwin")
     @mock.patch('struct.calcsize', lambda i: 4)
     @mock.patch("platform.mac_ver", lambda: ['10.10.0'])
-    def test_cannot_import_32bit_python(self):
-        with self.assertRaises(Exception) as e:
-            imp.reload(pyglet)
+    def test_32bit_osx_uses_carbon(self):
+        imp.reload(pyglet)
+        self.assertEqual(pyglet.options['darwin_cocoa'], False)
 
     @mock.patch("sys.platform", "darwin")
     @mock.patch("platform.mac_ver", lambda: ['10.6.0'])
-    def test_can_import_osx_current_version(self):
+    @mock.patch('struct.calcsize', lambda i: 8)
+    def test_64bit_osx_uses_cocoa(self):
         imp.reload(pyglet)
+        self.assertEqual(pyglet.options['darwin_cocoa'], True)
