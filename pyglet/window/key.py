74eb693b78a7344a8167ead8186476af16cb1f0a
jamessanders <devnull@localhost>
2015-02-18 17:52:13 +0000

    Fixed typo in doctest (was causing 2to3 to fail).

--- pyglet/window/key.py
+++ pyglet/window/key.py
@@ -147,7 +147,7 @@ def motion_string(motion):
 
     Example::
 
-        >>> motion_string(MOTION_NEXT_WORD):
+        >>> motion_string(MOTION_NEXT_WORD)
         'MOTION_NEXT_WORD'
 
     :Parameters:
