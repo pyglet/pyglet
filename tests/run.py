37676ad6ea290bbcd4a9db0023c186b21f25cbc4
Rob van der Most <silvester747@gmail.com>
2015-02-26 19:48:51 +0100

    Make it easy to run full automatic or sanity test.

--- tests/run.py
+++ tests/run.py
@@ -4,29 +4,56 @@ Test runner that wraps around unittest.
 
 import argparse
 import os
+import sys
 import unittest
 
 
 def _parse_args():
-    parser = argparse.ArgumentParser(description='Pyglet test runner')
+    parser = argparse.ArgumentParser(description='Pyglet test runner',
+                                     formatter_class=argparse.RawTextHelpFormatter)
 
-    suite_choices = ['unit', 'integration', 'interactive']
+    suite_choices = ['unit', 'integration', 'interactive', 'sanity', 'automatic']
+    suite_help = """Test suite(s) to run. Has the following options:
+- unit:        Run the unit tests
+- integration: Run the integration tests
+- interactive: Run the interactive tests
+- sanity:      Run all tests. For the interactive tests do not use interactive prompts and try to
+               run as many tests as possible
+- automatic:   Run all tests. For the interactive tests skip the tests that cannot validate without
+               user interaction."""
     parser.add_argument('suites',
                         metavar='SUITE',
                         nargs='*',
                         choices=suite_choices,
-                        help='Test suite to run'
+                        help=suite_help
                         )
     parser.add_argument('--non-interactive', '-n',
                         action='store_true',
-                        help='Do not use interactive prompts. Skip tests that cannot validate or run without.'
+                        help='[Interactive tests only] Do not use interactive prompts. Skip tests that cannot validate or run without.'
                         )
     parser.add_argument('--sanity', '-s',
                         action='store_true',
-                        help='Run a sanity check without interactive prompts. Only skips tests that cannot finish without user intervention.'
+                        help='[Interactive tests only] Do not use interactive prompts. Only skips tests that cannot finish without user intervention.'
                         )
 
-    return parser.parse_args()
+    options = parser.parse_args()
+
+    if 'sanity' in options.suites:
+        if len(options.suites) > 1:
+            print('sanity suite cannot be combined with other suites')
+            sys.exit(-1)
+        options.suites = ['unit', 'integration', 'interactive']
+        options.non_interactive = False
+        options.sanity = True
+    elif 'automatic' in options.suites:
+        if len(options.suites) > 1:
+            print('automatic suite cannot be combined with other suites')
+            sys.exit(-1)
+        options.suites = ['unit', 'integration', 'interactive']
+        options.non_interactive = True
+        options.sanity = False
+
+    return options
 
 def _load_suites(suites):
     loader = unittest.loader.defaultTestLoader
