ad15c712ba6a4920318521404fc24b3cf25d6238
Rob van der Most <silvester747@gmail.com>
2015-02-27 17:26:31 +0100

    Add option to create coverage report for tests. Only available if coverage module is installed.

--- tests/run.py
+++ tests/run.py
@@ -4,9 +4,15 @@ Test runner that wraps around unittest.
 
 import argparse
 import os
+import pyglet
 import sys
+from tests.annotations import Platform
 import unittest
 
+try:
+    from coverage import coverage
+except:
+    coverage = None
 
 def _parse_args():
     parser = argparse.ArgumentParser(description='Pyglet test runner',
@@ -14,13 +20,16 @@ def _parse_args():
 
     suite_choices = ['unit', 'integration', 'interactive', 'sanity', 'automatic']
     suite_help = """Test suite(s) to run. Has the following options:
-- unit:        Run the unit tests
-- integration: Run the integration tests
-- interactive: Run the interactive tests
+- unit:        Run the unit tests.
+- integration: Run the integration tests.
+- interactive: Run the interactive tests.
 - sanity:      Run all tests. For the interactive tests do not use interactive prompts and try to
-               run as many tests as possible
+               run as many tests as possible. (Same as: unit integration interactve --sanity).
 - automatic:   Run all tests. For the interactive tests skip the tests that cannot validate without
-               user interaction."""
+               user interaction. (Same as: unit integration interactive --non-interactive)."""
+    coverage_help = 'Determine test coverage and create an HTML report for it.'
+    if coverage is None:
+        coverage_help += '\nTo use, install coverage (pip install coverage)'
     parser.add_argument('suites',
                         metavar='SUITE',
                         nargs='*',
@@ -34,6 +43,10 @@ def _parse_args():
     parser.add_argument('--sanity', '-s',
                         action='store_true',
                         help='[Interactive tests only] Do not use interactive prompts. Only skips tests that cannot finish without user intervention.'
+                        )
+    parser.add_argument('--coverage', '-c',
+                        action='store_true',
+                        help=coverage_help
                         )
 
     options = parser.parse_args()
@@ -77,8 +90,45 @@ def _run_suites(test_suite, options):
         else:
             tests.interactive.interactive_test_base.set_noninteractive_sanity()
 
+    if coverage is not None and options.coverage:
+        cov = coverage(branch=True,
+                       source=['pyglet'],
+                       omit=_get_platform_omit())
+        cov.start()
+
     runner = unittest.TextTestRunner()
     runner.run(test_suite)
+
+    if coverage is not None and options.coverage:
+        cov.stop()
+        cov.html_report(directory='coverage_report')
+        html_report = os.path.abspath(os.path.join('coverage_report', 'index.html'))
+        print('Coverage report: file://' + html_report)
+
+def _get_platform_omit():
+    omit = ['pyglet/extlibs/*']
+
+    windows_specific = ['*win32*', '*wgl*', '*gdiplus*', '*wintab*', '*directsound*']
+    linux_specific = ['*xlib*', '*freetype*', '*glx*', '*gdkpixbuf2*', '*x11*', '*pulse*']
+    osx_specific = ['*agl*', '*darwin*']
+    osx_carbon_specific = ['*carbon*', '*quicktime*']
+    osx_cocoa_specific = ['*cocoa*', '*quartz*']
+
+    if pyglet.compat_platform not in Platform.LINUX:
+        omit.extend(linux_specific)
+    if pyglet.compat_platform not in Platform.WINDOWS:
+        omit.extend(windows_specific)
+    if pyglet.compat_platform not in Platform.OSX:
+        omit.extend(osx_specific)
+        omit.extend(osx_carbon_specific)
+        omit.extend(osx_cocoa_specific)
+    if pyglet.compat_platform in Platform.OSX:
+        if pyglet.options['darwin_cocoa']:
+            omit.extend(osx_carbon_specific)
+        else:
+            omit.extend(osx_cocoa_specific)
+
+    return omit
 
 if __name__ == '__main__':
     options =_parse_args()
