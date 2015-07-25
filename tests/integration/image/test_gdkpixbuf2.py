0c6b427b1e72b9dec32e76d9a0e75a86cf850178
Rob van der Most <silvester747@gmail.com>
2015-07-25 16:39:16 +0200

    Improve usage of pixbuf api. Close the loader after writing to make sure all data is parsed. Increase reference counts for kept objects.

--- tests/integration/image/test_gdkpixbuf2.py
+++ tests/integration/image/test_gdkpixbuf2.py
@@ -27,6 +27,23 @@ class GdkPixBufTest(PygletTestCase):
             assert image.height == 257
             assert image.width == 235
 
+    def test_load_image_requires_loader_close(self):
+        """Some image files require the loader to be closed to force it
+        to read and parse all data."""
+        filename = self.get_test_data_file('images', 'gdk_close.png')
+        with open(filename, 'rb') as f:
+            loader = gdkpixbuf2.GdkPixBufLoader(f, filename)
+            pixbuf = loader.get_pixbuf()
+            assert pixbuf is not None
+            assert pixbuf.height == 200
+            assert pixbuf.width == 200
+            assert pixbuf.channels == 4
+            assert pixbuf.has_alpha
+            image = pixbuf.to_image()
+            assert image is not None
+            assert image.height == 200
+            assert image.width == 200
+
     def test_load_animation(self):
         filename = self.get_test_data_file('images', 'dinosaur.gif')
         with open(filename, 'rb') as f:
@@ -43,3 +60,9 @@ class GdkPixBufTest(PygletTestCase):
                 assert frame.image.width == 129
                 assert frame.image.height == 79
 
+    def test_incomplete_load(self):
+        """Start loading a file and stop. Should clean up correctly."""
+        filename = self.get_test_data_file('images', 'gdk_close.png')
+        with open(filename, 'rb') as f:
+            loader = gdkpixbuf2.GdkPixBufLoader(f, filename)
+            del loader
