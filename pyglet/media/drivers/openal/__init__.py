43cb02c0f5bdb40027d6563f15230c7f2ba407fb
Rob van der Most <silvester747@gmail.com>
2015-08-17 21:09:50 +0200

    Change string literal to byte literal for Py3 compatbility. Thanks to Peter Harris. Fixes issue #57.

--- pyglet/media/drivers/openal/__init__.py
+++ pyglet/media/drivers/openal/__init__.py
@@ -609,7 +609,7 @@ class OpenALDriver(AbstractAudioDriver):
     def get_extensions(self):
         extensions = alc.alcGetString(self._device, alc.ALC_EXTENSIONS)
         if pyglet.compat_platform == 'darwin' or pyglet.compat_platform.startswith('linux'):
-            return ctypes.cast(extensions, ctypes.c_char_p).value.split(' ')
+            return ctypes.cast(extensions, ctypes.c_char_p).value.split(b' ')
         else:
             return _split_nul_strings(extensions)
 
