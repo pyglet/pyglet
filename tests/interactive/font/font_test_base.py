e981ce24f93de5de33ddcc77c864d3dfe56d33d9
Rob van der Most <silvester747@gmail.com>
2015-09-02 14:29:38 +0200

    Migrate last font tests to py.test. Remove old test base.

--- tests/interactive/font/font_test_base.py
+++ tests/interactive/font/font_test_base.py
@@ -7,7 +7,6 @@ from pyglet import gl
 from pyglet import font
 
 from tests.interactive.event_loop_test_base import TestWindow, EventLoopFixture
-from tests.interactive.windowed_test_base import WindowedTestCase
 
 
 class FontTestWindow(TestWindow):
@@ -35,6 +34,7 @@ class FontTestWindow(TestWindow):
                      text='Quickly brown fox',
                      color=(0, 0, 0, 1),
                      fill_width=False,
+                     margin=5,
                      **options):
 
         assert self.label is None, 'Label already created'
@@ -42,9 +42,9 @@ class FontTestWindow(TestWindow):
             self.load_font()
 
         if fill_width:
-            options['width'] = self.width - 10
+            options['width'] = self.width - 2*margin
 
-        self.label = font.Text(self.font, text, 5, 200, color=color, **options)
+        self.label = font.Text(self.font, text, margin, 200, color=color, **options)
         return self.label
 
     def on_draw(self):
@@ -109,32 +109,4 @@ class FontFixture(EventLoopFixture):
 @pytest.fixture
 def font_fixture(request):
     return FontFixture(request)
-
-
-class FontTestBase(WindowedTestCase):
-    """
-    Default test implementation. Use by creating a subclass and then calling the
-    `create_test_case` class method with the name of the test case and any class/instance
-    variables to set. This should be called outside the class definition!
-    """
-
-    # Defaults
-    font_name = ''
-    font_size = 24
-    text = 'Quickly brown fox'
-    color = 1, 1, 1, 1
-
-    def on_expose(self):
-        gl.glClearColor(0.5, 0, 0, 1)
-        gl.glClear(gl.GL_COLOR_BUFFER_BIT)
-        gl.glLoadIdentity()
-        self.draw()
-        self.window.flip()
-
-    def render(self):
-        fnt = font.load(self.font_name, self.font_size) 
-        self.label = font.Text(fnt, self.text, 10, 10, color=self.color)
-
-    def draw(self):
-        self.label.draw()
 
--- tests/interactive/font/test_text_label.py
+++ tests/interactive/font/test_text_label.py
@@ -22,6 +22,7 @@ def test_text_halign(font_fixture, halign):
             text=halign.upper(),
             halign=halign,
             fill_width=True,
+            margin=0,
             )
     font_fixture.ask_question(
             'Is the text horizontally {} aligned?'.format(halign),
@@ -43,4 +44,43 @@ def test_text_valign(font_fixture, valign, desc):
             )
     font_fixture.ask_question(
             'Is the text vertically {} aligned?\n{}'.format(valign, desc),
+            )
+
+
+@pytest.mark.parametrize('valign,halign', [
+    ('top', 'left'),
+    ('center', 'center'),
+    ('bottom', 'right')
+    ])
+def test_multiline_alignment(font_fixture, valign, halign):
+    """Test horizontal and vertical alignment with multi line text."""
+    w = font_fixture.create_window(
+            height=500,
+            )
+    w.create_label(
+            text='This text with multiple lines is aligned {}-{}'.format(valign, halign),
+            halign=halign,
+            valign=valign,
+            fill_width=True,
+            margin=0,
+            )
+    w.draw_baseline = True
+    font_fixture.ask_question(
+            'Is the text aligned {}-{}?'.format(valign, halign)
+            )
+
+
+@pytest.mark.parametrize('text,question', [
+    ('TEST TEST', 'TEST TEST should bo on a single line.'),
+    ('SPAM SPAM\nSPAM', 'SPAM should we twice on the first line, once on the second.')
+    ])
+def test_wrap_invariant(font_fixture, text, question):
+    """Test that text will not wrap when its width is set to its calculated width."""
+    w = font_fixture.create_window()
+    l = w.create_label(
+            text=text
+            )
+    l.width = l.width + 1
+    font_fixture.ask_question(
+            question
             )
