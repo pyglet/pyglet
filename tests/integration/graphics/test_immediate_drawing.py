62e2525ccd3dccfbe6b0347c316dc8f801fba435
Rob van der Most <Silvester747@gmail.com>
2015-09-13 13:28:03 +0200

    Skip immediate drawing tests for OSX. Not permanent solution.

--- tests/integration/graphics/test_immediate_drawing.py
+++ tests/integration/graphics/test_immediate_drawing.py
@@ -6,9 +6,11 @@ import unittest
 
 import pyglet
 
+from tests.annotations import Platform, skip_platform
 from .graphics_common import GraphicsGenericTestCase, get_feedback, GL_TRIANGLES
 
 
+@skip_platform(Platform.OSX)  # TODO: Check whether OpenGL < 3.0 or compatibility profile is enabled
 class ImmediateDrawingTestCase(GraphicsGenericTestCase, unittest.TestCase):
     def get_feedback(self, data):
         return get_feedback(lambda: pyglet.graphics.draw(self.n_vertices, GL_TRIANGLES, *data))
--- tests/integration/graphics/test_retained_drawing.py
+++ tests/integration/graphics/test_retained_drawing.py
@@ -6,9 +6,11 @@ import unittest
 
 import pyglet
 
+from tests.annotations import Platform, skip_platform
 from .graphics_common import GraphicsGenericTestCase, get_feedback, GL_TRIANGLES
 
 
+@skip_platform(Platform.OSX)  # TODO: Check whether OpenGL < 3.0 or compatibility profile is enabled
 class RetainedDrawingTestCase(GraphicsGenericTestCase, unittest.TestCase):
     def get_feedback(self, data):
         vertex_list = pyglet.graphics.vertex_list(self.n_vertices, *data)
