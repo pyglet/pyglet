7ec1b3c8871be0191bbe71554bc13113172bf1f9
Rob van der Most <silvester747@gmail.com>
2015-09-14 14:09:59 +0200

    Add test case for fire and forget playing of media.

--- tests/interactive/media/test_play_sound.py
+++ tests/interactive/media/test_play_sound.py
@@ -13,16 +13,25 @@ def test_playback(interactive, test_data):
     player.play()
 
     sound = test_data.get_file('media', 'alert.wav')
-    source = pyglet.media.load(sound, streaming= False)
+    source = pyglet.media.load(sound, streaming=False)
     player.queue(source)
 
-    interactive.user_verify('Can you hear the sound playing?', take_screenshot=False)
+    interactive.ask_question('Can you hear the sound playing?')
 
     sound2 = test_data.get_file('media', 'receive.wav')
     source2 = pyglet.media.load(sound2, streaming=False)
     player.queue(source2)
     player.next_source()
 
-    interactive.user_verify('Can you hear the second sound playing?', take_screenshot=False)
+    interactive.ask_question('Can you hear the second sound playing?')
 
+
+@pytest.mark.only_interactive
+def test_playback_fire_and_forget(interactive, test_data):
+    """Test playing back sound files using fire and forget."""
+    sound = test_data.get_file('media', 'alert.wav')
+    source = pyglet.media.load(sound, streaming=False)
+    source.play()
+
+    interactive.ask_question('Can you hear the sound playing?')
 
