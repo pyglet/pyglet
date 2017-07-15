77e38670f9f588d66b57cb8f138af061e931be0b
Serdar Yegulalp <thegline@gmail.com>
2017-07-15 18:51:51 +0000

    we don't even need a class, we can just substitute a function right in

--- pyglet/gl/lib_wgl.py
+++ pyglet/gl/lib_wgl.py
@@ -68,14 +68,8 @@ try:
 except AttributeError:
     _have_get_proc_address = False
 
-class WGLFunction(object):
-    __slots__ = ['name', 'requires', 'suggestions', 'ftype', 'func']
-    
-    def __call__(self,*a,**ka):
-        return self.func(*a,**ka)
-
 class WGLFunctionProxy(object):
-    __slots__ = ['name', 'requires', 'suggestions', 'ftype', 'func']
+    __slots__ = ['name', 'requires', 'suggestions', 'ftype']
 
     def __init__(self, name, ftype, requires, suggestions):
         assert _have_get_proc_address
@@ -83,7 +77,7 @@ class WGLFunctionProxy(object):
         self.ftype = ftype
         self.requires = requires
         self.suggestions = suggestions
-        self.func = None
+        #self.func = None
 
     def __call__(self, *args, **kwargs):
         from pyglet.gl import current_context
@@ -92,13 +86,13 @@ class WGLFunctionProxy(object):
                 'Call to function "%s" before GL context created' % self.name)
         address = wglGetProcAddress(asbytes(self.name))
         if cast(address, POINTER(c_int)):  # check cast because address is func
-            self.func = cast(address, self.ftype)
-            decorate_function(self.func, self.name)
+            func = cast(address, self.ftype)
+            decorate_function(func, self.name)
         else:
-            self.func = missing_function(
+            func = missing_function(
                 self.name, self.requires, self.suggestions)
-        self.__class__ = WGLFunction
-        return self.__call__(*args, **kwargs)
+        self = func
+        return func(*args, **kwargs)
 
 def link_GL(name, restype, argtypes, requires=None, suggestions=None):
     try:
