988204ecc84151e3c61906b02668c02c288b085c
Rob van der Most <Silvester747@gmail.com>
2017-11-22 18:01:28 +0100

    Default branch is now 1.4

--- doc/conf.py
+++ doc/conf.py
@@ -147,7 +147,7 @@ copyright = u'2006-2017, Alex Holkner'
 # built documents.
 #
 # The short X.Y version.
-version = '1.3'
+version = '1.4'
 # The full version, including alpha/beta/rc tags.
 release = pyglet.version
 
