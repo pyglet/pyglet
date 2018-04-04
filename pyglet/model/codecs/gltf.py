b99c3bd051b3287ed407ab6f24d3ff89e3bb814f
Benjamin Moran <benmoran@protonmail.com>
2018-04-04 18:14:59 +0900

    Some additional work on the glTF decoder. Not yet functional.

--- pyglet/model/codecs/gltf.py
+++ pyglet/model/codecs/gltf.py
@@ -48,7 +48,7 @@ from . import ModelDecodeException, ModelDecoder
 _struct_types = {
     GL_BYTE: 'b',
     GL_FLOAT: 'f',
-    GL_SHORT: 's',              # ('h')
+    GL_SHORT: 'h',              # ('h')
     GL_UNSIGNED_BYTE: 'B',
     GL_UNSIGNED_SHORT: 'H',     # ('S')
     GL_UNSIGNED_INT: 'I',
@@ -67,6 +67,23 @@ _accessor_types = {
 _targets = {
     GL_ELEMENT_ARRAY_BUFFER: "element_array",
     GL_ARRAY_BUFFER: "array",
+}
+
+# Vertex position 	"v[234][sifd]" 	"v[234]f"
+# Color 	"c[34][bBsSiIfd]" 	"c[34]B"
+# Edge flag 	"e1[bB]"
+# Fog coordinate 	"f[1234][bBsSiIfd]"
+# Normal 	"n3[bsifd]" 	"n3f"
+# Secondary color 	"s[34][bBsSiIfd]" 	"s[34]B"
+# Texture coordinate 	"[0-31]?t[234][sifd]" 	"[0-31]?t[234]f"
+# Generic attribute 	"[0-15]g(n)?[1234][bBsSiIfd]"
+
+_attributes = {
+    'POSITION': 'v',
+    'TEXCOORD_0': '0t',
+    'TEXCOORD_1': '1t',
+    'COLOR_0': 'c',
+    'NORMAL': 'n',
 }
 
 
@@ -106,27 +123,57 @@ def parse_gltf_file(filename, file=None):
     buffer_views = data.get('bufferViews', [])
     accessors = data.get('accessors', [])
 
-    for accessor in accessors:
+    def get_array(accessor):
+        # for accessor in accessors:
         # "required": ["bufferView", "byteOffset", "componentType", "count", "type"]
         buffer_view = buffer_views[accessor['bufferView']]
-        byte_offset = accessor['byteOffset']
+        accessor_byte_offset = accessor['byteOffset']
         component_type = accessor['componentType']
         count = accessor['count']
         data_type = accessor['type']
 
         size = _accessor_types[data_type] * count
         target = buffer_view['target']
+
         buffer = buffers[buffer_view['buffer']]
 
-        offset = buffer_view.get('byteOffset')
+        buffer_view_byte_offset = buffer_view.get('byteOffset')
         length = buffer_view.get('byteLength')
         stride = buffer_view.get('byteStride', 1)   # Default to 1, if no stride.
 
-        data = buffer.read(offset, length, stride)
-        arr = array.array(_struct_types[component_type], data)
+        offset = accessor_byte_offset + buffer_view_byte_offset
+        raw_bytes = buffer.read(offset, length, stride)
+        numeric_array = array.array(_struct_types[component_type], raw_bytes)
+        assert size == len(numeric_array)
 
-    # return mesh_list
-    return buffers, buffer_views
+        return numeric_array, _accessor_types[data_type]
+
+    vertex_lists = []
+
+    meshes = data.get('meshes')
+
+    for mesh in meshes:
+        indices = None
+
+        for primitive in mesh.get('primitives', []):
+            index = primitive.get('indices', None)
+            if index:
+                indices = get_array(accessors[index])
+
+            for attribute, i in primitive['attributes'].items():
+                arr, count = get_array(accessors[i])
+                attrib = _attributes[attribute]
+
+                fmt = "{0}{1}{2}".format(attrib, count, arr.typecode)
+
+                if indices:
+                    vlist = pyglet.graphics.vertex_list_indexed(count, indices, (fmt, arr))
+                else:
+                    vlist = pyglet.graphics.vertex_list(count, (fmt, arr))
+
+                vertex_lists.append(vlist)
+
+    return vertex_lists
 
 
 ###################################################
@@ -147,23 +194,26 @@ class GLTFModelDecoder(ModelDecoder):
         textures = {}
         vertex_lists = {}
 
-        # for mesh in mesh_list:
-        #     material = mesh.material
-        #     if material.texture_name:
-        #         texture = pyglet.resource.texture(material.texture_name)
-        #         group = TexturedMaterialGroup(material, texture)
-        #         textures[texture] = group
-        #     else:
-        #         group = MaterialGroup(material)
-        #
-        #     vlist = batch.add(len(mesh.vertices) // 3,
-        #                       GL_TRIANGLES,
-        #                       group,
-        #                       ('v3f/static', mesh.vertices),
-        #                       ('n3f/static', mesh.normals),
-        #                       ('t2f/static', mesh.tex_coords))
-        #
-        #     vertex_lists[vlist] = group
+        for mesh in mesh_list:
+            # material = mesh.material
+            # if material.texture_name:
+            #     texture = pyglet.resource.texture(material.texture_name)
+            #     group = TexturedMaterialGroup(material, texture)
+            #     textures[texture] = group
+            # else:
+            #     group = MaterialGroup(material)
+
+            diffuse = [1.0, 1.0, 1.0]
+            ambient = [1.0, 1.0, 1.0]
+            specular = [1.0, 1.0, 1.0]
+            emission = [0.0, 0.0, 0.0]
+            shininess = 100.0
+            opacity = 1.0
+            material = Material("Default", diffuse, ambient, specular, emission, shininess, opacity)
+
+            group = MaterialGroup(material=material)
+
+            vertex_lists[mesh] = group
 
         return Model(vertex_lists, textures, batch=batch)
 
