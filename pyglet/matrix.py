cc2c0947c51a1126d83dc0c856f0697539d042fa
Benjamin Moran <benmoran@protonmail.com>
2019-08-20 11:06:50 +0900

    matrix.Mat4 is now a tuple subclass for better compatibility with ctypes.

--- pyglet/matrix.py
+++ pyglet/matrix.py
@@ -1,6 +1,6 @@
 
 
-class Mat4:
+class Mat4(tuple):
     """A 4x4 Matrix
 
     `Mat4` is a simple 4x4 Matrix, with a few operators.
@@ -8,9 +8,8 @@ class Mat4:
     will perform elementwise multiplication, wheras the "@"
     operator will perform Matrix multiplication.
     """
-    __slots__ = 'array'
 
-    def __init__(self, array=None):
+    def __new__(cls, array=None):
         """Create a Matrix
 
         A Matrix can be created with tuple containing 16 floats
@@ -21,59 +20,12 @@ class Mat4:
             `array` : tuple of float or int
                 A tuple containing 16 values.
         """
-        self.array = array or (1.0, 0.0, 0.0, 0.0,
-                               0.0, 1.0, 0.0, 0.0,
-                               0.0, 0.0, 1.0, 0.0,
-                               0.0, 0.0, 0.0, 1.0)
-
-    def __add__(self, other):
-        assert isinstance(other, Mat4), "Only addition with Mat4 types is supported"
-        return Mat4(array=tuple(s + o for s, o in zip(self.array, other.array)))
-
-    def __sub__(self, other):
-        assert isinstance(other, Mat4), "Only subtraction from Mat4 types is supported"
-        return Mat4(array=tuple(s - o for s, o in zip(self.array, other.array)))
-
-    def __mul__(self, other):
-        assert isinstance(other, Mat4), "Only multiplication with Mat4 types is supported"
-        return Mat4(array=tuple(s * o for s, o in zip(self.array, other.array)))
-
-    def __matmul__(self, other):
-        assert isinstance(other, Mat4)
-        sarray = self.array
-        oarray = other.array
-        # Rows:
-        r0 = sarray[0:4]
-        r1 = sarray[4:8]
-        r2 = sarray[8:12]
-        r3 = sarray[12:16]
-        # Columns:
-        c0 = oarray[0::4]
-        c1 = oarray[1::4]
-        c2 = oarray[2::4]
-        c3 = oarray[3::4]
-
-        a = sum(s * o for s, o in zip(r0, c0))
-        b = sum(s * o for s, o in zip(r0, c1))
-        c = sum(s * o for s, o in zip(r0, c2))
-        d = sum(s * o for s, o in zip(r0, c3))
-
-        e = sum(s * o for s, o in zip(r1, c0))
-        f = sum(s * o for s, o in zip(r1, c1))
-        g = sum(s * o for s, o in zip(r1, c2))
-        h = sum(s * o for s, o in zip(r1, c3))
-
-        i = sum(s * o for s, o in zip(r2, c0))
-        j = sum(s * o for s, o in zip(r2, c1))
-        k = sum(s * o for s, o in zip(r2, c2))
-        l = sum(s * o for s, o in zip(r2, c3))
-
-        m = sum(s * o for s, o in zip(r3, c0))
-        n = sum(s * o for s, o in zip(r3, c1))
-        o = sum(s * o for s, o in zip(r3, c2))
-        p = sum(s * o for s, o in zip(r3, c3))
-
-        Mat4((a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p))
+        assert array is None or len(array) == 16,  "Mat4 requires 16 values"
+        array = array or (1.0, 0.0, 0.0, 0.0,
+                          0.0, 1.0, 0.0, 0.0,
+                          0.0, 0.0, 1.0, 0.0,
+                          0.0, 0.0, 0.0, 1.0)
+        return super().__new__(Mat4, array)
 
     @classmethod
     def create_orthogonal(cls, left, right, bottom, top, znear, zfar):
@@ -98,17 +50,52 @@ class Mat4:
     def create_perspective(cls, left, right, bottom, top, znear, zfar, fov=60):
         pass
 
-    def __getitem__(self, index):
-        if type(index) is tuple:
-            row, column = index
-            assert 0 <= row <= 3 and 0 <= column <= 3, "row and column must range from 0-3"
-            return self.array[row * 3 + column]
-        else:
-            return self.array[index]
+    def __add__(self, other):
+        assert isinstance(other, Mat4), "Only addition with Mat4 types is supported"
+        return Mat4(tuple(s + o for s, o in zip(self, other)))
 
-    def __iter__(self):
-        return iter(self.array)
+    def __sub__(self, other):
+        assert isinstance(other, Mat4), "Only subtraction from Mat4 types is supported"
+        return Mat4(tuple(s - o for s, o in zip(self, other)))
+
+    def __mul__(self, other):
+        assert isinstance(other, Mat4), "Only multiplication with Mat4 types is supported"
+        return Mat4(tuple(s * o for s, o in zip(self, other)))
+
+    def __matmul__(self, other):
+        assert isinstance(other, Mat4), "Only multiplication with Mat4 types is supported"
+        # Rows:
+        r0 = self[0:4]
+        r1 = self[4:8]
+        r2 = self[8:12]
+        r3 = self[12:16]
+        # Columns:
+        c0 = other[0::4]
+        c1 = other[1::4]
+        c2 = other[2::4]
+        c3 = other[3::4]
+
+        a = sum(s * o for s, o in zip(r0, c0))
+        b = sum(s * o for s, o in zip(r0, c1))
+        c = sum(s * o for s, o in zip(r0, c2))
+        d = sum(s * o for s, o in zip(r0, c3))
+
+        e = sum(s * o for s, o in zip(r1, c0))
+        f = sum(s * o for s, o in zip(r1, c1))
+        g = sum(s * o for s, o in zip(r1, c2))
+        h = sum(s * o for s, o in zip(r1, c3))
+
+        i = sum(s * o for s, o in zip(r2, c0))
+        j = sum(s * o for s, o in zip(r2, c1))
+        k = sum(s * o for s, o in zip(r2, c2))
+        l = sum(s * o for s, o in zip(r2, c3))
+
+        m = sum(s * o for s, o in zip(r3, c0))
+        n = sum(s * o for s, o in zip(r3, c1))
+        o = sum(s * o for s, o in zip(r3, c2))
+        p = sum(s * o for s, o in zip(r3, c3))
+
+        return Mat4((a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p))
 
     def __repr__(self):
-        arr = self.array
-        return f"{self.__class__.__name__}{arr[0:4]}\n    {arr[4:8]}\n    {arr[8:12]}\n    {arr[12:16]}"
+        return f"{self.__class__.__name__}{self[0:4]}\n    {self[4:8]}\n    {self[8:12]}\n    {self[12:16]}"
