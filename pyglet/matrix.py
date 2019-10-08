be2522789e40d0321083d125e5d842fb3565f2ad
Benjamin Moran <benmoran@protonmail.com>
2019-10-08 16:44:59 +0900

    Revert tests on pyglet.matrix

--- pyglet/matrix.py
+++ pyglet/matrix.py
@@ -2,7 +2,6 @@ import math as _math
 from functools import lru_cache as _lru_cache
 
 
-@_lru_cache()
 def create_orthogonal(left, right, bottom, top, znear, zfar):
     """Create a Mat4 with orthographic projection."""
     width = right - left
@@ -23,7 +22,6 @@ def create_orthogonal(left, right, bottom, top, znear, zfar):
                  tx, ty, tz, 1.0))
 
 
-@_lru_cache()
 def create_perspective(left, right, bottom, top, znear, zfar, fov=60):
     """Create a Mat4 with perspective projection."""
     width = right - left
@@ -50,25 +48,67 @@ def create_perspective(left, right, bottom, top, znear, zfar, fov=60):
                  0, 0, qn, 0))
 
 
-class Mat4(list):
+def rotate(matrix, angle=0, x=0, y=0, z=0):
+    # TODO: Fix this. NOT TESTED, and likely BROKEN
+    r = _math.radians(angle)
+    c = _math.cos(r)
+    s = _math.sin(r)
+
+    tempx, tempy, tempz = (1 - c) * x, (1 - c) * y, (1 - c) * z
+
+    ra = c + tempx * x
+    rb = 0 + tempx * y + s * z
+    rc = 0 + tempx * z - s * y
+
+    re = 0 + tempy * x - s * z
+    rf = c + tempy * y
+    rg = 0 + tempy * z + s * x
+
+    ri = 0 + tempz * x + s * y
+    rj = 0 + tempz * y - s * x
+    rk = c + tempz * z
+
+    # ra, rb, rc, --
+    # re, rf, rg, --
+    # ri, rj, rk, --
+    # --, --, --, --
+
+    rmat = Mat4((ra, rb, rc, 0, re, rf, rg, 0, ri, rj, rk, 0, 0, 0, 0, 0))
+
+    return matrix @ rmat
+
+
+def scale(matrix, value):
+    """Scale a matrix."""
+    temp = list(matrix)
+    temp[0] *= value
+    temp[5] *= value
+    temp[10] *= value
+    return Mat4(temp)
+
+
+def translate(matrix, x=0, y=0, z=0):
+    """Translate a matrix along x, y, and z axis."""
+    return Mat4((*matrix[:12], matrix[12] + x, matrix[13] + y, matrix[14] + z, matrix[15]))
+
+
+class Mat4(tuple):
     """A 4x4 Matrix
 
     `Mat4` is a simple immutable 4x4 Matrix, with a few operators.
     Two types of multiplication are possible. The "*" operator
     will perform elementwise multiplication, wheras the "@"
-    operator will perform Matrix multiplication. Internally
+    operator will perform Matrix multiplication. Internally,
     data is stored in a linear 1D array, allowing direct passing
     to OpenGL.
     """
 
-    __slots__ = []
-
-    def __init__(self, values=None):
+    def __new__(cls, values=None):
         """Create a 4x4 Matrix
 
         A Matrix can be created with list or tuple of 16 values.
         If nothing is provided, an "identity Matrix" will be created
-        (1.0 on the main diagonal).
+        (1.0 on the main diagonal). Matrix objects are immutable.
 
         :Parameters:
             `values` : tuple of float or int
@@ -79,62 +119,22 @@ class Mat4(list):
                             0.0, 1.0, 0.0, 0.0,
                             0.0, 0.0, 1.0, 0.0,
                             0.0, 0.0, 0.0, 1.0)
-        super().__init__(values)
-
-    def translate(self, x=0, y=0, z=0):
-        """Translate the Matrix along x, y, or z axis."""
-        self[12] += x
-        self[13] += y
-        self[14] += z
-
-    def scale(self, value):
-        """Scale the Matrix."""
-        self[0] *= value
-        self[5] *= value
-        self[10] *= value
-
-    def rotate(self, angle=0, x=0, y=0, z=0):
-        r = _math.radians(angle)
-        c = _math.cos(r)
-        s = _math.sin(r)
-
-        tempx, tempy, tempz = (1 - c) * x, (1 - c) * y, (1 - c) * z
-
-        ra = c + tempx * x
-        rb = 0 + tempx * y + s * z
-        rc = 0 + tempx * z - s * y
-
-        re = 0 + tempy * x - s * z
-        rf = c + tempy * y
-        rg = 0 + tempy * z + s * x
-
-        ri = 0 + tempz * x + s * y
-        rj = 0 + tempz * y - s * x
-        rk = c + tempz * z
-
-        # ra, rb, rc, --
-        # re, rf, rg, --
-        # ri, rj, rk, --
-        # --, --, --, --
-
-        rmat = Mat4((ra, rb, rc, 0, re, rf, rg, 0, ri, rj, rk, 0, 0, 0, 0, 0))
-
-        self[:] = self @ rmat
+        return super().__new__(Mat4, values)
 
     def __add__(self, other):
-        assert isinstance(other, Mat4), "Only addition with Mat4 types is supported"
+        assert isinstance(other, Mat4), "Can only add to other Mat4 types"
         return Mat4(tuple(s + o for s, o in zip(self, other)))
 
     def __sub__(self, other):
-        assert isinstance(other, Mat4), "Only subtraction from Mat4 types is supported"
+        assert isinstance(other, Mat4), "Can only subtract from other Mat4 types"
         return Mat4(tuple(s - o for s, o in zip(self, other)))
 
     def __mul__(self, other):
-        assert isinstance(other, Mat4), "Only multiplication with Mat4 types is supported"
+        assert isinstance(other, Mat4), "Can only multiply with other Mat4 types"
         return Mat4(tuple(s * o for s, o in zip(self, other)))
 
     def __matmul__(self, other):
-        assert isinstance(other, Mat4), "Only multiplication with Mat4 types is supported"
+        assert isinstance(other, Mat4), "Can only multiply with other Mat4 types"
         # Rows:
         r0 = self[0:4]
         r1 = self[4:8]
