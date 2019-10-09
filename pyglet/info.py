eba040b905473d96ffaf62a99eeb5e2154fb7497
Benjamin Moran <benmoran@protonmail.com>
2019-10-09 11:11:21 +0900

    Extend pyglet.info to print additional platform information.

--- pyglet/info.py
+++ pyglet/info.py
@@ -33,19 +33,21 @@
 # POSSIBILITY OF SUCH DAMAGE.
 # ----------------------------------------------------------------------------
 
-'''Get environment information useful for debugging.
+"""Get environment information useful for debugging.
 
 Intended usage is to create a file for bug reports, e.g.::
 
     python -m pyglet.info > info.txt
 
-'''
+"""
 from __future__ import print_function
 
 __docformat__ = 'restructuredtext'
 __version__ = '$Id: $'
 
 _first_heading = True
+
+
 def _heading(heading):
     global _first_heading
     if not _first_heading:
@@ -55,15 +57,25 @@ def _heading(heading):
     print(heading)
     print('-' * 78)
 
+
+def dump_platform():
+    """Dump OS specific """
+    import platform
+    print('platform: ', platform.platform())
+    print('release:  ', platform.release())
+    print('machine:  ', platform.machine())
+
+
 def dump_python():
-    '''Dump Python version and environment to stdout.'''
+    """Dump Python version and environment to stdout."""
     import os
     import sys
+    import platform
+    print('implementation:', platform.python_implementation())
     print('sys.version:', sys.version)
-    print('sys.platform:', sys.platform)
     print('sys.maxint:', sys.maxsize)
     if sys.platform == 'darwin':
-        try: 
+        try:
             from objc import __version__ as pyobjc_version
             print('objc.__version__:', pyobjc_version)
         except:
@@ -73,8 +85,9 @@ def dump_python():
         if key.startswith('PYGLET_'):
             print("os.environ['%s']: %s" % (key, value))
 
+
 def dump_pyglet():
-    '''Dump pyglet version and options.'''
+    """Dump pyglet version and options."""
     import pyglet
     print('pyglet.version:', pyglet.version)
     print('pyglet.compat_platform:', pyglet.compat_platform)
@@ -82,8 +95,9 @@ def dump_pyglet():
     for key, value in pyglet.options.items():
         print("pyglet.options['%s'] = %r" % (key, value))
 
+
 def dump_window():
-    '''Dump display, window, screen and default config info.'''
+    """Dump display, window, screen and default config info."""
     import pyglet.window
     display = pyglet.canvas.get_display()
     print('display:', repr(display))
@@ -99,33 +113,36 @@ def dump_window():
     dump_gl(window.context)
     window.close()
 
+
 def dump_gl(context=None):
-    '''Dump GL info.'''
+    """Dump GL info."""
     if context is not None:
         info = context.get_info()
     else:
         from pyglet.gl import gl_info as info
-    print('gl_info.get_version():',  info.get_version())
-    print('gl_info.get_vendor():',  info.get_vendor())
-    print('gl_info.get_renderer():',  info.get_renderer())
+    print('gl_info.get_version():', info.get_version())
+    print('gl_info.get_vendor():', info.get_vendor())
+    print('gl_info.get_renderer():', info.get_renderer())
     print('gl_info.get_extensions():')
     extensions = list(info.get_extensions())
     extensions.sort()
     for name in extensions:
         print('  ', name)
 
+
 def dump_glu():
-    '''Dump GLU info.'''
+    """Dump GLU info."""
     from pyglet.gl import glu_info
-    print('glu_info.get_version():',  glu_info.get_version())
+    print('glu_info.get_version():', glu_info.get_version())
     print('glu_info.get_extensions():')
     extensions = list(glu_info.get_extensions())
     extensions.sort()
     for name in extensions:
         print('  ', name)
 
+
 def dump_glx():
-    '''Dump GLX info.'''
+    """Dump GLX info."""
     try:
         from pyglet.gl import glx_info
     except:
@@ -153,13 +170,15 @@ def dump_glx():
         for name in glx_info.get_extensions():
             print('  ', name)
 
+
 def dump_media():
-    '''Dump pyglet.media info.'''
+    """Dump pyglet.media info."""
     import pyglet.media
     print('audio driver:', pyglet.media.get_audio_driver())
 
+
 def dump_ffmpeg():
-    '''Dump FFmpeg info.'''
+    """Dump FFmpeg info."""
     import pyglet
     pyglet.options['search_local_libs'] = True
     import pyglet.media
@@ -170,8 +189,9 @@ def dump_ffmpeg():
     else:
         print('FFmpeg not available.')
 
+
 def dump_al():
-    '''Dump OpenAL info.'''
+    """Dump OpenAL info."""
     try:
         from pyglet.media.drivers import openal
     except:
@@ -185,8 +205,9 @@ def dump_al():
     for extension in driver.get_extensions():
         print('  ', extension)
 
+
 def dump_wintab():
-    '''Dump WinTab info.'''
+    """Dump WinTab info."""
     try:
         from pyglet.input import wintab
     except:
@@ -198,8 +219,9 @@ def dump_wintab():
     spec_version = wintab.get_spec_version()
 
     print('WinTab: %s %d.%d (Spec %d.%d)' % (interface_name,
-        impl_version >> 8, impl_version & 0xff,
-        spec_version >> 8, spec_version & 0xff))
+                                             impl_version >> 8, impl_version & 0xff,
+                                             spec_version >> 8, spec_version & 0xff))
+
 
 def _try_dump(heading, func):
     _heading(heading)
@@ -209,8 +231,10 @@ def _try_dump(heading, func):
         import traceback
         traceback.print_exc()
 
+
 def dump():
-    '''Dump all information to stdout.'''
+    """Dump all information to stdout."""
+    _try_dump('Platform', dump_platform)
     _try_dump('Python', dump_python)
     _try_dump('pyglet', dump_pyglet)
     _try_dump('pyglet.window', dump_window)
@@ -220,6 +244,7 @@ def dump():
     _try_dump('pyglet.media.ffmpeg', dump_ffmpeg)
     _try_dump('pyglet.media.drivers.openal', dump_al)
     _try_dump('pyglet.input.wintab', dump_wintab)
+
 
 if __name__ == '__main__':
     dump()
