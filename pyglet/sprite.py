21b5c9e05a7a731b16b8707b7cea395e4dd2e435
Anton Hvornum <anton.feeds+github@gmail.com>
2019-10-14 21:25:14 +0200

    Fixed moving a non-instantiated Sprite()
    There won't be a `self._vertex_list`  until it's instantiated

--- pyglet/sprite.py
+++ pyglet/sprite.py
@@ -438,7 +438,8 @@ class Sprite(event.EventDispatcher):
                         int(vertices[2]), int(vertices[3]),
                         int(vertices[4]), int(vertices[5]),
                         int(vertices[6]), int(vertices[7]))
-        self._vertex_list.vertices[:] = vertices
+        if self._vertex_list:
+            self._vertex_list.vertices[:] = vertices
 
     def _update_color(self):
         r, g, b = self._rgb
