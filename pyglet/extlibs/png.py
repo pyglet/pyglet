bf13a7a5edf459026b74bbdb107a36e13028fa79
Benjamin Moran <benmoran@protonmail.com>
2020-01-24 16:30:26 +0900

    Updated license headers, and matrix module docstrings.

--- pyglet/extlibs/png.py
+++ pyglet/extlibs/png.py
@@ -1,58 +1,5 @@
-# ----------------------------------------------------------------------------
-# pyglet
-# Copyright (c) 2006-2008 Alex Holkner
-# Copyright (c) 2008-2019 pyglet contributors
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-#  * Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in
-#    the documentation and/or other materials provided with the
-#    distribution.
-#  * Neither the name of pyglet nor the names of its
-#    contributors may be used to endorse or promote products
-#    derived from this software without specific prior written
-#    permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-# ----------------------------------------------------------------------------
-
 # Retrieved from https://github.com/drj11/pypng
 # Revision: f5c4c76d81093b6c3f39f83b203f6832c496c110
-#
-# Pyglet Changelog
-# ----------------
-# * Removed shebang
-# * Added Pyglet license
-# * Converted to python_future
-
-# http://www.python.org/doc/2.2.3/whatsnew/node5.html
-from __future__ import generators
-from __future__ import division
-from __future__ import print_function
-from builtins import str
-from builtins import zip
-from builtins import map
-from builtins import range
-from builtins import object
-from functools import reduce
-from io import open
 
 # png.py - PNG encoder/decoder in pure Python
 #
@@ -201,14 +148,13 @@ __version__ = "0.0.18"
 import itertools
 import math
 import re
-# http://www.python.org/doc/2.4.4/lib/module-operator.html
 import operator
 import struct
 import sys
-# http://www.python.org/doc/2.4.4/lib/module-warnings.html
 import warnings
 import zlib
 
+from io import open
 from array import array
 from functools import reduce
 
--- pyglet/matrix.py
+++ pyglet/matrix.py
@@ -1,3 +1,48 @@
+# ----------------------------------------------------------------------------
+# pyglet
+# Copyright (c) 2006-2008 Alex Holkner
+# Copyright (c) 2008-2020 pyglet contributors
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in
+#    the documentation and/or other materials provided with the
+#    distribution.
+#  * Neither the name of pyglet nor the names of its
+#    contributors may be used to endorse or promote products
+#    derived from this software without specific prior written
+#    permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+# ----------------------------------------------------------------------------
+
+"""Matrix operations.
+
+This module provides a convenient :py:class:`~pyglet.matrix.Mat4` class for
+representing 4x4 matricies, and some associated helper functions for creating,
+rotating, scaling, and transforming these matrices. The internal datatype of
+:py:class:`~pyglet.matrix.Mat4` instances is a 1-dimensional array, so they
+can be passed directly to OpenGL.
+
+"""
+
 import math as _math
 import operator as _operator
 
@@ -90,7 +135,7 @@ def rotate(matrix, angle=0, x=0, y=0, z=0):
 class Mat4(tuple):
     """A 4x4 Matrix
 
-    `Mat4` is a simple immutable 4x4 Matrix, with a few operators.
+    `Mat4` is a simple immutable 4x4 matrix, with a few operators.
     Two types of multiplication are possible. The "*" operator
     will perform elementwise multiplication, wheras the "@"
     operator will perform Matrix multiplication. Internally,
