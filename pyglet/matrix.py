0b785dc8ffac0da86513e825552b49f839173596
Benjamin Moran <benmoran@protonmail.com>
2020-07-14 12:16:52 +0900

    Reorganized `pyglet.matrix` and started creation of unit tests.

--- pyglet/matrix.py
+++ pyglet/matrix.py
@@ -94,125 +94,6 @@ def create_perspective(left, right, bottom, top, znear, zfar, fov=60):
                  0, 0, qn, 0))
 
 
-def scale(matrix, x=1, y=1, z=1):
-    """Scale a Matrix on x, y, or z axis."""
-    temp = list(matrix)
-    temp[0] *= x
-    temp[5] *= y
-    temp[10] *= z
-    return Mat4(temp)
-
-
-def translate(matrix, x=0, y=0, z=0):
-    """Translate a Matrix along x, y, and z axis."""
-    return Mat4(matrix) @ Mat4((1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, x, y, z, 1))
-
-
-def rotate(matrix, angle=0, x=0, y=0, z=0):
-    """Rotate a Matrix on x, y, or z axis."""
-    assert all(abs(n) <= 1 for n in (x, y, z)), "x,y,z must be normalized (<=1)"
-    c = _math.cos(angle)
-    s = _math.sin(angle)
-    t = 1 - c
-    tempx, tempy, tempz = t * x, t * y, t * z
-
-    ra = c + tempx * x
-    rb = 0 + tempx * y + s * z
-    rc = 0 + tempx * z - s * y
-    re = 0 + tempy * x - s * z
-    rf = c + tempy * y
-    rg = 0 + tempy * z + s * x
-    ri = 0 + tempz * x + s * y
-    rj = 0 + tempz * y - s * x
-    rk = c + tempz * z
-
-    # ra, rb, rc, --
-    # re, rf, rg, --
-    # ri, rj, rk, --
-    # --, --, --, --
-
-    return Mat4(matrix) @ Mat4((ra, rb, rc, 0, re, rf, rg, 0, ri, rj, rk, 0, 0, 0, 0, 1))
-
-
-def transpose(matrix):
-    """The transpose of a Matrix."""
-    return Mat4(matrix[0::4] + matrix[1::4] + matrix[2::4] + matrix[3::4])
-
-
-def inverse(matrix):
-    """The inverse of a Matrix.
-
-    Using Gauss-Jordan elimination, the matrix supplied is transformed into
-    the identity matrix using a sequence of elementary row operations (below).
-    The same sequence of operations is applied to the identity matrix,
-    transforming it into the supplied matrix's inverse.
-
-    Elementary row operations:
-        - multiply row by a scalar
-        - swap two rows
-        - add two rows together"""
-
-    i = Mat4()  # identity matrix
-
-    # The pivot in each column is the element at Matrix[c, c] (diagonal elements).
-    # The pivot row is the row containing the pivot element. Pivot elements must
-    # be non-zero.
-    # Any time matrix is changed, so is i.
-    for c in range(4):
-        # Find and swap pivot row into place
-        if matrix[4*c + c] == 0:
-            for r in range(c + 1, 4):
-                if matrix[4*r + c] != 0:
-                    matrix = row_swap(matrix, c, r)
-                    i = row_swap(i, c, r)
-
-        # Make 0's in column for rows that aren't pivot row
-        for r in range(4):
-            if r != c:  # not the pivot row
-                r_piv = matrix[4*r + c]
-                if r_piv != 0:
-                    piv = matrix[4*c + c]
-                    scalar = r_piv / piv
-                    matrix = row_mul(matrix, c, scalar)
-                    matrix = row_sub(matrix, c, r)
-                    i = row_mul(i, c, scalar)
-                    i = row_sub(i, c, r)
-
-        # Put matrix in reduced row-echelon form.
-        piv = matrix[4*c + c]
-        matrix = row_mul(matrix, c, 1/piv)
-        i = row_mul(i, c, 1/piv)
-    return i
-
-
-def row_swap(matrix, r1, r2):
-    lo = min(r1, r2)
-    hi = max(r1, r2)
-    values = (matrix[:lo*4]
-            + matrix[hi*4:hi*4 + 4]
-            + matrix[lo*4 + 4:hi*4]
-            + matrix[lo*4:lo*4 + 4]
-            + matrix[hi*4 + 4:])
-    return Mat4(values)
-
-
-def row_mul(matrix, r, x):
-    values = (matrix[:r*4]
-            + tuple(v * x for v in matrix[r*4:r*4 + 4])
-            + matrix[r*4 + 4:])
-    return Mat4(values)
-
-
-# subtracts r1 from r2
-def row_sub(matrix, r1, r2):
-    row1 = matrix[4*r1:4*r1 + 4]
-    row2 = matrix[4*r2:4*r2 + 4]
-    values = (matrix[:r2*4]
-            + tuple(v2 - v1 for (v1, v2) in zip(row1, row2))
-            + matrix[r2*4 + 4:])
-    return Mat4(values)
-
-
 class Mat4(tuple):
     """A 4x4 Matrix
 
@@ -244,17 +125,60 @@ class Mat4(tuple):
         return super().__new__(Mat4, values)
 
     def row(self, index):
+        """Get a specific row as a tuple."""
         return self[index*4:index*4+4]
 
     def column(self, index):
+        """Get a specific column as a tuple."""
         return self[index::4]
 
+    def scale(self, x=1, y=1, z=1):
+        """Get a scale Matrix on x, y, or z axis."""
+        temp = list(self)
+        temp[0] *= x
+        temp[5] *= y
+        temp[10] *= z
+        return Mat4(temp)
+
+    def translate(self, x=0, y=0, z=0):
+        """Get a translate Matrix along x, y, and z axis."""
+        return Mat4(self) @ Mat4((1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, x, y, z, 1))
+
+    def rotate(self, angle=0, x=0, y=0, z=0):
+        """Get a rotation Matrix on x, y, or z axis."""
+        assert all(abs(n) <= 1 for n in (x, y, z)), "x,y,z must be normalized (<=1)"
+        c = _math.cos(angle)
+        s = _math.sin(angle)
+        t = 1 - c
+        tempx, tempy, tempz = t * x, t * y, t * z
+
+        ra = c + tempx * x
+        rb = 0 + tempx * y + s * z
+        rc = 0 + tempx * z - s * y
+        re = 0 + tempy * x - s * z
+        rf = c + tempy * y
+        rg = 0 + tempy * z + s * x
+        ri = 0 + tempz * x + s * y
+        rj = 0 + tempz * y - s * x
+        rk = c + tempz * z
+
+        # ra, rb, rc, --
+        # re, rf, rg, --
+        # ri, rj, rk, --
+        # --, --, --, --
+
+        return Mat4(self) @ Mat4((ra, rb, rc, 0, re, rf, rg, 0, ri, rj, rk, 0, 0, 0, 0, 1))
+
+    def transpose(self):
+        """Get a tranpose of this Matrix."""
+        return Mat4(self[0::4] + self[1::4] + self[2::4] + self[3::4])
+
     def __add__(self, other):
-        assert len(other) == 16, "Can only multiply with other Mat4 types"
+        assert len(other) == 16, "Can only add to other Mat4 types"
         return Mat4(tuple(s + o for s, o in zip(self, other)))
 
     def __sub__(self, other):
-        assert len(other) == 16, "Can only multiply with other Mat4 types"
+        assert len(other) == 16, "Can only subtract from other Mat4 types"
         return Mat4(tuple(s - o for s, o in zip(self, other)))
 
     def __pos__(self):
