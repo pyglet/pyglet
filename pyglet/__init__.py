49db89677596a978369995f658e8b680286ff7a3
Benjamin Moran <benmoran@protonmail.com>
2020-11-09 16:31:20 +0900

    Ready 1.5.9 release.

--- pyglet/__init__.py
+++ pyglet/__init__.py
@@ -61,7 +61,7 @@ _is_pyglet_doc_run = hasattr(sys, "is_pyglet_doc_run") and sys.is_pyglet_doc_run
 #:    >>> parse_version(pyglet.version) >= parse_version('1.1')
 #:    True
 #:
-version = '1.5.8'
+version = '1.5.9'
 
 
 if sys.version_info < (3, 5):
