2ed98c2b8c1b9946ad7f7540292635741301b41e
Einar Forselv <eforselv@gmail.com>
2020-11-16 14:53:37 +0100

    Extend model example

--- examples/3dmodel/model.py
+++ examples/3dmodel/model.py
@@ -1,9 +1,13 @@
+import math
 import pyglet
+pyglet.options['debug_gl_shaders'] = True
 from pyglet.gl import *
 
 window = pyglet.window.Window(width=720, height=480)
 window.projection = pyglet.window.Projection3D()
 batch = pyglet.graphics.Batch()
+time = 0
+
 
 @window.event
 def on_draw():
@@ -14,21 +18,33 @@ def on_draw():
 @window.event
 def on_mouse_drag(x, y, dx, dy, buttons, modifiers):
     rot = model.rotation
-    model.rotation = rot[0], rot[1] + dx / 10, rot[2] + dy / 10
+    model.rotation = rot[0], rot[1] + dx / 12, rot[2] - dy / 9
+
+    rot = model2.rotation
+    model2.rotation = rot[0], rot[1] + dx / 15, rot[2] - dy / 17
 
 
-def rotate(dt):
-    dt = dt * 100
+def animate(dt):
+    global time
+    time += dt
+
     rot = model.rotation
-    model.rotation = rot[0], rot[1] + dt, rot[2]
+    model.rotation = rot[0], rot[1] + dt * 27, rot[2] + dt * 35
+    model.translation = -1.5, 0, math.sin(time) * 0.7 - 4.0
+
+    rot = model2.rotation
+    model2.rotation = rot[0], rot[1] + dt * 20, rot[2] + dt * 45 
+    model2.translation = 1.5, 0, math.sin(-time) * 0.7 - 5.0
 
 
 if __name__ == "__main__":
-    glEnable(GL_MULTISAMPLE_ARB)
     glEnable(GL_DEPTH_TEST)
+    glEnable(GL_CULL_FACE)
 
-    model = pyglet.model.load("logo3d.obj", batch=batch)
+    model = pyglet.resource.model("logo3d.obj", batch=batch)
     model.translation = 0, 0, -2.2
+    model2 = pyglet.resource.model("box.obj", batch=batch)
+    model2.translation = 0, 0, -2.2
 
-    pyglet.clock.schedule_interval(rotate, 1/60)
+    pyglet.clock.schedule_interval(animate, 1/60)
     pyglet.app.run()
