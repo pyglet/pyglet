8456b3668eab660e71fc28d042323db9c8cf443d
pushfoo <36696816+pushfoo@users.noreply.github.com>
2021-05-13 15:46:49 -0400

    Remove unused import highlighted by lgtm bot

--- pyglet/canvas/base.py
+++ pyglet/canvas/base.py
@@ -32,8 +32,6 @@
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 # ----------------------------------------------------------------------------
-import warnings
-
 from pyglet import gl
 from pyglet import app
 from pyglet import window
