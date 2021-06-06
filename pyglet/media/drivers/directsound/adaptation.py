a7a509423575d2bfa28e27367ccf7f1854ab4fe5
Benjamin Moran <benmoran@protonmail.com>
2021-06-06 14:44:22 +0900

    #321 - Change DirectAudio gain2db function so that volume scales is more linear.

--- pyglet/media/drivers/directsound/adaptation.py
+++ pyglet/media/drivers/directsound/adaptation.py
@@ -56,12 +56,12 @@ def _gain2db(gain):
     Convert linear gain in range [0.0, 1.0] to 100ths of dB.
 
     Power gain = P1/P2
-    dB = 10 log(P1/P2)
+    dB = 2 log(P1/P2)
     dB * 100 = 1000 * log(power gain)
     """
     if gain <= 0:
         return -10000
-    return max(-10000, min(int(1000 * math.log10(min(gain, 1))), 0))
+    return max(-10000, min(int(1000 * math.log2(min(gain, 1))), 0))
 
 
 def _db2gain(db):
