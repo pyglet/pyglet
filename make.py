fce6287ecfacb5e8ac5bb6b960afa17fe5051c5b
Anurag Kumar <mailanu98@gmail.com>
2021-08-31 02:21:05 +0530

    check version of pip
    checking version of pip while installing pyglet

--- make.py
+++ make.py
@@ -12,6 +12,20 @@ DOC_DIR = op.join(THIS_DIR, 'doc')
 DIST_DIR = op.join(THIS_DIR, 'dist')
 GENDIST_TOOL = op.join(THIS_DIR, 'tools', 'gendist.sh')
 
+if sys.version_info < (3, 6):
+     pip_message = 'This may be due to an out of date pip. Make sure you have pip >= 9.0.1.'
+     try:
+         import pip
+         pip_version = tuple([int(x) for x in pip.__version__.split('.')[:3]])
+         if pip_version < (9, 0, 1) :
+             pip_message = 'Your pip version is out of date, please install pip >= 9.0.1. '\
+             'pip {} detected.'.format(pip.__version__)
+         else:
+             # pip is new enough - it must be something else
+             pip_message = ''
+     except Exception:
+         pass
+
 
 def clean():
     """Clean up all build artifacts, including generated documentation."""
