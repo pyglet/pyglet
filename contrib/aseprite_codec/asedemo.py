880ebd789d35efcd58d747b729c6d0522a5b75be
Benjamin Moran <benmoran@protonmail.com>
2021-09-17 22:52:10 +0900

    Clean up legacy OpenGL based code from /contrib.

--- contrib/aseprite_codec/asedemo.py
+++ contrib/aseprite_codec/asedemo.py
@@ -1,11 +1,11 @@
 import pyglet
-from pyglet.gl import *
 
 # Import the new codec
 import aseprite
 
 
 window = pyglet.window.Window()
+batch = pyglet.graphics.Batch()
 
 # Add image decoders from the loaded codec module.
 # This adds support to the image and resource modules.
@@ -13,16 +13,14 @@ pyglet.image.codecs.add_decoders(aseprite)
 
 # You can now load a animation of *.ase type:
 image = pyglet.image.load_animation("running.ase")
-sprite = pyglet.sprite.Sprite(img=image, x=50, y=50)
+sprite = pyglet.sprite.Sprite(img=image, x=100, y=100, batch=batch)
 sprite.scale = 8
 
 
 @window.event
 def on_draw():
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
     window.clear()
-    sprite.draw()
+    batch.draw()
 
 
 if __name__ == "__main__":
