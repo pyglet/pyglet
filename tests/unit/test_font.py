693217d801e83bc4ac7b0f7c6792539b1981f4f3
caffeinepills <vhxonline@gmail.com>
2022-04-11 20:56:28 -0500

    Support unicode font names for GDI (#584)
    * Allow alignment of text within width for a single line.
    
    * Implement unicode font names for GDI.

--- tests/unit/test_font.py
+++ tests/unit/test_font.py
@@ -11,7 +11,7 @@ def test_load_privatefont(test_data):
 
     pyglet.font.add_file(test_data.get_file('fonts', 'action_man.ttf'))
     action_man_font = pyglet.font.load("Action Man", size=12, dpi=96)
-    assert action_man_font.logfont.lfFaceName.decode("utf-8") == "Action Man"
+    assert action_man_font.logfont.lfFaceName == "Action Man"
 
 
 @require_platform(Platform.WINDOWS)
@@ -20,4 +20,4 @@ def test_load_privatefont_from_list(test_data):
 
     pyglet.font.add_file(test_data.get_file('fonts', 'action_man.ttf'))
     action_man_font = pyglet.font.load(["Action Man"], size=12, dpi=96)
-    assert action_man_font.logfont.lfFaceName.decode("utf-8") == "Action Man"
+    assert action_man_font.logfont.lfFaceName == "Action Man"
