85722bf31fc0ac102076f64ff1d15961484355ad
caffeinepills <vhxonline@gmail.com>
2022-05-19 09:41:57 -0500

    Enhancements to WintabTablet: - Pen button state is now passed during on_motion - Add support for ExpressKeys - WintabTabletCanvas.override_keys can be enabled to override global ExpressKeys configured in Wacom's application.
    Added 'debug_input' pyglet option for being able to debug input libraries.

--- examples/input/tablet.py
+++ examples/input/tablet.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import pyglet
+pyglet.options["debug_input"] = True
 
 window = pyglet.window.Window()
 tablets = pyglet.input.get_tablets()
@@ -25,10 +26,10 @@ def on_text(text):
     if not (0 <= index < len(tablets)):
         return
 
-    name = tablets[i].name
+    name = tablets[index].name
 
     try:
-        canvas = tablets[i].open(window)
+        canvas = tablets[index].open(window)
     except pyglet.input.DeviceException:
         print('Failed to open tablet %d on window' % index)
         return
@@ -44,9 +45,18 @@ def on_text(text):
         print('%s: on_leave(%r)' % (name, cursor))
 
     @canvas.event
-    def on_motion(cursor, x, y, pressure, tilt_x, tilt_y):
-        print('%s: on_motion(%r, %r, %r, %r, %r, %r)' % (name, cursor, x, y, pressure, tilt_x, tilt_y))
+    def on_motion(cursor, x, y, pressure, tilt_x, tilt_y, buttons):
+        print('%s: on_motion(%r, %r, %r, %r, %r, %r, %r)' % (name, cursor, x, y, pressure, tilt_x, tilt_y, buttons))
 
+    # If ExpressKey is supported for the OS, the events will be supported.
+    if 'on_express_key_press' in canvas.event_types:
+        @canvas.event
+        def on_express_key_press(control_id, location_id):
+            print(f'on_express_key_press(control_id={control_id}, location_id={location_id}')
+
+        @canvas.event
+        def on_express_key_release(control_id, location_id):
+            print(f'on_express_key_release(control_id={control_id}, location_id={location_id}')
 
 @window.event
 def on_mouse_press(x, y, button, modifiers):
