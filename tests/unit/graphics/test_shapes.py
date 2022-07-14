c78b58e438120823c5bca8bf0a6cffebb37bf41e
pushfoo <36696816+pushfoo@users.noreply.github.com>
2022-07-13 22:13:02 -0400

    Rename fixture to fit shape test filename

--- tests/unit/graphics/test_shapes.py
+++ tests/unit/graphics/test_shapes.py
@@ -9,26 +9,26 @@ from pyglet.shapes import (
 @pytest.fixture(params=[
     (Arc, (0, 0, 5)),
 ])
-def shape_instance(request):
+def rgba_shape(request):
     shape_type, required_args = request.param
     return shape_type(*required_args, color=(0, 255, 0, 37))
 
 
-def test_opacity_set_from_rgba_color_init_argument(shape_instance):
-    assert shape_instance.opacity == 37
+def test_opacity_set_from_rgba_color_init_argument(rgba_shape):
+    assert rgba_shape.opacity == 37
 
 
-def test_setting_opacity_does_not_change_rgb_channels(shape_instance):
-    original_color = shape_instance.color[:3]
-    shape_instance.opacity = 255
-    assert shape_instance.color[:3] == original_color
+def test_setting_opacity_does_not_change_rgb_channels(rgba_shape):
+    original_color = rgba_shape.color[:3]
+    rgba_shape.opacity = 255
+    assert rgba_shape.color[:3] == original_color
 
 
-def test_setting_rgba_color_changes_opacity(shape_instance):
-    shape_instance.color = (0, 0, 0, 0)
-    assert shape_instance.opacity == 0
+def test_setting_rgba_color_changes_opacity(rgba_shape):
+    rgba_shape.color = (0, 0, 0, 0)
+    assert rgba_shape.opacity == 0
 
 
-def test_setting_rgb_color_sets_full_opacity(shape_instance):
-    shape_instance.color = (0, 0, 255)
-    assert shape_instance.opacity == 255
+def test_setting_rgb_color_sets_full_opacity(rgba_shape):
+    rgba_shape.color = (0, 0, 255)
+    assert rgba_shape.opacity == 255
