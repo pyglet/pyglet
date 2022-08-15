ed9dbf0d228329ccf7a00dd75a65927adf23758b
pushfoo <36696816+pushfoo@users.noreply.github.com>
2022-08-15 12:42:22 -0400

    Add Sprite.update tests for rotation and scale arguments

--- tests/unit/test_sprite.py
+++ tests/unit/test_sprite.py
@@ -24,42 +24,93 @@ def _new_val_or_none(dimension_index: int = 0) -> Tuple[float, None]:
 
 
 @pytest.fixture(params=_new_val_or_none())
-def x_update_value(request):
+def x(request):
     return request.param
 
 
 @pytest.fixture(params=_new_val_or_none(1))
-def y_update_value(request):
+def y(request):
     return request.param
 
 
 @pytest.fixture(params=_new_val_or_none(2))
-def z_update_value(request):
+def z(request):
     return request.param
 
 
-def test_update_sets_passed_positions(dummy_sprite, x_update_value, y_update_value, z_update_value):
-
-    dummy_sprite.update(x=x_update_value, y=y_update_value, z=z_update_value)
-
-    if x_update_value is not None:
-        assert dummy_sprite.x == x_update_value
-
-    if y_update_value is not None:
-        assert dummy_sprite.y == y_update_value
-
-    if z_update_value is not None:
-        assert dummy_sprite.z == z_update_value
+@pytest.fixture(params=_new_val_or_none(3))
+def scale(request):
+    return request.param
 
 
-def test_update_leaves_unpassed_attributes_alone(dummy_sprite, x_update_value, y_update_value, z_update_value):
-    dummy_sprite.update(x=x_update_value, y=y_update_value, z=z_update_value)
+@pytest.fixture(params=_new_val_or_none(4))
+def scale_x(request):
+    return request.param
 
-    if x_update_value is None:
+
+@pytest.fixture(params=_new_val_or_none(5))
+def scale_y(request):
+    return request.param
+
+
+def test_update_sets_passed_positions(dummy_sprite, x, y, z):
+
+    dummy_sprite.update(x=x, y=y, z=z)
+
+    if x is not None:
+        assert dummy_sprite.x == x
+
+    if y is not None:
+        assert dummy_sprite.y == y
+
+    if z is not None:
+        assert dummy_sprite.z == z
+
+
+def test_update_leaves_unpassed_translations_alone(dummy_sprite, x, y, z):
+    dummy_sprite.update(x=x, y=y, z=z)
+
+    if x is None:
         assert dummy_sprite.x == 0
 
-    if y_update_value is None:
+    if y is None:
         assert dummy_sprite.y == 0
 
-    if z_update_value is None:
+    if z is None:
         assert dummy_sprite.z == 0
+
+
+def test_update_sets_passed_scales(dummy_sprite, scale, scale_x, scale_y):
+    dummy_sprite.update(scale=scale, scale_x=scale_x, scale_y=scale_y)
+
+    if scale is not None:
+        assert dummy_sprite.scale == scale
+
+    if scale_x is not None:
+        assert dummy_sprite.scale_x == scale_x
+
+    if scale_y is not None:
+        assert dummy_sprite.scale_y == scale_y
+
+
+def test_update_leaves_unpassed_scales_alone(dummy_sprite, x, y, z):
+    dummy_sprite.update(x=x, y=y, z=z)
+
+    if x is None:
+        assert dummy_sprite.x == 0
+
+    if y is None:
+        assert dummy_sprite.y == 0
+
+    if z is None:
+        assert dummy_sprite.z == 0
+
+
+def test_update_sets_rotation_when_passed(dummy_sprite):
+    dummy_sprite.update(rotation=3.0)
+    assert dummy_sprite.rotation == 3.0
+
+
+def test_update_leaves_rotation_alone_when_none(dummy_sprite):
+    dummy_sprite.update()
+    assert dummy_sprite.rotation == 0
