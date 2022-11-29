4f31ec4fdbaf61080bb403cb15dabda1fe794d0c
Alexander <90611665+alex-khod@users.noreply.github.com>
2022-11-29 15:28:21 +0800

    Fix: ShaderProgram calls batch.get_domain with `self._id` as program argument instead of `self` (#714)

--- pyglet/graphics/shader.py
+++ pyglet/graphics/shader.py
@@ -687,7 +687,7 @@ class ShaderProgram:
                 raise ShaderException(f"\nThe attribute `{name}` doesn't exist. Valid names: \n{list(attributes)}")
 
         batch = batch or pyglet.graphics.get_default_batch()
-        domain = batch.get_domain(False, mode, group, self._id, attributes)
+        domain = batch.get_domain(False, mode, group, self, attributes)
 
         # Create vertex list and initialize
         vlist = domain.create(count)
@@ -731,7 +731,7 @@ class ShaderProgram:
                 raise ShaderException(f"\nThe attribute `{name}` doesn't exist. Valid names: \n{list(attributes)}")
 
         batch = batch or pyglet.graphics.get_default_batch()
-        domain = batch.get_domain(True, mode, group, self._id, attributes)
+        domain = batch.get_domain(True, mode, group, self, attributes)
 
         # Create vertex list and initialize
         vlist = domain.create(count, len(indices))
