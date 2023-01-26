7803b465de20994ec2160e957fecbbd23b25d51c
caffeinepills <vhxonline@gmail.com>
2023-01-25 19:41:02 -0800

    Fix FFmpeg 4.0 not being found on MacOS

--- pyglet/media/codecs/ffmpeg_lib/libavutil.py
+++ pyglet/media/codecs/ffmpeg_lib/libavutil.py
@@ -13,7 +13,7 @@ _debug = debug_print('debug_media')
 avutil = pyglet.lib.load_library(
     'avutil',
     win32=('avutil-57', 'avutil-56'),
-    darwin=('avutil.57', 'avutil-56')
+    darwin=('avutil.57', 'avutil.56')
 )
 
 avutil.avutil_version.restype = c_int
