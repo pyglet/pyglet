15256062ff2f7237b6d726c2b15c022f72a0080a
Benjamin Moran <benmoran@protonmail.com>
2023-02-28 06:37:12 -0600

    Initialize the AudioDriver when the media module is first used.
    (cherry picked from commit 8d2215fc2ca796e55e94b1c2f2897d735d5a1e95)

--- pyglet/media/drivers/__init__.py
+++ pyglet/media/drivers/__init__.py
@@ -1,37 +1,3 @@
-# ----------------------------------------------------------------------------
-# pyglet
-# Copyright (c) 2006-2008 Alex Holkner
-# Copyright (c) 2008-2022 pyglet contributors
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-#  * Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in
-#    the documentation and/or other materials provided with the
-#    distribution.
-#  * Neither the name of pyglet nor the names of its
-#    contributors may be used to endorse or promote products
-#    derived from this software without specific prior written
-#    permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-# ----------------------------------------------------------------------------
 """Drivers for playing back media."""
 
 import atexit
@@ -41,59 +7,59 @@ import pyglet
 _debug = pyglet.options['debug_media']
 
 
+for driver_name in pyglet.options['audio']:
+    try:
+        if driver_name == 'pulse':
+            from . import pulse
+
+            _audio_driver = pulse.create_audio_driver()
+            break
+        elif driver_name == 'xaudio2':
+            from pyglet.libs.win32.constants import WINDOWS_8_OR_GREATER
+
+            if WINDOWS_8_OR_GREATER:
+                from . import xaudio2
+
+                _audio_driver = xaudio2.create_audio_driver()
+                break
+        elif driver_name == 'directsound':
+            from . import directsound
+
+            _audio_driver = directsound.create_audio_driver()
+            break
+        elif driver_name == 'openal':
+            from . import openal
+
+            _audio_driver = openal.create_audio_driver()
+            break
+        elif driver_name == 'silent':
+            from . import silent
+
+            _audio_driver = silent.create_audio_driver()
+            break
+    except Exception:
+        if _debug:
+            print(f'Error importing driver {driver_name}:')
+            import traceback
+
+            traceback.print_exc()
+else:
+    from . import silent
+
+    _audio_driver = silent.create_audio_driver()
+
+
 def get_audio_driver():
     """Get the preferred audio driver for the current platform.
 
-    Currently pyglet supports DirectSound, PulseAudio and OpenAL drivers. If
-    the platform supports more than one of those audio drivers, the
-    application can give its preference with :data:`pyglet.options` ``audio``
-    keyword. See the Programming guide, section
-    :doc:`/programming_guide/media`.
+    See :data:`pyglet.options` ``audio``, and the Programming guide,
+    section :doc:`/programming_guide/media` for more information on
+    setting the preferred driver.
 
     Returns:
-        AbstractAudioDriver : The concrete implementation of the preferred 
-        audio driver for this platform.
+        AbstractAudioDriver : The concrete implementation of the preferred
+                              audio driver for this platform.
     """
-    global _audio_driver
-
-    if _audio_driver:
-        return _audio_driver
-
-    _audio_driver = None
-
-    for driver_name in pyglet.options['audio']:
-        try:
-            if driver_name == 'pulse':
-                from . import pulse
-                _audio_driver = pulse.create_audio_driver()
-                break
-            elif driver_name == 'xaudio2':
-                from pyglet.libs.win32.constants import WINDOWS_8_OR_GREATER
-                if WINDOWS_8_OR_GREATER:
-                    from . import xaudio2
-                    _audio_driver = xaudio2.create_audio_driver()
-                    break
-            elif driver_name == 'directsound':
-                from . import directsound
-                _audio_driver = directsound.create_audio_driver()
-                break
-            elif driver_name == 'openal':
-                from . import openal
-                _audio_driver = openal.create_audio_driver()
-                break
-            elif driver_name == 'silent':
-                from . import silent
-                _audio_driver = silent.create_audio_driver()
-                break
-        except Exception:
-            if _debug:
-                print('Error importing driver %s:' % driver_name)
-                import traceback
-                traceback.print_exc()
-    else:
-        from . import silent
-        _audio_driver = silent.create_audio_driver()
-
     return _audio_driver
 
 
@@ -110,5 +76,4 @@ def _delete_audio_driver():
     _audio_driver = None
 
 
-_audio_driver = None
 atexit.register(_delete_audio_driver)
