8d2215fc2ca796e55e94b1c2f2897d735d5a1e95
Benjamin Moran <benmoran@protonmail.com>
2023-02-28 21:37:12 +0900

    Initialize the AudioDriver when the media module is first used.

--- pyglet/media/drivers/__init__.py
+++ pyglet/media/drivers/__init__.py
@@ -7,59 +7,59 @@ import pyglet
 _debug = pyglet.options['debug_media']
 
 
+for driver_name in pyglet.options['audio']:
+    try:
+        if driver_name == 'pulse':
+            from . import pulse
+
+            _audio_driver = pulse.create_audio_driver()
+            break
+        elif driver_name == 'xaudio2':
+            from pyglet.libs.win32.constants import WINDOWS_8_OR_GREATER
+
+            if WINDOWS_8_OR_GREATER:
+                from . import xaudio2
+
+                _audio_driver = xaudio2.create_audio_driver()
+                break
+        elif driver_name == 'directsound':
+            from . import directsound
+
+            _audio_driver = directsound.create_audio_driver()
+            break
+        elif driver_name == 'openal':
+            from . import openal
+
+            _audio_driver = openal.create_audio_driver()
+            break
+        elif driver_name == 'silent':
+            from . import silent
+
+            _audio_driver = silent.create_audio_driver()
+            break
+    except Exception:
+        if _debug:
+            print(f'Error importing driver {driver_name}:')
+            import traceback
+
+            traceback.print_exc()
+else:
+    from . import silent
+
+    _audio_driver = silent.create_audio_driver()
+
+
 def get_audio_driver():
     """Get the preferred audio driver for the current platform.
 
-    Currently pyglet supports DirectSound, PulseAudio and OpenAL drivers. If
-    the platform supports more than one of those audio drivers, the
-    application can give its preference with :data:`pyglet.options` ``audio``
-    keyword. See the Programming guide, section
-    :doc:`/programming_guide/media`.
+    See :data:`pyglet.options` ``audio``, and the Programming guide,
+    section :doc:`/programming_guide/media` for more information on
+    setting the preferred driver.
 
     Returns:
-        AbstractAudioDriver : The concrete implementation of the preferred 
-        audio driver for this platform.
+        AbstractAudioDriver : The concrete implementation of the preferred
+                              audio driver for this platform.
     """
-    global _audio_driver
-
-    if _audio_driver:
-        return _audio_driver
-
-    _audio_driver = None
-
-    for driver_name in pyglet.options['audio']:
-        try:
-            if driver_name == 'pulse':
-                from . import pulse
-                _audio_driver = pulse.create_audio_driver()
-                break
-            elif driver_name == 'xaudio2':
-                from pyglet.libs.win32.constants import WINDOWS_8_OR_GREATER
-                if WINDOWS_8_OR_GREATER:
-                    from . import xaudio2
-                    _audio_driver = xaudio2.create_audio_driver()
-                    break
-            elif driver_name == 'directsound':
-                from . import directsound
-                _audio_driver = directsound.create_audio_driver()
-                break
-            elif driver_name == 'openal':
-                from . import openal
-                _audio_driver = openal.create_audio_driver()
-                break
-            elif driver_name == 'silent':
-                from . import silent
-                _audio_driver = silent.create_audio_driver()
-                break
-        except Exception:
-            if _debug:
-                print(f'Error importing driver {driver_name}:')
-                import traceback
-                traceback.print_exc()
-    else:
-        from . import silent
-        _audio_driver = silent.create_audio_driver()
-
     return _audio_driver
 
 
@@ -76,5 +76,4 @@ def _delete_audio_driver():
     _audio_driver = None
 
 
-_audio_driver = None
 atexit.register(_delete_audio_driver)
