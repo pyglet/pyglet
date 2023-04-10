2975e2d317e70c1ac8f9d106c4e3ef19dd9e4927
pushfoo <36696816+pushfoo@users.noreply.github.com>
2023-04-10 05:24:26 -0400

    Add test for RGB channels getting set on caret correctly + helper class

--- tests/unit/text/test_caret_color.py
+++ tests/unit/text/test_caret_color.py
@@ -1,3 +1,4 @@
+from collections import UserList
 from unittest.mock import Mock, NonCallableMock
 
 import pytest
@@ -5,6 +6,20 @@ import pytest
 from pyglet.graphics.shader import ShaderProgram
 from pyglet.graphics.vertexdomain import IndexedVertexList
 from pyglet.text import layout, caret
+
+
+class ListSlicesAsTuple(UserList):
+    """
+    Mutable sequence which returns tuples when sliced like ctypes objects
+
+    It makes tests cleaner by allowing direct comparison to color tuples and
+    slices of them.
+    """
+
+    def __getitem__(self, item):
+        if isinstance(item, slice):
+            return tuple(super().__getitem__(item))
+        return super().__getitem__(item)
 
 
 @pytest.fixture(autouse=True)
@@ -28,7 +43,7 @@ def mock_layout():
     # Allow the shader program to create a mock vertex list on demand
     def _fake_vertex_list_method(count, mode, batch=None, group=None, colors=None):
         vertex_list = NonCallableMock(spec=IndexedVertexList)
-        vertex_list.colors = list(colors[1])
+        vertex_list.colors = ListSlicesAsTuple(colors[1])
         return vertex_list
 
     program.vertex_list = _fake_vertex_list_method
@@ -47,9 +62,18 @@ def rgb_caret(mock_layout, original_rgb_color):
     return caret.Caret(layout=mock_layout, color=original_rgb_color)
 
 
+@pytest.fixture
+def rgb_or_rgba_caret(mock_layout, original_rgb_or_rgba_color):
+    return caret.Caret(layout=mock_layout, color=original_rgb_or_rgba_color)
+
+
 def test_init_sets_opacity_to_255_when_rgb_color_argument(rgb_caret):
     assert rgb_caret.color[3] == 255
 
 
 def test_init_sets_opacity_from_rgba_value_as_color_argument(rgba_caret):
     assert rgba_caret.color[3] == 37
+
+
+def test_init_sets_rgb_channels_correctly(rgb_or_rgba_caret, original_rgb_or_rgba_color):
+    assert rgb_or_rgba_caret.color[:3] == original_rgb_or_rgba_color[:3]
