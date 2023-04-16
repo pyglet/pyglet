6a61ca73f16d39eefc136371c2d8aca5e127de3a
pushfoo <36696816+pushfoo@users.noreply.github.com>
2023-04-15 23:09:36 -0400

    Clean up imports in context managers

--- pyglet/libs/win32/context_managers.py
+++ pyglet/libs/win32/context_managers.py
@@ -28,26 +28,14 @@ After::
 Performance-critical code should avoid using these classes in favor of
 direct win32 API calls.
 """
-import contextlib
-
-import pyglet
+from contextlib import contextmanager
 from typing import Optional, Generator
-
-if pyglet.compat_platform in ('win32', 'cygwin'):
-    from ctypes.wintypes import HANDLE
-    from ctypes import WinError
-    from pyglet.libs.win32 import _user32 as user32
-
-else:  # Nasty local imports for testing on non-Win32 systems
-    from ctypes import c_void_p as HANDLE
-    from unittest.mock import Mock
-
-    user32 = Mock()
-    user32.GetDC = lambda a: HANDLE()
-    user32.attach_callable_mock('ReleaseDC', Mock())
+from ctypes.wintypes import HANDLE
+from ctypes import WinError
+from pyglet.libs.win32 import _user32 as user32
 
 
-@contextlib.contextmanager
+@contextmanager
 def device_context(window_handle: Optional[int] = None) -> Generator[HANDLE, None, None]:
     """
     A Windows device context wrapped as a context manager.
