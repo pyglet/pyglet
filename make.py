b378909d06bc570acecfc5a04d5ade7563e4aa4e
Grant Hur <fifttim@gmail.com>
2023-04-16 20:32:13 -0700

    Update make.py
    delete unneeded print().

--- make.py
+++ make.py
@@ -54,8 +54,7 @@ def dist():
 
 def _print_usage():
     print('Usage:', op.basename(sys.argv[0]), '<command>')
-    print('  where commands are:', ', '.join(avail_cmds))
-    print()
+    print('  where commands are:', ', '.join(avail_cmds), "\n")
     for name, cmd in avail_cmds.items():
         print(name, '\t', cmd.__doc__)
 
