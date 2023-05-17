8068b599186123dc9f352ccf3b4fb914e525c94c
caffeinepills <vhxonline@gmail.com>
2023-05-17 18:58:45 -0500

    Added support for FFmpeg 6.0. Tested with versions 4.4, 5.1, and 6.0; both audio and video. (#835)
    Added a warning if the loaded FFmpeg versions are not the expected versions we have tested with.
    FFmpeg debug message now shows some extra information when FFmpeg is not available.
    Added a missing print for OSError in the load_library to prevent hidden errors when loaded.
    Removed mp4 extension from WMF. (Experimental in the first place, and had no acceleration. Just use MP4 for FFmpeg for smoothest experience)
    
    (cherry picked from commit 515d7a52a63fc20d76109f06a02f059459ec03b8)

--- pyglet/media/codecs/__init__.py
+++ pyglet/media/codecs/__init__.py
@@ -1,39 +1,6 @@
-# ----------------------------------------------------------------------------
-# pyglet
-# Copyright (c) 2006-2008 Alex Holkner
-# Copyright (c) 2008-2022 pyglet contributors
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-#  * Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in
-#    the documentation and/or other materials provided with the
-#    distribution.
-#  * Neither the name of pyglet nor the names of its
-#    contributors may be used to endorse or promote products
-#    derived from this software without specific prior written
-#    permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-# ----------------------------------------------------------------------------
+import warnings
 
-from pyglet.util import Codecs, Decoder, Encoder
+from pyglet.util import CodecRegistry, Decoder, Encoder
 from .base import *
 
 import pyglet
@@ -131,9 +98,21 @@ def have_ffmpeg():
         from . import ffmpeg_lib
         if _debug:
             print('FFmpeg available, using to load media files.')
+
+        found = False
+        for release_versions, build_versions in ffmpeg_lib.release_versions.items():
+            if ffmpeg_lib.compat.versions == build_versions:
+                if _debug:
+                    print(f'FFMpeg Release Version: {release_versions}. Versions Loaded: {ffmpeg_lib.compat.versions}')
+                found = True
+                break
+
+        if not found:
+            warnings.warn(f'FFmpeg release version not found. This may be a new or untested release. Unknown behavior may occur. Versions Loaded: {ffmpeg_lib.compat.versions}')
+
         return True
 
-    except (ImportError, FileNotFoundError, AttributeError):
+    except (ImportError, FileNotFoundError, AttributeError) as err:
         if _debug:
-            print('FFmpeg not available.')
+            print(f'FFmpeg not available: {err}')
         return False
