8fd365a8e503c9fd86e98178ce748f539488fc2b
Grant Yul Hur <g.hur.personal@gmail.com>
2023-06-08 03:48:55 -0700

    Sugested changes

--- pyglet/clock.py
+++ pyglet/clock.py
@@ -391,31 +391,28 @@ class Clock:
 
             return False
 
-        """ 
-        sorted list is required to produce expected results
-        taken() will iterate through the heap, expecting it to be sorted
-        and will not always catch the smallest value, so sort here.
-        do not remove the sort key...it is faster than relaying comparisons
-        NOTE: do not rewrite as popping from heap, as that is super slow! 
-        """
+        # sorted list is required to produce expected results
+        # taken() will iterate through the heap, expecting it to be sorted
+        # and will not always catch the smallest value, so sort here.
+        # do not remove the sort key...it is faster than relaying comparisons
+        # NOTE: do not rewrite as popping from heap, as that is super slow! 
+
         self._schedule_interval_items.sort(key=_attrgetter('next_ts'))
 
-        """ 
-        Binary division over interval:
+        # Binary division over interval:
         
-        0                          interval
-        |--------------------------|
-          5  3   6   2   7  4  8   1          Order of search
+        # 0                          interval
+        # |--------------------------|
+        #   5  3   6   2   7  4  8   1          Order of search
         
-        i.e., first scheduled at interval,
-              then at            interval/2
-              then at            interval/4
-              then at            interval*3/4
-              then at            ...
+        # i.e., first scheduled at interval,
+        #       then at            interval/2
+        #       then at            interval/4
+        #       then at            interval*3/4
+        #       then at            ...
         
-        Schedule is hopefully then evenly distributed for any interval,
-        and any number of scheduled functions. 
-        """
+        # Schedule is hopefully then evenly distributed for any interval,
+        # and any number of scheduled functions.
 
         next_ts = last_ts + interval
         if not taken(next_ts, interval / 4):
