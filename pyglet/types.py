a1e9afeccfce11c3d4252ab36dc0ac4672843988
pushfoo <36696816+pushfoo@users.noreply.github.com>
2023-07-06 15:13:38 -0400

    Reduce comment length per discord feedback

--- pyglet/types.py
+++ pyglet/types.py
@@ -6,14 +6,5 @@ __all__ = [
     "Buffer"
 ]
 
-# This Union alias has the given name & members for multiple reasons:
-# 1. We can't use typing_extensions.Buffer because pyglet bans runtime
-#    Python dependencies.
-# 2. We can't use the official Buffer type from PEP-688 until 3.12
-#    becomes pyglet's minimum version. This is 2027 according to the
-#    current release schedule.
-# 3. typing.ByteString is deprecated as of 3.9
-# 4. ctypes.Array is useful for integrating with other data sources but
-#    will not be recognized without explicit inclusion due to lack of
-#    buffer protocol support.
+# Backwards compatible placeholder for `collections.abc.Buffer` from Python 3.12
 Buffer = Union[bytes, bytearray, memoryview, ctypes.Array]
