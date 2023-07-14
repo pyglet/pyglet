d41a011bed866203a7c362143756f35c5da8d890
pushfoo <36696816+pushfoo@users.noreply.github.com>
2023-07-14 16:48:33 -0400

    Switch the angle argument of shapes.Arc to use degrees

--- pyglet/shapes.py
+++ pyglet/shapes.py
@@ -462,7 +462,7 @@ class ShapeBase(ABC):
 class Arc(ShapeBase):
     _draw_mode = GL_LINES
 
-    def __init__(self, x, y, radius, segments=None, angle=math.tau, start_angle=0,
+    def __init__(self, x, y, radius, segments=None, angle=360.0, start_angle=0.0,
                  closed=False, color=(255, 255, 255, 255), batch=None, group=None):
         """Create an Arc.
 
@@ -481,10 +481,10 @@ class Arc(ShapeBase):
                 automatically calculated using the formula:
                 `max(14, int(radius / 1.25))`.
             `angle` : float
-                The angle of the arc, in radians. Defaults to tau (pi * 2),
-                which is a full circle.
+                The angle of the arc, in degrees. Defaults to 360.0, which is
+                a full circle.
             `start_angle` : float
-                The start angle of the arc, in radians. Defaults to 0.
+                The start angle of the arc, in degrees. Defaults to 0.
             `closed` : bool
                 If True, the ends of the arc will be connected with a line.
                 defaults to False.
@@ -532,12 +532,12 @@ class Arc(ShapeBase):
             x = -self._anchor_x
             y = -self._anchor_y
             r = self._radius
-            tau_segs = self._angle / self._segments
-            start_angle = self._start_angle - math.radians(self._rotation)
+            segment_radians = math.radians(self._angle) / self._segments
+            start_radians = math.radians(self._start_angle - self._rotation)
 
             # Calculate the outer points of the arc:
-            points = [(x + (r * math.cos((i * tau_segs) + start_angle)),
-                       y + (r * math.sin((i * tau_segs) + start_angle))) for i in range(self._segments + 1)]
+            points = [(x + (r * math.cos((i * segment_radians) + start_radians)),
+                       y + (r * math.sin((i * segment_radians) + start_radians))) for i in range(self._segments + 1)]
 
             # Create a list of doubled-up points from the points:
             vertices = []
