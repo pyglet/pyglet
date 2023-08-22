cb969f30d57d4339cfa0409f5bc0d933675a5f02
CORRUPTOR2037 <glados.ver17.dmw@gmail.com>
2023-08-22 21:59:33 +0300

    changed widgets example to show layout usage

--- examples/gui/widgets.py
+++ examples/gui/widgets.py
@@ -1,6 +1,6 @@
 import pyglet
 
-window = pyglet.window.Window(540, 500, caption="Widget Example")
+window = pyglet.window.Window(540, 500, caption="Widget Example", resizable=True)
 batch = pyglet.graphics.Batch()
 pyglet.gl.glClearColor(0.8, 0.8, 0.8, 1.0)
 
@@ -10,6 +10,10 @@ def on_draw():
     window.clear()
     batch.draw()
 
+@window.event
+def on_resize(width, height):
+    layout.size = (width, height)
+    frame.on_widgets_realign()
 
 ####################################
 # load resources to use for Widgets:
@@ -53,30 +57,65 @@ def text_entry_handler(text):
 # A Frame instance to hold all Widgets:
 frame = pyglet.gui.Frame(window, order=4)
 
+# A Layout instance to show widgets on the screen in a table order
+layout_style = {
+    'padding': 50,
+    'row-size': "50px",
+    'background': (220, 220, 240, 255),
+    'cell-background': (230, 230, 255, 255),
+    'cell-margin': 5,
+    'cell-padding': 5,
+}
+layout = pyglet.gui.Layout(0, 0, 
+                           width=window.width, height=window.height,
+                           rows=5, columns=2,
+                           batch=batch, style=layout_style)
+layout.set_column_margin(0, 20)
+layout.set_column_size(0, "200px")
+layout.set_cell_span(0, 0, colspan=2)
+layout.set_style('cell-content-alignment', ('center', 'bottom'))
 
-togglebutton = pyglet.gui.ToggleButton(100, 400, pressed=pressed, depressed=depressed, hover=hover, batch=batch)
+header_label = pyglet.text.Label("Menu", batch=batch, color=(0, 0, 0, 255))
+layout.cell(0, 0).content = header_label
+layout.cell(0, 0).set_style('background', (255,255,255,255))
+
+togglebutton = pyglet.gui.ToggleButton(0,0, pressed=pressed, depressed=depressed, hover=hover, batch=batch)
 togglebutton.set_handler('on_toggle', toggle_button_handler)
+toggle_label = pyglet.text.Label("Toggle Button: False", batch=batch, color=(0, 0, 0, 255))
+layout.cell(1, 0).content = toggle_label
+layout.cell(1, 1).content = togglebutton
 frame.add_widget(togglebutton)
-toggle_label = pyglet.text.Label("Toggle Button: False", x=300, y=400, batch=batch, color=(0, 0, 0, 255))
 
-
-pushbutton = pyglet.gui.PushButton(100, 300, pressed=pressed, depressed=depressed, hover=hover, batch=batch)
+pushbutton = pyglet.gui.PushButton(0,0, pressed=pressed, depressed=depressed, hover=hover, batch=batch)
 pushbutton.set_handler('on_press', push_button_handler)
 pushbutton.set_handler('on_release', release_button_handler)
+push_label = pyglet.text.Label("Push Button: False", batch=batch, color=(0, 0, 0, 255))
+layout.cell(2, 0).content = push_label
+layout.cell(2, 1).content = pushbutton
 frame.add_widget(pushbutton)
-push_label = pyglet.text.Label("Push Button: False", x=300, y=300, batch=batch, color=(0, 0, 0, 255))
 
-
-slider = pyglet.gui.Slider(100, 200, bar, knob, edge=5, batch=batch)
+slider = pyglet.gui.Slider(0,0, bar, knob, edge=5, batch=batch)
 slider.set_handler('on_change', slider_handler)
+
+slider_label = pyglet.text.Label("Slider Value: 0.0", batch=batch, color=(0, 0, 0, 255))
+layout.cell(3, 0).content = slider_label
+layout.cell(3, 1).content = slider
+layout.cell(3, 1).set_style('stretch-content-x', True)
 frame.add_widget(slider)
-slider_label = pyglet.text.Label("Slider Value: 0.0", x=300, y=200, batch=batch, color=(0, 0, 0, 255))
 
-
-text_entry = pyglet.gui.TextEntry("Enter Your Name", 100, 100, 150, batch=batch)
-frame.add_widget(text_entry)
+text_entry = pyglet.gui.TextEntry("Enter Your Name", 0,0, 0, batch=batch)
 text_entry.set_handler('on_commit', text_entry_handler)
-text_entry_label = pyglet.text.Label("Text: None", x=300, y=100, batch=batch, color=(0, 0, 0, 255))
+text_entry_label = pyglet.text.Label("Text: None", batch=batch, color=(0, 0, 0, 255))
+layout.cell(4, 0).content = text_entry_label
+layout.cell(4, 1).content = text_entry
+layout.cell(4, 1).set_style('stretch-content', True)
+frame.add_widget(text_entry)
 
+for i in range(5):
+    layout.cell(i, 0).set_style({'stretch-content': True, 'padding': 5})
+    layout.cell(i, 0).content.content_halign = 'right' if i > 0 else 'center'
+    layout.cell(i, 0).content.content_valign = 'center'
+
+frame.on_widgets_realign()
 
 pyglet.app.run()
