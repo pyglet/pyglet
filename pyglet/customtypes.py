4ef2e6f18d48187ebede735885e464faf609a05c
shenjack <3695888@qq.com>
2023-09-02 12:11:18 +0800

    improve custom types based on https://github.com/pyglet/pyglet/pull/928/files#diff-c6d7de478cd7e0cbe79e4e516884e0f3f0659abd5e243e2635a7bbe0a88d21f9

--- pyglet/customtypes.py
+++ pyglet/customtypes.py
@@ -1,12 +1,31 @@
 """Holds type aliases used throughout the codebase."""
 import ctypes
 
-from typing import Union
+from typing import Union, Tuple
 
-
-__all__ = [
-    "Buffer"
-]
+# just Number
+Number = Union[int, float]
 
 # Backwards compatible placeholder for `collections.abc.Buffer` from Python 3.12
 Buffer = Union[bytes, bytearray, memoryview, ctypes.Array]
+
+# Points
+Point2D = Tuple[Number, Number]
+Point3D = Tuple[Number, Number, Number]
+
+# Colors
+RGB = Tuple[int, int, int]
+RGBA = Tuple[int, int, int, int]
+Color = Union[RGB, RGBA]
+
+__all__ = [
+    'Number',
+    'Buffer',
+    
+    'Point2D',
+    'Point3D',
+    
+    'RGB',
+    'RGBA',
+    'Color',
+]
