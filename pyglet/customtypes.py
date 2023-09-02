01cd67580ec5f04589d97351b0fef0e0e97ba1ef
shenjack <3695888@qq.com>
2023-09-02 14:04:38 +0800

    imrove some detail

--- pyglet/customtypes.py
+++ pyglet/customtypes.py
@@ -6,6 +6,9 @@ from typing import Union, Tuple, TypeVar
 # just Number
 Number = Union[int, float]
 
+# Colors
+Channel = TypeVar('Channel', int, float)
+
 # Backwards compatible placeholder for `collections.abc.Buffer` from Python 3.12
 Buffer = Union[bytes, bytearray, memoryview, ctypes.Array]
 
@@ -13,8 +16,6 @@ Buffer = Union[bytes, bytearray, memoryview, ctypes.Array]
 Point2D = Tuple[Number, Number]
 Point3D = Tuple[Number, Number, Number]
 
-# Colors
-Channel = TypeVar('Channel')
 
 RGB = Tuple[Channel, Channel, Channel]
 RGBA = Tuple[Channel, Channel, Channel, Channel]
@@ -22,8 +23,19 @@ RGBA = Tuple[Channel, Channel, Channel, Channel]
 # Can be used as Color[int] or Color[float]
 Color = Union[RGB[Channel], RGBA[Channel]]
 
+# predefined Colors
+RGB255 = RGB[int]
+RGBFloat = RGB[float]
+
+RGBA255 = RGBA[int]
+RGBAFloat = RGBA[float]
+
+Color255 = Color[int]
+ColorFloat = Color[float]
+
 __all__ = [
     'Number',
+    'Channel',
     'Buffer',
     
     'Point2D',
@@ -32,4 +44,8 @@ __all__ = [
     'RGB',
     'RGBA',
     'Color',
+    
+    'RGB255', 'RGBFloat',
+    'RGBA255', 'RGBAFloat',
+    'Color255', 'ColorFloat',
 ]
