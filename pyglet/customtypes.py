9dd805f7d07cd07beb847397bfac790a08db0448
pushfoo <36696816+pushfoo@users.noreply.github.com>
2023-10-16 04:22:34 -0400

    Add forward-compatible ByteString union

--- pyglet/customtypes.py
+++ pyglet/customtypes.py
@@ -1,12 +1,19 @@
 """Holds type aliases used throughout the codebase."""
 import ctypes
+import sys
 
-from typing import Union
-
+from typing import Union, TYPE_CHECKING
 
 __all__ = [
-    "Buffer"
+    "Buffer",
+    'ByteString'
 ]
 
 # Backwards compatible placeholder for `collections.abc.Buffer` from Python 3.12
 Buffer = Union[bytes, bytearray, memoryview, ctypes.Array]
+
+# Handle deprecation of ByteString since 3.9
+if TYPE_CHECKING or sys.version_info >= (3, 14):
+    ByteString = Union[bytes, bytearray, memoryview]
+else:
+    from typing import ByteString
