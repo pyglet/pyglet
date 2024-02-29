17d97ed2295c39aec30cdcc0d802444d611a0732
Benjamin Moran <benmoran@protonmail.com>
2024-02-29 17:11:49 +0900

    tests: remove Linux tests assuming Arial font will always be available as default.

--- tests/integration/font/test_fontconfig.py
+++ tests/integration/font/test_fontconfig.py
@@ -1,13 +1,15 @@
 """
 Tests for font integration in all platforms.
 """
+from tests.annotations import Platform, skip_platform
 
 from pyglet import font
 
+
+@skip_platform(Platform.LINUX)  # Arial may not be installed
 def test_have_font():
     """
     Test functionality to check for availability of fonts.
     """
     assert font.have_font('Arial')
     assert not font.have_font('missing-font-name')
-
