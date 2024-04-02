d2a72410ed1d8c1b0472aec9431ab2049061cf82
Benjamin Moran <benmoran@protonmail.com>
2024-04-02 11:12:32 +0900

    input: minor cleanup after rebasing on master branch.

--- pyglet/input/base.py
+++ pyglet/input/base.py
@@ -625,17 +625,6 @@ class Controller(EventDispatcher):
         tscale = 1.0 / (control.max - control.min)
         scale = 2.0 / (control.max - control.min)
         bias = -1.0 - control.min * scale
-        if control.inverted:
-            scale = -scale
-            bias = -bias
-    def _bind_axis_control(self, relation, control, axis_name):
-        if not (control.min or control.max):
-            warnings.warn(f"Control('{control.name}') min & max values are both 0. Skipping.")
-            return
-
-        tscale = 1.0 / (control.max - control.min)
-        scale = 2.0 / (control.max - control.min)
-        bias = -1.0 - control.min * scale
         sign = 1.0
         if control.inverted:
             scale = -scale
@@ -645,18 +634,6 @@ class Controller(EventDispatcher):
         if relation.sign in (Sign.POSITIVE, Sign.NEGATIVE):
             setattr(self, f"_{axis_name}_sign", relation.sign)
 
-        # Analog to digital comparison operators and actuation limits
-        dpad_comparison_map = {Sign.NEGATIVE: (operator.lt, -0.1), Sign.POSITIVE: (operator.gt, 0.1)}
-
-        if axis_name in ("dpup", "dpdown"):
-            @control.event
-            def on_change(value):
-                normalized_value = value * scale + bias
-                compare, limit = dpad_comparison_map[self._dpup_sign]
-                setattr(self, 'dpup', compare(normalized_value, limit))
-                compare, limit = dpad_comparison_map[self._dpdown_sign]
-                setattr(self, 'dpdown', compare(normalized_value, limit))
-                self.dispatch_event('on_dpad_motion', self, self.dpleft, self.dpright, self.dpup, self.dpdown)
         dpad_defaults = {'dpup': Sign.POSITIVE, 'dpdown': Sign.NEGATIVE,
                          'dpleft': Sign.NEGATIVE, 'dpright': Sign.POSITIVE}
 
@@ -669,15 +646,6 @@ class Controller(EventDispatcher):
                 self.dpady = round(value * scale * sign + bias)     # normalized
                 self.dispatch_event('on_dpad_motion', self, Vec2(self.dpadx, self.dpady))
 
-        elif axis_name in ("dpleft", "dpright"):
-            @control.event
-            def on_change(value):
-                normalized_value = value * scale + bias
-                compare, limit = dpad_comparison_map[self._dpleft_sign]
-                setattr(self, 'dpleft', compare(normalized_value, limit))
-                compare, limit = dpad_comparison_map[self._dpright_sign]
-                setattr(self, 'dpright', compare(normalized_value, limit))
-                self.dispatch_event('on_dpad_motion', self, self.dpleft, self.dpright, self.dpup, self.dpdown)
         elif axis_name in ("dpleft", "dpright"):
             @control.event
             def on_change(value):
@@ -694,20 +662,10 @@ class Controller(EventDispatcher):
         elif axis_name in ("leftx", "lefty"):
             @control.event
             def on_change(value):
-                setattr(self, axis_name, value * scale + bias)  # normalized value
-                self.dispatch_event('on_stick_motion', self, "leftstick", self.leftx, -self.lefty)
-        elif axis_name in ("leftx", "lefty"):
-            @control.event
-            def on_change(value):
                 normalized_value = value * scale + bias
                 setattr(self, axis_name, normalized_value)
                 self.dispatch_event('on_stick_motion', self, "leftstick", Vec2(self.leftx, -self.lefty))
 
-        elif axis_name in ("rightx", "righty"):
-            @control.event
-            def on_change(value):
-                setattr(self, axis_name, value * scale + bias)  # normalized value
-                self.dispatch_event('on_stick_motion', self, "rightstick", self.rightx, -self.righty)
         elif axis_name in ("rightx", "righty"):
             @control.event
             def on_change(value):
