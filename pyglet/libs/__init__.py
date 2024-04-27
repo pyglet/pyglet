aba72ded5ec964921e3fadad2c26a5281e305e87
pushfoo <36696816+pushfoo@users.noreply.github.com>
2024-04-27 15:05:40 -0400

    Add top-level docstring for libs

--- pyglet/libs/__init__.py
+++ pyglet/libs/__init__.py
@@ -1,1 +1,9 @@
+"""Platform-specific support components.
 
+These consist of:
+
+#. :py:mod:`ctypes` bindings for datastructures and functions
+#. Pythonic wrappers around these
+
+Some or all of these may be auto-generated bindings.
+"""
