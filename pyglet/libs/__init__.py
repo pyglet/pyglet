b238fb3a31a589ea344715ea122d150b8dcaf8fd
pushfoo <36696816+pushfoo@users.noreply.github.com>
2024-05-13 18:28:14 -0400

    Explain libs quirks in its top-level docstring
    * Explain why pyglet.libs should use minimal formatting in docstrings
    
    * Mention pyglet.libs not being part of web doc (Ty @caffeinepills)
    
    * Add mention of vendored libraries which may be modified

--- pyglet/libs/__init__.py
+++ pyglet/libs/__init__.py
@@ -2,8 +2,18 @@
 
 These consist of:
 
-#. :py:mod:`ctypes` bindings for datastructures and functions
-#. Pythonic wrappers around these
+1. ctypes bindings for datastructures and functions
+2. pyglet-specific wrapper functions around raw ctypes calls
+3. vendored libraries in original or modified forms
 
-Some or all of these may be auto-generated bindings.
+When documenting these modules:
+
+1. Use minimal formatting in any docstrings
+2. Leave licenses at the tops of files in place
+
+Simple docstrings with minimal formatting are best because:
+
+1. No web doc is built for pyglet.lib
+2. The docstrings will be used to debug complex platform issues
+3. IDEs mangle formatting in any hover tooltips while debugging
 """
