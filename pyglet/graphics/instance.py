e919bee29237b207f0c5aa99115ed8aa1970ee6c
caffeinepills <vhxonline@gmail.com>
2024-06-26 22:27:16 -0500

    Instance typing

--- pyglet/graphics/instance.py
+++ pyglet/graphics/instance.py
@@ -1,6 +1,7 @@
+"""Concept for higher level instance generation and organization."""
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Protocol, Sequence
+from typing import TYPE_CHECKING, Any, Protocol, Sequence
 
 if TYPE_CHECKING:
     from pyglet.graphics import Batch
@@ -13,7 +14,9 @@ class VertexListObject(Protocol):
 
 
 class InstanceSourceMixin:
-    def __init__(self, attributes: Sequence[str]):
+    _vertex_list: VertexList
+
+    def __init__(self, attributes: Sequence[str]) -> None:
         self._attributes = tuple(attributes)
         self._instances = []
 
@@ -21,12 +24,12 @@ class InstanceSourceMixin:
     def instances(self) -> list[VertexInstance]:
         return self._instances
 
-    def create_instance(self, **kwargs) -> VertexInstance:
+    def create_instance(self, **kwargs: Any) -> VertexInstance:
         instance = self._vertex_list.add_instance(**kwargs)
         self._instances.append(instance)
         return instance
 
-    def pop(self):
+    def pop(self) -> VertexInstance:
         instance = self._instances.pop()
         instance.delete()
         return instance
@@ -35,7 +38,7 @@ class InstanceSourceMixin:
 class InstanceGenerator:
     _attributes: tuple[str, ...]
 
-    def __init__(self, source: VertexListObject, attributes: Sequence[str]):
+    def __init__(self, source: VertexListObject, attributes: Sequence[str]) -> None:
         self.source = source
         self._attributes = tuple(attributes)
         self._instances = []
@@ -52,23 +55,23 @@ class InstanceGenerator:
     def instance_attributes(self) -> tuple[str, ...]:
         return self._attributes
 
-    def migrate(self):
+    def migrate(self) -> None:
         vlist = self.source._vertex_list
         batch = self.source.batch
 
-        instanced_domain = batch.convert_to_instanced(vlist.domain, self._attributes)
+        instanced_domain = batch._convert_to_instanced(vlist.domain, self._attributes)
         vlist.set_instance_source(instanced_domain, self._attributes)
 
     @property
     def instances(self) -> list[VertexInstance]:
         return self._instances
 
-    def create(self, **kwargs) -> VertexInstance:
+    def create(self, **kwargs: Any) -> VertexInstance:
         instance = self.source._vertex_list.add_instance(**kwargs)
         self._instances.append(instance)
         return instance
 
-    def pop(self):
+    def pop(self) -> VertexInstance:
         instance = self._instances.pop()
         instance.delete()
         return instance
