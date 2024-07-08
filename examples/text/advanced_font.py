cef203c439ded508c3802569874f971a8ef8c588
shenjack <3695888@qq.com>
2024-07-08 21:47:47 +0800

    clean up for advanced_font

--- examples/text/advanced_font.py
+++ examples/text/advanced_font.py
@@ -1,25 +1,23 @@
-
-"""Example of advanced font rendering features. Currently only supported on Windows."""
-
+"""Example of advanced font rendering features."""
+# ruff: noqa: E501
 import pyglet
-pyglet.options["advanced_font_features"] = True
-
-if pyglet.compat_platform != 'win32':
-    print("This example is only for Windows")
-    exit()
+import os
 
 # On Windows, it's possible to change the font anti-aliasing mode.
 # Uncomment the below lines to set the options:
 #
-# from pyglet.font.directwrite import DirectWriteGlyphRenderer
-# D2D1_TEXT_ANTIALIAS_MODE_DEFAULT = 0
-# D2D1_TEXT_ANTIALIAS_MODE_CLEARTYPE = 1
-# D2D1_TEXT_ANTIALIAS_MODE_GRAYSCALE = 2
-# D2D1_TEXT_ANTIALIAS_MODE_ALIASED = 3
-# DirectWriteGlyphRenderer.antialias_mode = D2D1_TEXT_ANTIALIAS_MODE_ALIASED
+from pyglet.font.directwrite import DirectWriteGlyphRenderer
+D2D1_TEXT_ANTIALIAS_MODE_DEFAULT = 0
+D2D1_TEXT_ANTIALIAS_MODE_CLEARTYPE = 1 # not working?
+D2D1_TEXT_ANTIALIAS_MODE_GRAYSCALE = 2 # same as default
+D2D1_TEXT_ANTIALIAS_MODE_ALIASED = 3 # looks worse than grayscale
+DirectWriteGlyphRenderer.antialias_mode = D2D1_TEXT_ANTIALIAS_MODE_DEFAULT
 
 window = pyglet.window.Window()
 batch = pyglet.graphics.Batch()
+
+float_font_size = pyglet.text.Label("Hello World on size 14.1", font_name="Arial", font_size=14.1, x=50, y=500, batch=batch)
+float_font_size_1 = pyglet.text.Label("Hello World on size 15", font_name="Arial", font_size=15, x=270, y=500, batch=batch)
 
 arial_bold = pyglet.text.Label("Hello World 👽", font_name="Arial", bold=True, font_size=25, x=50, y=400, batch=batch)
 arial_black = pyglet.text.Label("Hello World 👾", font_name="Arial", bold="black", font_size=25, x=50, y=350, batch=batch)
@@ -32,16 +30,17 @@ segoe_ui_semilight = pyglet.text.Label("Hello World 🎱", font_name="Segoe UI",
 segoe_ui_light = pyglet.text.Label("Hello World 🥳👍", font_name="Segoe UI", bold="light", font_size=25, x=50, y=50, batch=batch)
 segoe_ui = pyglet.text.Label("Hello World 😀✌", font_name="Segoe UI", font_size=25, x=50, y=10, batch=batch)
 
-# On Windows DirectWrite can render directly to an image for special cases!
-# Note: Labels are recommended unless you know what you are doing, or if you use these in a limited fashion.
-font = pyglet.font.load("Segoe UI")
-image = font.render_to_image("I am rendered as a texture! 🌎", 100, 300)
-sprite = pyglet.sprite.Sprite(image, x=400, y=400, batch=batch)
-sprite.rotation = 45
+if os.name == "nt":
+    # On Windows DirectWrite can render directly to an image for special cases!
+    # Note: Labels are recommended unless you know what you are doing, or if you use these in a limited fashion.
+    font = pyglet.font.load("Segoe UI")
+    image = font.render_to_image("I am rendered as a texture! 🌎", 100, 300) # pyright: ignore reportAttributeAccessIssue
+    sprite = pyglet.sprite.Sprite(image, x=400, y=400, batch=batch)
+    sprite.rotation = 45
 
 
 @window.event
-def on_draw():
+def on_draw() -> None:  # noqa: D103
     window.clear()
     batch.draw()
 
