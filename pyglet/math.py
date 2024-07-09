d881fb0f3fdb74d4a693e2b25f9c4700ec99e015
pushfoo <36696816+pushfoo@users.noreply.github.com>
2024-07-09 07:31:53 -0400

    Put back Vec3.__rfloordiv__ lost in copy and paste accident

--- pyglet/math.py
+++ pyglet/math.py
@@ -486,7 +486,7 @@ class Vec3(_typing.NamedTuple):
                   other / self[2]  # type: ignore
             )
 
-    def __rfloordiv__(self, other: float | tuple[float, float, float]) -> Vec3:
+    def __floordiv__(self, other: float | tuple[float, float, float]) -> Vec3:
         try:
             return Vec3(
                 self[0] // other[0],  # type: ignore
@@ -498,6 +498,20 @@ class Vec3(_typing.NamedTuple):
                  self[0] // other,  # type: ignore
                  self[1] // other,  # type: ignore
                  self[2] // other   # type: ignore
+            )
+
+    def __rfloordiv__(self, other: float | tuple[float, float, float]) -> Vec3:
+        try:
+            return Vec3(
+                other[0] // self[0], # type: ignore
+                other[1] // self[1], # type: ignore
+                other[2] // self[2] # type: ignore
+            )
+        except TypeError:
+            return Vec3(
+                 other // self[0],  # type: ignore
+                 other // self[1],  # type: ignore
+                 other // self[2]  # type: ignore
             )
 
     def __abs__(self) -> Vec3:
