51f8fe1699b681e54904cd7be338ded14959ca81
pushfoo <36696816+pushfoo@users.noreply.github.com>
2024-07-09 06:46:35 -0400

    Fix body for Vec3.__pow__

--- pyglet/math.py
+++ pyglet/math.py
@@ -408,9 +408,17 @@ class Vec3(_typing.NamedTuple):
 
     def __pow__(self, other: Vec3 | tuple[float, float, float] | float) -> Vec3:
         try:
-            return Vec3(self[0] ** other[0], self[1] ** other[1], self[2] ** other[2])
+            return Vec3(
+                self[0] ** other[0],  # type: ignore
+                self[1] ** other[1],  # type: ignore
+                self[2] ** other[2]   # type: ignore
+            )
         except TypeError:
-            return Vec3(self[0] ** other, self[1] ** other, self[2] ** other)
+            return Vec3(
+                 self[0] ** other,  # type: ignore
+                 self[1] ** other,  # type: ignore
+                 self[2] ** other   # type: ignore
+            )
 
     def __lt__(self, other: Vec3) -> bool:
         return self[0] ** 2 + self[1] ** 2 + self[2] ** 2 < other[0] ** 2 + other[1] ** 2 + other[2] ** 2
