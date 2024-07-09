837c5e25e2c86d16b2f9ce520abecb2f4111a822
pushfoo <36696816+pushfoo@users.noreply.github.com>
2024-07-09 09:00:57 -0400

    Expand Vec4 immutability tests to cover swizzle

--- tests/unit/math/test_vec4.py
+++ tests/unit/math/test_vec4.py
@@ -62,6 +62,24 @@ def test_mutability():
     with pytest.raises(TypeError):
         v[0] = 1  # __setitem__ is not supported
 
+    # Swizzle is an output-only operation
+    with pytest.raises(AttributeError):
+        v.xyzw = (1, 2, 3, 4)
+    with pytest.raises(AttributeError):
+        v.yxzw = (2, 1, 3, 4)
+    with pytest.raises(AttributeError):
+        v.yzxw = (2, 3, 1, 4)
+    with pytest.raises(AttributeError):
+        v.zyxw = (3, 2, 1, 4)
+    with pytest.raises(AttributeError):
+        v.xy = (1, 2)
+    with pytest.raises(AttributeError):
+        v.xyz = (1, 2, 3)
+    with pytest.raises(AttributeError):
+        v.wzyx = (4, 3, 2, 1)
+    with pytest.raises(AttributeError):
+        v.xxxx = (1, 1, 1, 1)
+
 
 def test_len():
     """Len of the collection is always 2."""
