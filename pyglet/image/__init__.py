7de9e0c6f1b002950239f4f231f167a5d0228965
caffeinepills <vhxonline@gmail.com>
2024-07-17 02:07:55 -0500

    Add separate blit shader to resolve blending issue. (#1170)

--- pyglet/image/__init__.py
+++ pyglet/image/__init__.py
@@ -1386,7 +1386,7 @@ class Texture(AbstractImage):
         glBindVertexArray(vao_id)
 
         # Activate shader program:
-        program = pyglet.graphics.get_default_shader()
+        program = pyglet.graphics.get_default_blit_shader()
         program.use()
         pos_attrs = program.attributes['position']
         tex_attrs = program.attributes['tex_coords']
@@ -1415,7 +1415,7 @@ class Texture(AbstractImage):
 
         glDrawElements(GL_TRIANGLES, len(indices), GL_UNSIGNED_BYTE, 0)
         glFlush()
-
+        
         # Deactivate shader program:
         program.stop()
         # Discard everything after blitting:
