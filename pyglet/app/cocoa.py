8065bbc99c6f585231800bcf7e1de6df02955b23
Benjamin Moran <benmoran@protonmail.com>
2024-07-30 17:23:57 +0900

    macos: experiment with platform_event_loop in thread

--- pyglet/app/cocoa.py
+++ pyglet/app/cocoa.py
@@ -1,8 +1,12 @@
+from __future__ import annotations
+
 import signal
-from pyglet import app
+
+from threading import Thread
+
 from pyglet.app.base import PlatformEventLoop, EventLoop
-from pyglet.libs.darwin import cocoapy, AutoReleasePool, ObjCSubclass, PyObjectEncoding, ObjCInstance, send_super, \
-    ObjCClass, get_selector
+from pyglet.libs.darwin import cocoapy, AutoReleasePool, PyObjectEncoding, send_super
+from pyglet.libs.darwin import ObjCClass, ObjCSubclass, ObjCInstance, get_selector
 
 NSApplication = cocoapy.ObjCClass('NSApplication')
 NSMenu = cocoapy.ObjCClass('NSMenu')
@@ -13,18 +17,16 @@ NSUserDefaults = cocoapy.ObjCClass('NSUserDefaults')
 NSTimer = cocoapy.ObjCClass('NSTimer')
 
 
-
 def add_menu_item(menu, title, action, key):
     with AutoReleasePool():
         title = cocoapy.CFSTR(title)
         action = cocoapy.get_selector(action)
         key = cocoapy.CFSTR(key)
-        menuItem = NSMenuItem.alloc().initWithTitle_action_keyEquivalent_(
-            title, action, key)
-        menu.addItem_(menuItem)
+        menu_item = NSMenuItem.alloc().initWithTitle_action_keyEquivalent_(title, action, key)
+        menu.addItem_(menu_item)
 
         # cleanup
-        menuItem.release()
+        menu_item.release()
 
 
 def create_menu():
@@ -50,82 +52,86 @@ def create_menu():
 
 
 class _AppDelegate_Implementation:
+
     _AppDelegate = ObjCSubclass('NSObject', '_AppDelegate')
 
     @_AppDelegate.method(b'@' + PyObjectEncoding)
-    def init(self, pyglet_loop):
+    def init(self, pyglet_platform_loop):
         objc = ObjCInstance(send_super(self, 'init'))
-        self._pyglet_loop = pyglet_loop
+        self._pyglet_platform_loop = pyglet_platform_loop
         return objc  # objc is self
 
     @_AppDelegate.method('v')
     def updatePyglet_(self):
-        self._pyglet_loop.nsapp_step()
+        self._pyglet_platform_loop.step()
 
     @_AppDelegate.method('v@')
     def applicationWillTerminate_(self, notification):
-        self._pyglet_loop.is_running = False
-        self._pyglet_loop.has_exit = True
+        self._pyglet_platform_loop.is_running = False
+        self._pyglet_platform_loop.has_exit = True
 
     @_AppDelegate.method('v@')
     def applicationDidFinishLaunching_(self, notification):
-        self._pyglet_loop._finished_launching = True
+        self._pyglet_platform_loop._finished_launching = True
+
 
 _AppDelegate = ObjCClass('_AppDelegate')  # the actual class
 
-class CocoaAlternateEventLoop(EventLoop):
-    """This is an alternate loop developed mainly for ARM64 variants of macOS.
-    nextEventMatchingMask_untilDate_inMode_dequeue_ is very broken with ctypes calls. Events eventually stop
-    working properly after X returns. This event loop differs in that it uses the built-in NSApplication event
-    loop. We tie our schedule into it via timer.
-    """
-    def __init__(self):
-        super().__init__()
-        self.platform_event_loop = None
 
-    def run(self, interval=1/60):
-        if not interval:
-            self.clock.schedule(self._redraw_windows)
-        else:
-            self.clock.schedule_interval(self._redraw_windows, interval)
+# class CocoaAlternateEventLoop(EventLoop):
+#     """This is an alternate loop developed mainly for ARM64 variants of macOS.
+#     nextEventMatchingMask_untilDate_inMode_dequeue_ is very broken with ctypes calls. Events eventually stop
+#     working properly after X returns. This event loop differs in that it uses the built-in NSApplication event
+#     loop. We tie our schedule into it via timer.
+#     """
+#     def __init__(self):
+#         super().__init__()
+#         self.platform_event_loop = None
+#
+#     def run(self, interval=1/60):
+#         if not interval:
+#             self.clock.schedule(self._redraw_windows)
+#         else:
+#             self.clock.schedule_interval(self._redraw_windows, interval)
+#
+#         self.has_exit = False
+#
+#         from pyglet.window import Window
+#         Window._enable_event_queue = False
+#
+#         # Dispatch pending events
+#         for window in pyglet.app.windows:
+#             window.switch_to()
+#             window.dispatch_pending_events()
+#
+#         self.platform_event_loop = pyglet.app.platform_event_loop
+#
+#         self.dispatch_event('on_enter')
+#         self.is_running = True
+#         self.platform_event_loop.nsapp_start(interval)
+#
+#     def exit(self):
+#         """Safely exit the event loop at the end of the current iteration.
+#
+#         This method is a thread-safe equivalent for setting
+#         :py:attr:`has_exit` to ``True``.  All waiting threads will be
+#         interrupted (see :py:meth:`sleep`).
+#         """
+#         self.has_exit = True
+#         if self.platform_event_loop is not None:
+#             self.platform_event_loop.notify()
+#
+#         self.is_running = False
+#         self.dispatch_event('on_exit')
+#
+#         if self.platform_event_loop is not None:
+#             self.platform_event_loop.nsapp_stop()
 
-        self.has_exit = False
-
-        from pyglet.window import Window
-        Window._enable_event_queue = False
-
-        # Dispatch pending events
-        for window in app.windows:
-            window.switch_to()
-            window.dispatch_pending_events()
-
-        self.platform_event_loop = app.platform_event_loop
-
-        self.dispatch_event('on_enter')
-        self.is_running = True
-        self.platform_event_loop.nsapp_start(interval)
-
-    def exit(self):
-        """Safely exit the event loop at the end of the current iteration.
-
-        This method is a thread-safe equivalent for setting
-        :py:attr:`has_exit` to ``True``.  All waiting threads will be
-        interrupted (see :py:meth:`sleep`).
-        """
-        self.has_exit = True
-        if self.platform_event_loop is not None:
-            self.platform_event_loop.notify()
-
-        self.is_running = False
-        self.dispatch_event('on_exit')
-
-        if self.platform_event_loop is not None:
-            self.platform_event_loop.nsapp_stop()
 
 class CocoaPlatformEventLoop(PlatformEventLoop):
 
     def __init__(self):
-        super(CocoaPlatformEventLoop, self).__init__()
+        super().__init__()
         with AutoReleasePool():
             # Prepare the default application.
             self.NSApp = NSApplication.sharedApplication()
@@ -139,13 +145,13 @@ class CocoaPlatformEventLoop(PlatformEventLoop):
             # If we don't do this, new windows will not display on 10.8 after finishLaunching
             # has been called.
             defaults = NSUserDefaults.standardUserDefaults()
-            ignoreState = cocoapy.CFSTR("ApplePersistenceIgnoreState")
-            if not defaults.objectForKey_(ignoreState):
-                defaults.setBool_forKey_(True, ignoreState)
+            ignore_state = cocoapy.CFSTR("ApplePersistenceIgnoreState")
+            if not defaults.objectForKey_(ignore_state):
+                defaults.setBool_forKey_(True, ignore_state)
 
-            holdEnabled = cocoapy.CFSTR("ApplePressAndHoldEnabled")
-            if not defaults.objectForKey_(holdEnabled):
-                defaults.setBool_forKey_(False, holdEnabled)
+            hold_enabled = cocoapy.CFSTR("ApplePressAndHoldEnabled")
+            if not defaults.objectForKey_(hold_enabled):
+                defaults.setBool_forKey_(False, hold_enabled)
 
             self._finished_launching = False
 
@@ -157,44 +163,6 @@ class CocoaPlatformEventLoop(PlatformEventLoop):
                 self.NSApp.finishLaunching()
                 self.NSApp.activateIgnoringOtherApps_(True)
                 self._finished_launching = True
-
-    def nsapp_start(self, interval):
-        """Used only for CocoaAlternateEventLoop"""
-        from pyglet.app import event_loop
-        self._event_loop = event_loop
-
-        def term_received(*args):
-            if self.timer:
-                self.timer.invalidate()
-                self.timer = None
-
-            self.nsapp_stop()
-
-        # Force NSApp to close if Python receives sig events.
-        signal.signal(signal.SIGINT, term_received)
-        signal.signal(signal.SIGTERM, term_received)
-
-        self.appdelegate = _AppDelegate.alloc().init(self)
-        self.NSApp.setDelegate_(self.appdelegate)
-
-        self.timer = NSTimer.scheduledTimerWithTimeInterval_target_selector_userInfo_repeats_(
-             interval,  # Clamped internally to 0.0001 (including 0)
-             self.appdelegate,
-             get_selector('updatePyglet:'),
-             False,
-             True
-         )
-
-        self.NSApp.run()
-
-    def nsapp_step(self):
-        """Used only for CocoaAlternateEventLoop"""
-        self._event_loop.idle()
-        self.dispatch_posted_events()
-
-    def nsapp_stop(self):
-        """Used only for CocoaAlternateEventLoop"""
-        self.NSApp.terminate_(None)
 
     def step(self, timeout=None):
         with AutoReleasePool():
@@ -250,3 +218,94 @@ class CocoaPlatformEventLoop(PlatformEventLoop):
                 )
 
             self.NSApp.postEvent_atStart_(notifyEvent, False)
+
+
+class ArmPlatformEventLoop(PlatformEventLoop):
+
+    _event_loop: EventLoop
+    appdelegate: _AppDelegate
+    timer: NSTimer
+
+    def __init__(self, interval=1/60):
+        super().__init__()
+        with AutoReleasePool():
+            # Prepare the default application.
+            self.NSApp = NSApplication.sharedApplication()
+            if self.NSApp.isRunning():
+                # Application was already started by GUI library (e.g. wxPython).
+                return
+            if not self.NSApp.mainMenu():
+                create_menu()
+            self.NSApp.setActivationPolicy_(cocoapy.NSApplicationActivationPolicyRegular)
+            # Prevent Lion / Mountain Lion from automatically saving application state.
+            # If we don't do this, new windows will not display on 10.8 after finishLaunching
+            # has been called.
+            defaults = NSUserDefaults.standardUserDefaults()
+            ignore_state = cocoapy.CFSTR("ApplePersistenceIgnoreState")
+            if not defaults.objectForKey_(ignore_state):
+                defaults.setBool_forKey_(True, ignore_state)
+
+            hold_enabled = cocoapy.CFSTR("ApplePressAndHoldEnabled")
+            if not defaults.objectForKey_(hold_enabled):
+                defaults.setBool_forKey_(False, hold_enabled)
+
+            self._finished_launching = False
+
+        self._interval = interval
+        self._timer_thread = None
+
+    def start(self):
+        self._timer_thread = Thread(target=self._start_timer, daemon=True)
+        self._timer_thread.start()
+
+    def _start_timer(self):
+        from pyglet.app import event_loop
+        self._event_loop = event_loop
+
+        def _sigterm_received(*args):
+            if self.timer:
+                self.timer.invalidate()
+                self.timer = None
+            self.stop()
+
+        # Force NSApp to close if Python receives sig events.
+        signal.signal(signal.SIGINT, _sigterm_received)
+        signal.signal(signal.SIGTERM, _sigterm_received)
+
+        self.appdelegate = _AppDelegate.alloc().init(self)
+        self.NSApp.setDelegate_(self.appdelegate)
+
+        self.timer = NSTimer.scheduledTimerWithTimeInterval_target_selector_userInfo_repeats_(
+             self._interval,  # Clamped internally to 0.0001 (including 0)
+             self.appdelegate,
+             get_selector('updatePyglet:'),
+             False,
+             True
+        )
+
+        self.NSApp.run()
+
+    def step(self, timeout: float | None = None) -> None:
+        """Used only for CocoaAlternateEventLoop"""
+        self.dispatch_posted_events()
+
+    def stop(self):
+        """Used only for CocoaAlternateEventLoop"""
+        self.NSApp.terminate_(None)
+
+    def notify(self):
+        with (AutoReleasePool()):
+            notify_event = \
+                NSEvent.otherEventWithType_location_modifierFlags_timestamp_windowNumber_context_subtype_data1_data2_(
+                    cocoapy.NSApplicationDefined,  # type
+                    cocoapy.NSPoint(0.0, 0.0),     # location
+                    0,                             # modifierFlags
+                    0,                             # timestamp
+                    0,                             # windowNumber
+                    None,                          # graphicsContext
+                    0,                             # subtype
+                    0,                             # data1
+                    0,                             # data2
+                    )
+
+            self.NSApp.postEvent_atStart_(notify_event, False)
