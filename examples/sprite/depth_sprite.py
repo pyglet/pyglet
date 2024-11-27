e378c2d1fa1cb9c3760ae9efd7c1cfab799ce1a8
Benjamin Moran <benmoran@protonmail.com>
2024-11-27 14:20:30 +0900

    examples: rollback accidentally commited test

--- examples/sprite/depth_sprite.py
+++ examples/sprite/depth_sprite.py
@@ -11,36 +11,18 @@ window = pyglet.window.Window(height=250)
 fragment_source = """#version 150 core
     in vec4 vertex_colors;
     in vec3 texture_coords;
-    out vec4 final_color;
+    out vec4 final_colors;
 
     uniform sampler2D sprite_texture;
 
-    const float _alpha_clip = 0.01;
-
-    const mat4 dither_matrix = mat4(
-        vec4(0.0625, 0.5625, 0.1875, 0.6875),
-        vec4(0.8125, 0.3125, 0.9375, 0.4375),
-        vec4(0.25, 0.75, 0.125, 0.625),
-        vec4(1.0, 0.5, 0.875, 0.375)
-    );
-
-    float getValue(int x, int y) {
-        int mx = x % 4;
-        int my = y % 4;
-        return dither_matrix[mx][my];
-    }
-
-    void main() {
-        vec4 color = texture(sprite_texture, texture_coords.xy) * vertex_colors;
-
-        float limit = getValue(int(gl_FragCoord.x), int(gl_FragCoord.y));
-
-        if (color.a < limit) {
-             discard;
+    void main()
+    {
+        final_colors = texture(sprite_texture, texture_coords.xy) * vertex_colors;
+        
+        // No GL_ALPHA_TEST in core, use shader to discard.
+        if(final_colors.a < 0.01){
+            discard;
         }
-
-        final_color = vec4(color.rgb, 1.0);
-
     }
 """
 
@@ -55,12 +37,12 @@ class DepthSpriteGroup(pyglet.sprite.SpriteGroup):
         glEnable(GL_BLEND)
         glBlendFunc(self.blend_src, self.blend_dest)
 
-        # glEnable(GL_DEPTH_TEST)
-        # glDepthFunc(GL_LESS)
+        glEnable(GL_DEPTH_TEST)
+        glDepthFunc(GL_LESS)
 
     def unset_state(self):
-        # glDisable(GL_BLEND)
-        # glDisable(GL_DEPTH_TEST)
+        glDisable(GL_BLEND)
+        glDisable(GL_DEPTH_TEST)
         self.program.stop()
 
 
@@ -86,8 +68,7 @@ depth_shader = pyglet.graphics.shader.ShaderProgram(vertex_shader, fragment_shad
 def make_sprite(zvalue):
     sprite = DepthSprite(image, x=0, y=0, z=zvalue, batch=batch, program=depth_shader)
     # Random color multiplier.
-    sprite.color = (random.randint(100, 255), random.randint(100, 255), random.randint(100, 255))
-    sprite.opacity = 60
+    sprite.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
     # Add sprites to keep in memory, like a list. Otherwise, they will get GC'd when out of scope.
     sprites.append(sprite)
 
@@ -107,7 +88,6 @@ def update(dt):
         sprite.update(x=i * .75 * (image.width - 15),
                       y=.25 * window.height * (1 + math.cos(2 * elapsed + i * math.pi / sprite_count)))
         sprite.z = sprite.y
-        # sprite.opacity = sprite.y + 50
     elapsed += dt
 
 
