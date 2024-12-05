7ba3da9b10ca97b50c6f950ccecfaa194cf32450
caffeinepills <vhxonline@gmail.com>
2024-12-04 21:34:09 -0600

    Add support for FFmpeg 7.1 (#1241)
    * Add support for FFMpeg 7.x, tested on 7.1
    * Update docs to add versioning.

--- pyglet/media/codecs/ffmpeg_lib/libswresample.py
+++ pyglet/media/codecs/ffmpeg_lib/libswresample.py
@@ -1,37 +1,50 @@
 """Wrapper for include/libswresample/swresample.h
 """
-from ctypes import c_int, c_int64
+from ctypes import c_int, c_int64, c_char_p, c_double
 from ctypes import c_uint8
 from ctypes import c_void_p, POINTER, Structure
 
 import pyglet.lib
 from pyglet.util import debug_print
 from . import compat
+from .libavutil import AVChannelLayout, AVSampleFormat
 
 _debug = debug_print('debug_media')
 
 swresample = pyglet.lib.load_library(
     'swresample',
-    win32=('swresample-4', 'swresample-3'),
-    darwin=('swresample.4', 'swresample.3')
+    win32=('swresample-5', 'swresample-4', 'swresample-3'),
+    darwin=('swresample.5', 'swresample.4', 'swresample.3')
 )
 
 swresample.swresample_version.restype = c_int
 
-compat.set_version('swresample', swresample.swresample_version() >> 16)
+swresample_version = swresample.swresample_version() >> 16
 
+compat.set_version('swresample', swresample_version)
 
 SWR_CH_MAX = 32
-
 
 class SwrContext(Structure):
     pass
 
 
-swresample.swr_alloc_set_opts.restype = POINTER(SwrContext)
-swresample.swr_alloc_set_opts.argtypes = [POINTER(SwrContext),
-                                          c_int64, c_int, c_int, c_int64,
-                                          c_int, c_int, c_int, c_void_p]
+if swresample_version < 5:
+    swresample.swr_alloc_set_opts.restype = POINTER(SwrContext)
+    swresample.swr_alloc_set_opts.argtypes = [POINTER(SwrContext),
+                                              c_int64, c_int, c_int, c_int64,
+                                              c_int, c_int, c_int, c_void_p]
+else:
+    swresample.swr_alloc.restype = POINTER(SwrContext)
+    swresample.swr_alloc.argtypes = []
+
+    swresample.swr_alloc_set_opts2.restype = c_int
+    swresample.swr_alloc_set_opts2.argtypes = [POINTER(POINTER(SwrContext)),
+                                              POINTER(AVChannelLayout), AVSampleFormat, c_int,
+                                              POINTER(AVChannelLayout), AVSampleFormat, c_int,
+                                              c_int, c_void_p]
+
+
 swresample.swr_init.restype = c_int
 swresample.swr_init.argtypes = [POINTER(SwrContext)]
 swresample.swr_free.argtypes = [POINTER(POINTER(SwrContext))]
@@ -50,5 +63,6 @@ swresample.swr_get_out_samples.argtypes = [POINTER(SwrContext), c_int]
 __all__ = [
     'swresample',
     'SWR_CH_MAX',
-    'SwrContext'
+    'SwrContext',
+    'swresample_version',
 ]
