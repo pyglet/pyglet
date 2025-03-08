187ee95e21b3822445cf3a38e65bb05761ef59ea
caffeinepills <ragnarok.cam@gmail.com>
2025-03-07 18:47:14 -0600

    Revert "Formatted text optimization"

--- pyglet/text/layout/incremental.py
+++ pyglet/text/layout/incremental.py
@@ -290,8 +290,7 @@ class IncrementalTextLayout(TextLayout, EventDispatcher):
                 self.glyphs[start:end] = font.get_glyphs(text)
 
         # Update owner runs
-        owner_runs = self._get_owner_runs(self.glyphs[invalid_start:invalid_end])
-        self._owner_runs.set_runs(invalid_start, invalid_end, owner_runs)
+        self._get_owner_runs(self._owner_runs, self.glyphs, invalid_start, invalid_end)
 
         # Updated glyphs need flowing
         self._invalid_flow.invalidate(invalid_start, invalid_end)
