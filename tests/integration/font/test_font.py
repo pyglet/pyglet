a2d8b05a39e226d96d7d1a5706047b9bd5fe8e00
caffeinepils <vhxonline@gmail.com>
2025-06-27 15:39:47 -0500

    Linux runners don't have Arial. Use DejaVu Sans

--- tests/integration/font/test_font.py
+++ tests/integration/font/test_font.py
@@ -1,25 +1,35 @@
-from unittest.mock import patch
-
 import pyglet
+from tests.annotations import skip_platform, require_platform, Platform
 
 
 def test_font_create_default(test_data):
     ft = pyglet.font.load()
     assert ft.name is not None
 
+
 def test_default_platform_font():
     """Ensure the platform has a default font from the manager."""
     assert pyglet.font.manager.get_platform_default_name() is not None
 
+
 def test_missing_font():
     assert not pyglet.font.have_font('definitely-doesnt-exist-font')
 
+
+@skip_platform(Platform.LINUX)
 def test_load_no_custom_from_list(test_data):
     # First found font, should be Arial since Action Man is not loaded.
-    myfont = pyglet.font.load(["Action Man", "Times New Roman"], size=12, dpi=96)
-    assert myfont.name == "Times New Roman"
+    myfont = pyglet.font.load(["Action Man", "Arial"], size=12, dpi=96)
+    assert myfont.name == "Arial"
     # Make sure name resolves to an actual found font.
-    assert pyglet.font.manager.get_resolved_name(["Action Man", "Times New Roman"]) == 'Times New Roman'
+    assert pyglet.font.manager.get_resolved_name(["Action Man", "Arial"]) == 'Times New Roman'
+
+@require_platform(Platform.LINUX)  # Same as above, but Linux runner uses DejaVu Sans.
+def test_load_no_custom_from_list(test_data):
+    myfont = pyglet.font.load(["Action Man", "DejaVu Sans"], size=12, dpi=96)
+    assert myfont.name == "DejaVu Sans"
+    # Make sure name resolves to an actual found font.
+    assert pyglet.font.manager.get_resolved_name(["Action Man", "DejaVu Sans"]) == 'DejaVu Sans'
 
 def test_load_privatefont(test_data):
     file = test_data.get_file('fonts', 'action_man.ttf')
@@ -40,6 +50,7 @@ def test_load_privatefont_from_list(test_data):
 
     # List should resolve to actual font name, overwriting from test_load_no_custom_from_list
     assert pyglet.font.manager.get_resolved_name(["Action Man", "Arial"]) == "Action Man"
+
 
 # Not sure how to properly test a dispatched event, but this seems to work.
 def test_font_load_callback(test_data):
