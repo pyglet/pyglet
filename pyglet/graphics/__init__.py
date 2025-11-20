62f5991905a0522b5b9e95536ec32765063d5ef2
caffeinepills <vhxonline@gmail.com>
2025-11-20 14:09:36 -0600

    Doc update

--- pyglet/graphics/__init__.py
+++ pyglet/graphics/__init__.py
@@ -14,8 +14,9 @@ from pyglet.graphics import api  # noqa: F401
 from pyglet.graphics.draw import Group, ShaderGroup  # noqa: F401
 from pyglet.graphics.state import State  # noqa: F401
 from pyglet.graphics.api import Batch, get_default_shader, get_default_batch, core, Shader, ShaderProgram, ComputeShaderProgram  # noqa: F401
-from pyglet.graphics.texture import Texture, TextureGrid, Texture3D   # noqa: F401
+from pyglet.graphics.texture import Texture, TextureGrid, Texture3D, TextureArray   # noqa: F401
 from pyglet.graphics.atlas import TextureBin, TextureArrayBin, TextureAtlas   # noqa: F401
+from pyglet.graphics.framebuffer import Framebuffer, Renderbuffer  # noqa: F401
 #
 # def _load_backend_base(name: str):
 #     if pyglet.options.backend == "opengl":
