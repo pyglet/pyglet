d66a40d6a998b74c0f1700b11cd8dc02baeb2166
caffeinepills <vhxonline@gmail.com>
2025-12-02 13:51:35 -0600

    Update test for vertex list for fragmentation changes.

--- tests/integration/graphics/test_vertex_list.py
+++ tests/integration/graphics/test_vertex_list.py
@@ -5,7 +5,7 @@ import pytest
 import pyglet
 from pyglet.graphics import GeometryMode
 
-def create_quad_vertex_list(x, y, z, width, height):
+def _create_quad_vertices(x, y, z, width, height):
     return (x, y, z,
             x + width, y, z,
             x + width, y + height, z,
@@ -79,7 +79,7 @@ class VertexListTest(unittest.TestCase):
     def test_indexed_vertex_list_creation(self):
         program = pyglet.graphics.api.get_default_shader()
 
-        vertices = create_quad_vertex_list(0, 0, 0, 50, 50)
+        vertices = _create_quad_vertices(0, 0, 0, 50, 50)
         colors = (1, 0.5, 0.2, 1) * 4
         indices = [0, 1, 2, 0, 2, 3]
 
@@ -98,8 +98,8 @@ class VertexListTest(unittest.TestCase):
     def test_indexed_vertex_list_property_set(self):
         program = pyglet.graphics.api.get_default_shader()
 
-        vertices_1 = create_quad_vertex_list(0, 0, 0, 50, 50)
-        vertices_2 = create_quad_vertex_list(3, 2, 1, 0, 20)
+        vertices_1 = _create_quad_vertices(0, 0, 0, 50, 50)
+        vertices_2 = _create_quad_vertices(3, 2, 1, 0, 20)
         colors = (1, 0.5, 0.2, 1) * 4
         indices = [0, 1, 2, 0, 2, 3]
 
@@ -114,3 +114,59 @@ class VertexListTest(unittest.TestCase):
         vertex_list.position[:] = vertices_2
 
         assert tuple(vertex_list.position[:]) == pytest.approx(vertices_2)
+
+    def test_fragmentation(self):
+        """This test splits up draws to ensure the vertex list is actually fragmented when another list is deleted.
+
+         Ensure the bucket correctly splits and re-joins when necessary.
+
+         Also initializes the draw to ensure multi-draw does not error.
+         """
+        program = pyglet.graphics.api.get_default_shader()
+        batch = pyglet.graphics.Batch()
+
+        vertices = _create_quad_vertices(0, 0, 0, 50, 50)
+        colors = (1, 0.5, 0.2, 1) * 4
+        indices = [0, 1, 2, 0, 2, 3]
+
+        vertex_list1 = program.vertex_list_indexed(4, GeometryMode.TRIANGLES, indices,
+                                                  batch=batch,
+                                                  group=None,
+                                                  position=('f', vertices),
+                                                  colors=('f', colors))
+
+        vertex_list2 = program.vertex_list_indexed(4, GeometryMode.TRIANGLES, indices,
+                                                   batch=batch,
+                                                   group=None,
+                                                   position=('f', vertices),
+                                                   colors=('f', colors))
+
+        vertex_list3 = program.vertex_list_indexed(4, GeometryMode.TRIANGLES, indices,
+                                                   batch=batch,
+                                                   group=None,
+                                                   position=('f', vertices),
+                                                   colors=('f', colors))
+
+        shared_bucket = vertex_list1.bucket
+        assert vertex_list1.domain == vertex_list2.domain == vertex_list3.domain
+        assert vertex_list1.bucket == vertex_list2.bucket == vertex_list3.bucket
+        assert len(shared_bucket.merged_ranges) == 1  # All 3 should be merged together.
+
+        vertex_list2.delete()
+
+        assert vertex_list1.bucket == vertex_list3.bucket
+        assert len(shared_bucket.merged_ranges) == 2  # Fragmented into 2 calls.
+
+        batch.draw()
+
+        # Add another to make sure fragmentation is filled in where possible.
+        vertex_list4 = program.vertex_list_indexed(4, GeometryMode.TRIANGLES, indices,
+                                                   batch=batch,
+                                                   group=None,
+                                                   position=('f', vertices),
+                                                   colors=('f', colors))
+
+        shared_bucket = vertex_list1.bucket
+        assert vertex_list1.domain == vertex_list4.domain == vertex_list3.domain
+        assert vertex_list1.bucket == vertex_list4.bucket == vertex_list3.bucket
+        assert len(shared_bucket.merged_ranges) == 1  # All 3 should be merged together again.
