02f79c4c7afa4d0470da900687c8e3a54b3c55cf
Benjamin Moran <benmoran@protonmail.com>
2025-12-07 09:17:41 +0900

    Add +/- to fps_change example.

--- examples/window/fps_change.py
+++ examples/window/fps_change.py
@@ -5,21 +5,33 @@ from pyglet.window import key
 window = pyglet.window.Window(vsync=False)
 fps = pyglet.window.FPSDisplay(window=window)
 batch = pyglet.graphics.Batch()
-label = pyglet.text.Label("Press a number from 1-0 to change the FPS. (number * 10)", x=10, y=50, batch=batch)
+label = pyglet.text.Label("Press a number from 1-0 (or +/-) to change the FPS. (number * 10)", x=10, y=50, batch=batch)
 
 circle = pyglet.shapes.Circle(200, 200, 25, color=(200, 60, 130), batch=batch)
 MOVEMENT_SPEED = 600
+REDRAW_RATE = 60
+
 
 
 @window.event
 def on_key_press(keycode, mod):
-    rates = {key._1: 1/10, key._2: 1/20, key._3: 1/30, key._4: 1/40, key._5: 1/50,
-             key._6: 1/60, key._7: 1/70, key._8: 1/80, key._9: 1/90, key._0: 1/100}
+    global REDRAW_RATE, fps
+    new_redraw_rate = REDRAW_RATE
+
+    rates = {key._1: 10, key._2: 20, key._3: 30, key._4: 40, key._5: 50,
+             key._6: 60, key._7: 70, key._8: 80, key._9: 90, key._0: 100}
 
     if redraw_rate := rates.get(keycode):
+        new_redraw_rate = redraw_rate
+    elif keycode in (key.PLUS, key.EQUAL):
+        new_redraw_rate = REDRAW_RATE + 10
+    elif keycode == key.MINUS:
+        new_redraw_rate = REDRAW_RATE - 10
+
+    if new_redraw_rate != REDRAW_RATE:
+        REDRAW_RATE = new_redraw_rate
         pyglet.clock.unschedule(window.draw)
-        pyglet.clock.schedule_interval(window.draw, redraw_rate)
-        global fps
+        pyglet.clock.schedule_interval(window.draw, 1/REDRAW_RATE)
         fps = pyglet.window.FPSDisplay(window=window)
 
 
@@ -27,10 +39,12 @@ def on_key_press(keycode, mod):
 def on_refresh(dt):
     global MOVEMENT_SPEED
 
-    circle.x += MOVEMENT_SPEED * dt
+    if circle.x > window.width:
+        MOVEMENT_SPEED = -abs(MOVEMENT_SPEED)
+    elif circle.x < 0:
+        MOVEMENT_SPEED = abs(MOVEMENT_SPEED)
 
-    if circle.x > window.width or circle.x < 0:
-        MOVEMENT_SPEED = -MOVEMENT_SPEED
+    circle.x += MOVEMENT_SPEED * dt
 
     window.clear()
     batch.draw()
