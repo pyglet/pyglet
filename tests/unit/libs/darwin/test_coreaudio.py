f094a8f37054bd3c91419679cc82ebb1472ed062
Johnathan August Fisher <joaufi@protonmail.com>
2026-04-05 22:29:15 -0400

    Parameterize test

--- tests/unit/libs/darwin/test_coreaudio.py
+++ tests/unit/libs/darwin/test_coreaudio.py
@@ -4,12 +4,7 @@ from tests.annotations import require_platform, Platform
 
 
 @require_platform(Platform.OSX)
-def test_coreaudio__try_to_add_key_and_value_to_err_str_db__type_error_raised():
+@pytest.mark.parametrize("key", ["some_key", kAudioFileFileNotFoundError])
+def test_coreaudio__try_to_change_key_and_value_of_err_str_db__type_error_raised(key: str):
     with pytest.raises(TypeError):
-        err_str_db["some_key"] = "some_value"
-
-
-@require_platform(Platform.OSX)
-def test_coreaudio__try_to_change_value_of_key_in_err_str_db__type_error_raised():
-    with pytest.raises(TypeError):
-        err_str_db[kAudioFileFileNotFoundError] = "some_value"
+        err_str_db[key] = "some_value"
