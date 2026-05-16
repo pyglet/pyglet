5ab01f0f87788bf68757d42702648a5fe1ce3858
Benjamin Moran <benmoran@protonmail.com>
2026-05-16 21:53:57 +0900

    math: faster Quaternion multiplication. More unit tests.

--- pyglet/math.py
+++ pyglet/math.py
@@ -1535,8 +1535,10 @@ class Quaternion(_typing.NamedTuple):
         return self.conjugate() * (1 / self.dot(self))
 
     def __matmul__(self, other: Quaternion) -> Quaternion:
-        a, u = self.w, Vec3(*self[1:])
-        b, v = other.w, Vec3(*other[1:])
-        scalar = a * b - u.dot(v)
-        vector = v * a + u * b + u.cross(v)
-        return Quaternion(scalar, *vector)
+        w1, x1, y1, z1 = self
+        w2, x2, y2, z2 = other
+        w = w1 * w2 - x1 * x2 - y1 * y2 - z1 * z2
+        x = w1 * x2 + x1 * w2 + y1 * z2 - z1 * y2
+        y = w1 * y2 - x1 * z2 + y1 * w2 + z1 * x2
+        z = w1 * z2 + x1 * y2 - y1 * x2 + z1 * w2
+        return Quaternion(w, x, y, z)
--- tests/unit/math/test_quat.py
+++ tests/unit/math/test_quat.py
@@ -1,14 +1,20 @@
 import pytest
+
+import math
+
 from pyglet.math import Quaternion, Mat3, Mat4
 
 
-def test_create():
-    q = Quaternion()
-    assert q.x == 0
-    assert q.y == 0
-    assert q.z == 0
-    assert q.w == 1
-    assert q.length() == 1
+@pytest.fixture
+def identity():
+    return Quaternion(1, 0, 0, 0)
+
+
+def isclose(q1: Quaternion, q2: Quaternion, tolerance: float = 0.000001) -> bool:
+    return (math.isclose(q1.w, q2.w, rel_tol=tolerance, abs_tol=tolerance) and
+            math.isclose(q1.x, q2.x, rel_tol=tolerance, abs_tol=tolerance) and
+            math.isclose(q1.y, q2.y, rel_tol=tolerance, abs_tol=tolerance) and
+            math.isclose(q1.z, q2.z, rel_tol=tolerance, abs_tol=tolerance))
 
 
 def test_to_mat3():
@@ -21,3 +27,37 @@ def test_to_mat4():
     # TODO: Needs improvement. Just checking identity matrix.
     q = Quaternion()
     assert q.to_mat4() == Mat4()
+
+
+def test_norm_preservation():
+    # normalizing before or after multiplication should match.
+    p = Quaternion(1, 2, 3, 4)
+    q = Quaternion(5, 6, 7, 8)
+    assert isclose((p @ q).normalize(), p.normalize() @ q.normalize())
+
+
+def test_conjugate_rule():
+    p = Quaternion(1, 2, 3, 4)
+    q = Quaternion(5, 6, 7, 8)
+    assert (p @ q).conjugate() == q.conjugate() @ p.conjugate()
+
+
+def test_inverse_property(identity):
+    q = Quaternion(1, 2, 3, 4)
+    assert isclose(q @ ~q, identity)
+    assert isclose(~q @ q, identity)
+
+
+def test_rotation_composition(identity):
+    # 36 degrees rotation around the X axis
+    r = Quaternion(math.cos(math.pi / 10), math.sin(math.pi / 10), 0, 0)
+
+    # Compose 10 rotations (360 degrees total)
+    result = r
+    for _ in range(9):
+        result = result @ r
+
+    negative_identity = Quaternion(-1, 0, 0, 0)
+
+    # Check if result is close to either identity or negative identity
+    assert isclose(result, identity) or isclose(result, negative_identity)
