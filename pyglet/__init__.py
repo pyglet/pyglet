5f33736e77728e7dbadc178ee042c95805239f73
Benjamin Moran <benmoran@protonmail.com>
2026-06-30 10:51:44 +0900

    Ready v3.0.dev6 release

--- pyglet/__init__.py
+++ pyglet/__init__.py
@@ -18,7 +18,7 @@ if TYPE_CHECKING:
     from typing import Any, Callable, ItemsView, Sized
 
 #: The release version
-version = '3.0.dev5'
+version = '3.0.dev6'
 __version__ = version
 
 MIN_PYTHON_VERSION = 3, 8
