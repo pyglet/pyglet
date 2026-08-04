9807f2f8ea3005b2a0906ba56c4f14652688aedb
caffeinepills <vhxonline@gmail.com>
2026-08-03 19:23:30 -0500

    Fix opengles3 compute shader test.

--- tests/integration/graphics/opengl/test_compute_shader.py
+++ tests/integration/graphics/opengl/test_compute_shader.py
@@ -21,7 +21,7 @@ COMPUTE_SRC_GLES = """#version 310 es
 precision highp float;
 layout(local_size_x = 1, local_size_y = 1, local_size_z = 1) in;
 
-layout(rgba32f) uniform highp image2D img_output;
+layout(rgba32f) writeonly uniform highp image2D img_output;
 
 void main() {
     ivec2 texel_coord = ivec2(gl_GlobalInvocationID.xy);
