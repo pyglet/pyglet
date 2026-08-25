743ebfd161a875e191a4b9a555d92a90145ae5d9
caffeinepills <vhxonline@gmail.com>
2026-08-25 14:30:53 -0500

    Update tests.

--- tests/integration/graphics/test_texture2d.py
+++ tests/integration/graphics/test_texture2d.py
@@ -52,24 +52,6 @@ class TestTextureUploadFetch(unittest.TestCase):
         self.assertEqual(data[9 * 4], 7)
         self.assertEqual(data[10 * 4], 7)
 
-    def test_upload_respects_texture_anchor(self):
-        width, height = 4, 4
-        base = self.create_image(width, height, 0)
-        texture = Texture.create(width, height, blank_data=True)
-        texture.upload(base, 0, 0, 0)
-
-        texture.anchor_x = 1
-        texture.anchor_y = 1
-        image = self.create_image(2, 2, 9)
-        texture.upload(image, 1, 1, 0)
-
-        fetched = texture.get_image_data()
-        data = fetched.get_bytes('RGBA', fetched.width * 4)
-        self.assertEqual(data[0 * 4], 9)
-        self.assertEqual(data[1 * 4], 9)
-        self.assertEqual(data[4 * 4], 9)
-        self.assertEqual(data[5 * 4], 9)
-
     def test_upload_invalid_level(self):
         texture = Texture.create(4, 4, blank_data=True)
         with self.assertRaisesRegex(Exception, "Mipmap level must be non-negative"):
