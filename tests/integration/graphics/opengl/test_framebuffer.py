2319de245299dfc3fffb4b0b6fdb24754d23b746
caffeinepills <vhxonline@gmail.com>
2026-09-22 17:47:00 -0500

    Skip GL2 for test.

--- tests/integration/graphics/opengl/test_framebuffer.py
+++ tests/integration/graphics/opengl/test_framebuffer.py
@@ -64,6 +64,7 @@ def test_framebuffer_attach_texture_and_readback(test_window):
         texture.delete()
 
 
+@skip_graphics_api(GraphicsAPIGroups.GL2)
 def test_framebuffer_draw_buffers_and_individual_clears(test_window):
     test_window.switch_to()
 
@@ -121,6 +122,7 @@ def test_framebuffer_dsa_preserves_bound_framebuffer(test_window):
         texture.delete()
 
 
+@skip_graphics_api(GraphicsAPIGroups.GL2)
 def test_nested_framebuffer_contexts_avoid_redundant_binds(test_window):
     test_window.switch_to()
 
@@ -147,6 +149,7 @@ def test_nested_framebuffer_contexts_avoid_redundant_binds(test_window):
         framebuffer.delete()
 
 
+@skip_graphics_api(GraphicsAPIGroups.GL2)
 def test_framebuffer_fallback_operations_restore_binding(test_window):
     test_window.switch_to()
 
